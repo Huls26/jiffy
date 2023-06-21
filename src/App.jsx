afedfb8adc36018d2c18a127a221745091747c90
Jules Moreno <julesmoreno66@gmail.com>
2023-06-21 09:46:43 +0800

    File structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,6 @@
-// eslint-disable-next-line import/no-unresolved
-import useCounter from '@hooks/useCounter';
-import './App.css';
-
 function App() {
-  const { count, increment } = useCounter();
-
-  console.log('testing pre-commit');
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={increment}
-          >
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.tsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <h1>Web app</h1>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -19,10 +19,13 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@api': path.resolve(__dirname, './src/api'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@features': path.resolve(__dirname, './src/features'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
       '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
