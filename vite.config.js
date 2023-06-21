0d2ef21ccf893a82f6620fcb8d2dae73daf8faaa
Jules Moreno <julesmoreno66@gmail.com>
2023-06-21 10:04:59 +0800

    Add react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@features': path.resolve(__dirname, './src/features'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@layout': path.resolve(__dirname, './src/layout'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@routes': path.resolve(__dirname, './src/routes'),
       '@utils': path.resolve(__dirname, './src/utils'),
