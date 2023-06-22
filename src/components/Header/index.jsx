9d36e6b1ff31b0210fa3623dcc757e71cf29cc74
Jules Moreno <julesmoreno66@gmail.com>
2023-06-22 10:12:53 +0800

    tailwind.config.js: setup color and font family

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,14 @@
 export default function Header() {
   return (
     <header>
-      <h1>Logo Here</h1>
+      <h1 className="font-A text-lg">Logo Here</h1>
       <form>
-        <input type="text" />
+        <input type="text" placeholder="Search" />
         <input type="submit" />
       </form>
 
       <div>
-        <h1>Create post logo</h1>
+        <h1 className="font-A">Create post logo</h1>
         <div className="bg-black w-10 h-10 text-white text-[.7em] text-center rounded-full">
           user photo
         </div>
