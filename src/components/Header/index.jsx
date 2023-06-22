29cbdc63f1177710fde03f29243a13fb86dcdd85
Jules Moreno <julesmoreno66@gmail.com>
2023-06-22 15:36:42 +0800

    Header.jsx: add flex arrange line up & body: add background color

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,15 +2,20 @@
 
 export default function Header() {
   return (
-    <header>
+    <header className="
+    flex justify-between items-center
+    px-5 py-3
+    border
+    "
+    >
       <h1 className="font-A text-lg">Logo Here</h1>
       <form>
         <input type="text" placeholder="Search" />
         <input type="submit" />
       </form>
 
-      <div>
-        <h1 className="font-A">Create post logo</h1>
+      <div className="flex space-x-2 items-center">
+        <h1 className="font-A">+</h1>
         <div className="bg-dark-1 w-10 h-10 text-white text-[.7em] text-center rounded-full">
           user photo
         </div>
