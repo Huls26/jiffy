1cd26d89ea3f3c3027c60595e604c66bde47774a
Jules Moreno <julesmoreno66@gmail.com>
2023-06-26 09:59:59 +0800

    ViewContent.jsx: add commentSeection.jsx

--- src/features/ViewContent/components/CommentSection.jsx
+++ src/features/ViewContent/components/CommentSection.jsx
@@ -1,11 +1,59 @@
 import PropTypes from 'prop-types';
 
 import UserImage from '@components/UserImage';
+import UserComments from './UserComments';
 
 export default function CommentSection({ details }) {
+  const imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg';
+  const comments = [
+    {
+      id: '1',
+      userImg: imgUrl,
+      username: 'Patrick Star',
+      date: '9 months ago',
+      userComments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, ut.',
+    },
+    {
+      id: '2',
+      userImg: imgUrl,
+      username: 'Patrick Star',
+      date: 'today',
+      userComments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, ut.',
+    },
+    {
+      id: '3',
+      userImg: imgUrl,
+      username: 'Patrick Star',
+      date: '3 days ago',
+      userComments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, ut.',
+    },
+    {
+      id: '4',
+      userImg: imgUrl,
+      username: 'Patrick Star',
+      date: '3 months ago',
+      userComments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, ut.',
+    },
+    {
+      id: '5',
+      userImg: imgUrl,
+      username: 'Patrick Star',
+      date: 'today',
+      userComments: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, ut.',
+    },
+  ];
+
+  const renderComments = comments
+    .map((commentDetails) => (
+      <UserComments
+        key={commentDetails.id}
+        commentsDetail={commentDetails}
+      />
+    ));
+
   return (
     <section className="px-3 py-4">
-      <form className="flex space-x-3">
+      <form className="flex space-x-3 mb-5">
         <UserImage userImg={details.userImg} />
         <label htmlFor="comment" className="w-full">
           <input
@@ -25,6 +73,8 @@ export default function CommentSection({ details }) {
           />
         </label>
       </form>
+
+      {renderComments}
     </section>
   );
 }
