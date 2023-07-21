3e8e3c3adbfda3d89312ebb7f303fb49aa518494
Jules Moreno <julesmoreno66@gmail.com>
2023-07-21 16:08:27 +0800

    CreatePost.jsx: add api and hook and utils code clean up

--- src/features/CreatePost/Components/CreatePostEditor.jsx
+++ src/features/CreatePost/Components/CreatePostEditor.jsx
@@ -1,164 +1,25 @@
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { doc, setDoc } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import { dataContext } from '@context/dataContext';
-import { v4 as uuidv4 } from 'uuid';
-import {
-  db,
-  storage,
-} from '@api/FB';
+import SpinnerCard from '@components/Loading/SpinnerCard';
+import useHandleCreatePost from '../hooks/useHandleCreatePost';
 import ErrorMessage from './ErrorMessage';
 import CancelFileBtn from './CancelFileBtn';
-// import { Form } from 'react-router-dom';
 
-// clean up CreatePostEditor
 /* eslint-disable max-len */
 export default function CreatePostEditor() {
-  const [data] = useContext(dataContext);
-  const { userId, userData } = data;
-  const { username, userImg } = userData;
-  const navigate = useNavigate();
-  const [file, setFile] = useState(() => ({
-    title: '', textContent: '', errorMessage: '', isLoading: false,
-  }));
-  const docDataPost = {
-    content: '',
-    textContent: '',
-    createdBy: userId,
-    username,
-    description: '',
-    likes: 0,
-    peopleLikes: [],
-    date: new Date(),
-    title: '',
-    userImg,
-    comments: [],
-  };
-  const disabledElement = file.isLoading ? 'opacity-50' : '';
-
-  function handleChange(event) {
-    const { target } = event;
-    const { value, name } = target;
-
-    if (name === 'title') {
-      setFile((fileData) => ({
-        ...fileData,
-        [name]: value,
-      }));
-    } else if (name === 'textContent') {
-      // reset value of input type="file"
-      // reset value
-      document.getElementById('imageFile').value = null;
-      setFile((fileData) => ({
-        ...fileData,
-        [name]: value,
-        fileName: '',
-        imgFile: '',
-        imgFileValue: '',
-        errorMessage: '',
-      }));
-    }
-  }
-
-  function handleCancelFile() {
-    // reset value of input type="file"
-    // reset value
-    document.getElementById('imageFile').value = null;
-    setFile((fileData) => ({
-      ...fileData,
-      fileName: '',
-      imgFile: '',
-      imgFileValue: '',
-      errorMessage: '',
-    }));
-  }
-
-  function handleImageFile(event) {
-    const { target } = event;
-    const [fileName] = target.files;
-
-    if (fileName) {
-      // cut the length of the word
-      const maxLengthWord = 15;
-      const isLong = fileName.name.length > maxLengthWord;
-      const splitWord = fileName.name.split('').slice(0, maxLengthWord);
-      const concatWord = [...splitWord, '...'].join('');
-      const cutword = isLong ? concatWord : fileName.name;
-
-      setFile((dataFile) => ({
-        ...dataFile,
-        fileName: cutword,
-        imgFile: fileName,
-        imgFileValue: target.value,
-        textContent: '',
-        errorMessage: '',
-      }));
-    }
-  }
-
-  async function createPost(docData, fileBody, setData) {
-    const newId = uuidv4();
-    setData((postData) => ({
-      ...postData,
-      isLoading: true,
-    }));
-
-    if (fileBody.imgFile) {
-      // create a url or a path name
-      // upload image
-      // get url of image in firestore storage
-      // set a document/object to firestore
-      // if content image is available set a url
-      // else store textContent
-      // redirect user to main page after post is created
-      const pathStorage = `posts/${userId}/${fileBody.fileName}${newId}`;
-      const ImageRef = ref(storage, pathStorage);
-      await uploadBytes(ImageRef, fileBody.imgFile);
-      const imageUrl = await getDownloadURL(ImageRef);
-      await setDoc(doc(db, 'posts', newId), {
-        ...docData,
-        content: imageUrl,
-        title: fileBody.title,
-      });
-      setData((postData) => ({
-        ...postData,
-        isLoading: false,
-      }));
-      navigate('/');
-    } else if (fileBody.textContent) {
-      await setDoc(doc(db, 'posts', newId), {
-        ...docData,
-        textContent: fileBody.textContent,
-        title: fileBody.title,
-      });
-      setData((postData) => ({
-        ...postData,
-        isLoading: false,
-      }));
-      navigate('/');
-    }
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    // store to firebase if there is file
-    if (file.imgFile || file.textContent) {
-      createPost(docDataPost, file, setFile);
-    } else {
-      setFile((dataFile) => ({
-        ...dataFile,
-        errorMessage: 'Oops nothing is created',
-      }));
-    }
-  }
+  const {
+    file,
+    disabledElement,
+    handleChange,
+    handleCancelFile,
+    handleImageFile,
+    handleSubmit,
+  } = useHandleCreatePost();
 
   return (
     <>
       <ErrorMessage message={file.errorMessage} />
-      {file.isLoading && <h1>...Loading</h1>}
-      <form onSubmit={handleSubmit} method="post" className={`${disabledElement} outline-none`}>
+      <form onSubmit={handleSubmit} method="post" className={`${disabledElement} outline-none relative`}>
+        {file.isLoading && <SpinnerCard />}
+
         <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
           <div className="px-4 py-2 bg-primary-1 rounded-t-lg">
             <label htmlFor="title" className="sr-only">Your comment</label>
