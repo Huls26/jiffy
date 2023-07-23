a156487b71457257f176afd9291d92840b8c3290
Jules Moreno <julesmoreno66@gmail.com>
2023-07-23 10:38:21 +0800

    DisplayCOntent.jsx: wrap-word of text content

--- src/features/Contents/components/DisplayContent.jsx
+++ src/features/Contents/components/DisplayContent.jsx
@@ -21,7 +21,7 @@ export default function DisplayContent() {
     )
     : (
       <div className="w-full h-full px-11 flex items-center justify-center">
-        <p className="text-dark-1 text-2xl font-bold font-PS text-justify leading-tight opacity-90 text-ellipsis overflow-hidden">
+        <p className="text-dark-1 text-2xl font-bold font-PS text-justify leading-tight opacity-90 text-ellipsis overflow-hidden break-words">
           {sliceWord}
         </p>
       </div>
