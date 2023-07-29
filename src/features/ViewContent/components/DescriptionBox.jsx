9a5ff34f17b02735df509aa845cb10e97401c408
Jules Moreno <julesmoreno66@gmail.com>
2023-07-29 16:09:19 +0800

    ViewContent/Description.jsx: code clean uup and add DisplayDescriptText.jsx

--- src/features/ViewContent/components/DescriptionBox.jsx
+++ src/features/ViewContent/components/DescriptionBox.jsx
@@ -1,29 +1,25 @@
 import { useLocation } from 'react-router-dom';
-import modifyTimeDate from '../utils/modifyTimeDate';
-/* eslint-disable react/jsx-curly-brace-presence */
+// import { useState } from 'react';
+import DisplayTimeDate from './DisplayTimeDate';
+import DisplayDescriptionText from './DisplayDescriptionText';
+
+// code clean up
 export default function DescriptionBox() {
   const { state } = useLocation();
   const { docData } = state;
-  const timeDate = modifyTimeDate(docData);
 
   return (
     <section className="
                   border-dark-2 border border-b-2 border-r-2
-                    px-3 pt-3 pb-4
+                    p-3 pb-2
                     mx-3 mb-4
                     rounded-lg
                     leading-none
                     text-gray-dark font-A
                   "
     >
-      <p className="font-bold mb-4">{timeDate}</p>
-
-      <p className="font-semibold">
-        {
-          docData.description
-        }
-        &apos;&apos;&apos;add more button&apos;&apos;
-      </p>
+      <DisplayTimeDate docData={docData} />
+      <DisplayDescriptionText docData={docData} />
     </section>
   );
 }
