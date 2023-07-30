0e5c4c358720c7a4bbbbbf565a88a006a7b67aa0
Jules Moreno <julesmoreno66@gmail.com>
2023-07-30 09:54:10 +0800

    ViewPage.jsx: Fix view scroll reset

--- src/pages/ViewPage.jsx
+++ src/pages/ViewPage.jsx
@@ -1,15 +1,17 @@
-import { lazy } from 'react';
+import { lazy, useLayoutEffect } from 'react';
 // import ContentComponents from '@features/ViewContent';
 const ContentComponents = lazy(() => import('@features/ViewContent'));
 
 export default function ViewPage() {
-  // eslint-disable-next-line max-len
-  // const imgUrl = 'https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg';
   const userDetails = {
     userImg: 'https://images.pexels.com/photos/3768263/pexels-photo-3768263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     username: 'John Lee',
     textDetails: 'something awesome text',
   };
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <main>
