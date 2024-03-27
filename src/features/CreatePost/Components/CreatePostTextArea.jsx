e84b0c400e7a8687ad9856d95277ee0704fcd8b1
Jules Moreno <julesmoreno66@gmail.com>
2024-03-27 14:39:36 +0800

    src/features/CreatePost/style/createPostTextArea.js: separate style

--- src/features/CreatePost/Components/CreatePostTextArea.jsx
+++ src/features/CreatePost/Components/CreatePostTextArea.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import style from '../style/createPostTextArea';
 
 export default function CreatePostTextArea({ file, handleChange }) {
   return (
     <section className="px-4 py-2 bg-primary-1 rounded-t-lg">
-      <textarea maxLength="252" value={file.textContent} onChange={handleChange} name="textContent" id="textContent" rows="4" className="w-full px-1 text-lg font-A text-dark-1 bg-white border border-primary-1 focus:border-gray rounded-md dark:bg-primary-1 focus:ring-0 dark:placeholder-gray outline-none" placeholder={file.imgFile ? `${file.imgFileValue} \n- Post Image` : 'Write Something...'} disabled={file.isLoading} />
+      <textarea maxLength="252" value={file.textContent} onChange={handleChange} name="textContent" id="textContent" rows="4" className={style().textAreaStyle} placeholder={file.imgFile ? `${file.imgFileValue} \n- Post Image` : 'Write Something...'} disabled={file.isLoading} />
     </section>
   );
 }
