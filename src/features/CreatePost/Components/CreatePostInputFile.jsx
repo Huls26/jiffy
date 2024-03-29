bbc413a66cdab4e20bdf3db2d26bd7f7e6d464e3
Jules Moreno <julesmoreno66@gmail.com>
2024-03-29 15:08:31 +0800

    src/features/CreatePost/style/createPostInputFile.js: separate style

--- src/features/CreatePost/Components/CreatePostInputFile.jsx
+++ src/features/CreatePost/Components/CreatePostInputFile.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
+import style from '../style/createPostInputFile';
 
 export default function CreatePostInputFile({ file, handleImageFile }) {
   return (
-    <label htmlFor="imageFile" className="inline-flex justify-center items-center p-1 text-dark-1 rounded cursor-pointer hover:text-white hover:bg-gray active:bg-gray-dark">
+    <label htmlFor="imageFile" className={style().labelStyle}>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
         <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
       </svg>
@@ -19,5 +20,5 @@ CreatePostInputFile.propTypes = {
     fileName: PropTypes.any,
     isLoading: PropTypes.bool,
   }).isRequired,
-  handleImageFile: PropTypes.string.isRequired,
+  handleImageFile: PropTypes.func.isRequired,
 };
