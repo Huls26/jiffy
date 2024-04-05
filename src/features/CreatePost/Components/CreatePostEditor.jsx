fde3786e3c67c7d62f17643d96fcbe9408c53142
Jules Moreno <julesmoreno66@gmail.com>
2024-04-05 14:32:36 +0800

    src/features/CreatePost/Components/CreatePostEditor.jsx: update component

--- src/features/CreatePost/Components/CreatePostEditor.jsx
+++ src/features/CreatePost/Components/CreatePostEditor.jsx
@@ -1,13 +1,12 @@
 import SpinnerCard from '@/components/Loading/SpinnerCard';
-import CreatePostTitleInput from './CreatePostTitleInput';
-import useHandleCreatePost from '../hooks/useHandleCreatePost';
-import ErrorMessage from './ErrorMessage';
-import CancelFileBtn from './CancelFileBtn';
-import CreatePostBtn from './CreatePostBtn';
-import CreatePostTextArea from './CreatePostTextArea';
-import CreatePostInputFile from './CreatePostInputFile';
 
-/* eslint-disable max-len */
+import ErrorMessage from './ErrorMessage';
+import CreatePostTitleInput from './CreatePostTitleInput';
+import CreatePostTextArea from './CreatePostTextArea';
+import CreatePostBtnSection from './CreatePostBtnSection';
+
+import useHandleCreatePost from '../hooks/useHandleCreatePost';
+
 export default function CreatePostEditor() {
   const {
     file,
@@ -27,16 +26,11 @@ export default function CreatePostEditor() {
         <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
           <CreatePostTitleInput file={file} handleChange={handleChange} />
           <CreatePostTextArea file={file} handleChange={handleChange} />
-          {
-            // separate element create component for composable
-          }
-          <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-            <CreatePostBtn file={file} />
-            <div className="flex pl-0 sm:pl-2">
-              <CreatePostInputFile file={file} handleImageFile={handleImageFile} />
-              <CancelFileBtn file={file} onClick={() => handleCancelFile()} />
-            </div>
-          </div>
+          <CreatePostBtnSection
+            file={file}
+            handleImageFile={handleImageFile}
+            handleCancelFile={handleCancelFile}
+          />
         </div>
       </form>
     </>
