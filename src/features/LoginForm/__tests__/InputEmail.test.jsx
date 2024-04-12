a38d1e41805b772ed051296a9d3249735a517c8b
Jules Moreno <julesmoreno66@gmail.com>
2024-04-12 18:11:52 +0800

    src/features/LoginForm/__tests__/InputEmail.test.jsx: add test

---
+++ src/features/LoginForm/__tests__/InputEmail.test.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+import { render, screen } from '@testing-library/react';
+import InputEmail from '../components/InputEmail';
+
+describe('InputEmail', () => {
+  test('renders an input field with the correct placeholder', () => {
+    render(<InputEmail emailBorder="border-2" isErrorEmail={false} email="" />);
+
+    const input = screen.getByPlaceholderText('Email');
+
+    expect(input).toBeInTheDocument();
+  });
+
+  test('renders an error border when isErrorEmail is true', () => {
+    render(<InputEmail emailBorder="border-2" isErrorEmail={false} email="" />);
+
+    const input = screen.getByPlaceholderText('Email');
+    const errorBorder = screen.getByTestId('email-border');
+
+    expect(input).toBeInTheDocument();
+    expect(errorBorder).toBeInTheDocument();
+  });
+});
