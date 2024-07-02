af5a284c45c73713ed5ae490bfaceff64c492049
Jules <julesmoreno66@gmail.com>
2024-07-02 17:05:47 +0800

    tailwind.config.js: config set tailwind to jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit",
-  purge: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // "./node_modules/ant-design-vue/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: false, // or'media' or 'class'
+  // darkMode: false, // or'media' or 'class'
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
