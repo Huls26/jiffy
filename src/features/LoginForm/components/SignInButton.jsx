d93b05cf03a891e925d2844b9b9cac10311877ad
Jules <julesmoreno66@gmail.com>
2024-07-04 15:41:17 +0800

    src/features/LoginForm/components/SignInButton.jsx: created component to compose with other component

---
+++ src/features/LoginForm/components/SignInButton.jsx
@@ -0,0 +1,15 @@
+export default function SignInButton() {
+  return (
+    <button
+      type="button"
+      className="
+        w-full px-8 py-3 
+        font-semibold rounded-md 
+        dark:text-gray-50 dark:bg-blue-600 
+        hover:opacity-70 active:opacity-55
+      "
+    >
+      Sign in
+    </button>
+  );
+}
