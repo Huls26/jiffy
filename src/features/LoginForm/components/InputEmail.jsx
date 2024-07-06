413d67dc13772b07526cb945786cd7962cdb4430
Jules <julesmoreno66@gmail.com>
2024-07-06 11:30:32 +0800

    src/features/LoginForm/components/InputEmail.jsx: add useContext line 5 & create handleChange func

--- src/features/LoginForm/components/InputEmail.jsx
+++ src/features/LoginForm/components/InputEmail.jsx
@@ -2,9 +2,14 @@ import { LoginContext } from "@/pages/LoginPage";
 import { useContext } from "react";
 
 export default function InputEmail() {
-  const state = useContext(LoginContext);
+  const [state, dispatch] = useContext(LoginContext);
 
-  console.log(state);
+  function handleChange(event) {
+    dispatch({
+      type: "UPDATE_EMAIL",
+      payload: `${event.target.value}`,
+    });
+  }
 
   return (
     <section>
@@ -15,6 +20,8 @@ export default function InputEmail() {
         type="email"
         name="email"
         id="email"
+        value={state.email}
+        onChange={handleChange}
         placeholder="leroy@jenkins.com"
         className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
       />
