a6c759d5661b857acc6990a0b06423996840dcf5
Jules <julesmoreno66@gmail.com>
2024-07-06 10:35:10 +0800

    src/features/LoginForm/sections/SignInTextSection.jsx: separete components

---
+++ src/features/LoginForm/sections/SignInTextSection.jsx
@@ -0,0 +1,10 @@
+export default function SignInTextSection() {
+  return (
+    <section className="mb-8 text-center cursor-not-allowed">
+      <h1 className="my-3 text-4xl font-bold">Sign in</h1>
+      <p className="text-sm dark:text-gray-600">
+        Sign in to access your account
+      </p>
+    </section>
+  );
+}
