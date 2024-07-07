0b6a55b082efe7c29257fb30d4497f0105ae9223
Jules <julesmoreno66@gmail.com>
2024-07-07 15:33:15 +0800

    src/features/LoginForm/components/InputPassword.jsx: add context line 5 & change autocomple attribute to "current-password" line 21

--- src/features/LoginForm/components/InputPassword.jsx
+++ src/features/LoginForm/components/InputPassword.jsx
@@ -1,9 +1,24 @@
+import { LoginContext } from "@/pages/LoginPage";
+import { useContext } from "react";
+
 export default function InputPassword() {
+  const [state, dispatch] = useContext(LoginContext);
+
+  function handleChange(event) {
+    dispatch({
+      type: "UPDATE_PASSWORD",
+      payload: `${event.target.value}`,
+    });
+  }
+  console.log(state);
   return (
     <input
       type="password"
       name="password"
       id="password"
+      value={state.password}
+      onChange={handleChange}
+      autoComplete="current-password"
       placeholder="*****"
       className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
     />
