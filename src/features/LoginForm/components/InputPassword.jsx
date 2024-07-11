d3e5c9471e83d2d785224163031dbe6857525c36
Jules <julesmoreno66@gmail.com>
2024-07-11 10:36:18 +0800

    src/features/LoginForm/components/InputPassword.jsx: handleChange utils line 14

--- src/features/LoginForm/components/InputPassword.jsx
+++ src/features/LoginForm/components/InputPassword.jsx
@@ -1,23 +1,17 @@
 import { LoginContext } from "@/pages/LoginPage";
 import { useContext } from "react";
+import handleChange from "../utils/handleChange";
 
 export default function InputPassword() {
   const [state, dispatch] = useContext(LoginContext);
 
-  function handleChange(event) {
-    dispatch({
-      type: "UPDATE_PASSWORD",
-      payload: `${event.target.value}`,
-    });
-  }
-  console.log(state);
   return (
     <input
       type="password"
       name="password"
       id="password"
       value={state.password}
-      onChange={handleChange}
+      onChange={(event) => handleChange(event, dispatch, "UPDATE_PASSWORD")}
       autoComplete="current-password"
       placeholder="*****"
       className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
