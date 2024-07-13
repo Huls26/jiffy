950217514149ae461d9006709bfba16ff0634404
Jules <julesmoreno66@gmail.com>
2024-07-13 17:26:15 +0800

    src/hooks/LoginPageHooks/LoginPageContextReducer.js: add documentation/comment

--- src/hooks/LoginPageHooks/LoginPageContextReducer.js
+++ src/hooks/LoginPageHooks/LoginPageContextReducer.js
@@ -3,6 +3,19 @@ export const INITIAL_STATE = {
   password: "",
 };
 
+/**
+ * A reducer function for managing user authentication state.
+ *
+ * @function reducerMethod
+ * @param {Object} state - The current state of the application.
+ * @param {Object} action - The action object dispatched to the reducer.
+ * @param {string} state.email - The current email of the user.
+ * @param {string} state.password - The current password of the user.
+ * @param {string} action.type - The type of action being performed.
+ * @param {string} action.payload - The payload associated with the action.
+ * @returns {Object} - The updated state of the application.
+ * @throws {Error} - Throws an error if an invalid action type is encountered.
+ */
 // biome-ignore lint/style/useDefaultParameterLast: <explanation>
 export default function reducerMethod(state = INITIAL_STATE, action) {
   switch (action.type) {
