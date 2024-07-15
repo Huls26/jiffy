feab032697c622b6c464d0b5a19c83d5d4858a20
Jules <julesmoreno66@gmail.com>
2024-07-15 16:29:00 +0800

    src/features/LoginForm/context/LoginContextProvider.js: update context compose useLoginCOntext to loginContext provider

--- src/features/LoginForm/context/LoginContextProvider.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const loginContext = createContext();
-const LoginContextProvider = loginContext.Provider;
-
-export default LoginContextProvider;
-export { loginContext };
