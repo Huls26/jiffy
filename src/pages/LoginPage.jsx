03a239f9df618118a675c1d9248f80f0d358a5d3
Jules <julesmoreno66@gmail.com>
2024-07-15 16:30:03 +0800

    src/pages/LoginPage.jsx: update Context provider line 19

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,16 +2,12 @@ import LoginForm from "@/features/LoginForm";
 import LoginContextProvider from "@/features/LoginForm/context/LoginContextProvider";
 import SignInTextSection from "@/features/LoginForm/sections/SignInTextSection";
 
-import useLoginContext from "@/hooks/LoginPageHooks/useLoginContext";
-
 /**
  * This is the main component for the login page.
  * It renders a form for users to sign in.
  *
  * @returns {JSX.Element} - The JSX element for the login page.
  */ export default function LoginPage() {
-  const [state, dispatch] = useLoginContext();
-
   return (
     <main
       className="
@@ -20,7 +16,7 @@ import useLoginContext from "@/hooks/LoginPageHooks/useLoginContext";
                   dark:bg-gray-900 dark:text-gray-100 
                 "
     >
-      <LoginContextProvider value={[state, dispatch]}>
+      <LoginContextProvider>
         <SignInTextSection />
         <LoginForm />
       </LoginContextProvider>
