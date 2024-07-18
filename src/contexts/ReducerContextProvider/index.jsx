8515e1261dbbe101e290f6c3a766b0ebd4926536
Jules <julesmoreno66@gmail.com>
2024-07-18 15:11:29 +0800

    src/contexts/ReducerContextProvider/index.jsx: create context provider

---
+++ src/contexts/ReducerContextProvider/index.jsx
@@ -0,0 +1,58 @@
+import PropTypes from "prop-types";
+import { createContext, useReducer } from "react";
+
+/**
+ * A React context object for managing state and dispatching actions.
+ *
+ * @constant reducerContext
+ * @type {React.Context<any>}
+ * @default
+ * @example
+ * import { reducerContext } from "@/contexts/ReducerContextProvider";
+ *
+ * function LoginComponent() {
+ *   const [state, dispatch] = React.useContext(loginContext);
+ *   // ...
+ * }
+ */
+const reducerContext = createContext();
+export { reducerContext };
+
+/**
+ * A React context provider component for managing state and dispatching actions.
+ *
+ * @function ReducerContextProvider
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child components to be rendered within the provider.
+ * @returns {React.ReactElement} - The React context provider component.
+ *
+ *  @example
+ * import { ReducerContextProvider } from "@/contexts/ReducerContextProvider";
+ *
+ * function App() {
+ *   return (
+ *     <ReducerContextProvider reducerMethod={reducer} INITIAL_STATE={initialState}>
+ *       <YourAppComponents />
+ *     </ReducerContextProvider>
+ *   );
+ * }
+ */
+export default function ReducerContextProvider({
+  reducerMethod,
+  INITIAL_STATE,
+  children,
+}) {
+  const [state, dispatch] = useReducer(reducerMethod, INITIAL_STATE);
+
+  return (
+    <reducerContext.Provider value={[state, dispatch]}>
+      {children}
+    </reducerContext.Provider>
+  );
+}
+
+ReducerContextProvider.propTypes = {
+  reducerMethod: PropTypes.func.isRequired,
+  INITIAL_STATE: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
