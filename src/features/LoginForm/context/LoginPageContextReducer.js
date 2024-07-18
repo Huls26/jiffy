082568dd527a9f7ce2c39ec22858f441da358aa2
Jules <julesmoreno66@gmail.com>
2024-07-18 15:10:25 +0800

    src/features/LoginForm/context/LoginPageContextReducer.js: move reducer method and initial state

---
+++ src/features/LoginForm/context/LoginPageContextReducer.js
@@ -0,0 +1,29 @@
+export const INITIAL_STATE = {
+  email: "",
+  password: "",
+};
+
+/**
+ * A reducer function for managing user authentication state.
+ *
+ * @function reducerMethod
+ * @param {Object} state - The current state of the application.
+ * @param {Object} action - The action object dispatched to the reducer.
+ * @param {string} state.email - The current email of the user.
+ * @param {string} state.password - The current password of the user.
+ * @param {string} action.type - The type of action being performed.
+ * @param {string} action.payload - The payload associated with the action.
+ * @returns {Object} - The updated state of the application.
+ * @throws {Error} - Throws an error if an invalid action type is encountered.
+ */
+// biome-ignore lint/style/useDefaultParameterLast: <explanation>
+export default function reducerMethod(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case "UPDATE_EMAIL":
+      return { ...state, email: action.payload };
+    case "UPDATE_PASSWORD":
+      return { ...state, password: action.payload };
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
