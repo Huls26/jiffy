b9e0cca99dde130ad6609a5717ab82e7b5258793
Jules <julesmoreno66@gmail.com>
2024-07-22 15:23:34 +0800

    src/features/LoginForm/sections/InputSection.jsx: used InputLabelContainer component line 8

--- src/features/LoginForm/sections/InputSection.jsx
+++ src/features/LoginForm/sections/InputSection.jsx
@@ -1,11 +1,19 @@
+import InputLabelContainer from "@/components/componentContainers/InputLabelContainer";
 import ForgotPassword from "../components/ForgotPassword";
-import InputEmail from "../components/InputEmail";
 import InputPassword from "../components/InputPassword";
 
 export default function InputSection() {
   return (
     <section className="space-y-4">
-      <InputEmail />
+      <InputLabelContainer
+        label="Email address"
+        type="email"
+        name="email"
+        id="email"
+        dispatchType="UPDATE_EMAIL"
+        autoComplete="email"
+        placeholder="aquino@mail.com"
+      />
       <ForgotPassword />
       <InputPassword />
     </section>
