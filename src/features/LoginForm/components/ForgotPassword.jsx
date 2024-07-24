0d5c79fdef96aba6ca808bab4672dd3634d01a74
Jules <julesmoreno66@gmail.com>
2024-07-24 14:59:42 +0800

    src/features/LoginForm/components/ForgotPassword.jsx: add documentation/comment

--- src/features/LoginForm/components/ForgotPassword.jsx
+++ src/features/LoginForm/components/ForgotPassword.jsx
@@ -1,3 +1,8 @@
+/**
+ * This function renders a section with a label and a link for a password reset.
+ *
+ * @returns {JSX.Element} - A JSX element representing the password reset section.
+ */
 export default function ForgotPassword() {
   return (
     <section className="flex justify-between mb-2">
