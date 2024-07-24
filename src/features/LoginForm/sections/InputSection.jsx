62a6c29ab120801ee80cd964f718a583722be669
Jules <julesmoreno66@gmail.com>
2024-07-24 15:00:27 +0800

    jsrc/features/LoginForm/sections/InputSection.jsx: add documentation/comment

--- src/features/LoginForm/sections/InputSection.jsx
+++ src/features/LoginForm/sections/InputSection.jsx
@@ -2,6 +2,12 @@ import InputLabelContainer from "@/components/componentContainers/InputLabelCont
 import ForgotPassword from "../components/ForgotPassword";
 import InputPassword from "../components/InputPassword";
 
+/**
+ * This function represents a section containing various input components for a user login form.
+ * It includes an email input field, a forgot password link, and a password input field.
+ *
+ * @returns {JSX.Element} - A JSX element representing the input section.
+ */
 export default function InputSection() {
   return (
     <section className="space-y-4">
