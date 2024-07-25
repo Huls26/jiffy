29a690c37961b5dcd0c219581af07235b3b421f4
Jules <julesmoreno66@gmail.com>
2024-07-25 14:26:29 +0800

    src/components/buttons/__test__/SubmitFullBtn.test.jsx: test created

---
+++ src/components/buttons/__test__/SubmitFullBtn.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom"; // for the "toBeInTheDocument" matcher
+import SubmitFullButton from "@/components/SubmitFullButton";
+
+describe("SubmitFullButton Component", () => {
+  test("renders the button with the correct text", () => {
+    const buttonText = "Submit";
+
+    render(<SubmitFullButton text={buttonText} />);
+
+    const buttonElement = screen.getByRole("button", { name: buttonText });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveTextContent(buttonText);
+  });
+
+  test("has the correct classes applied", () => {
+    const buttonText = "Submit";
+
+    render(<SubmitFullButton text={buttonText} />);
+
+    const buttonElement = screen.getByRole("button", { name: buttonText });
+    expect(buttonElement).toHaveClass(
+      "w-full",
+      "px-8",
+      "py-3",
+      "font-semibold",
+      "rounded-md",
+      "dark:text-gray-50",
+      "dark:bg-blue-600",
+      "hover:opacity-70",
+      "active:opacity-55",
+    );
+  });
+
+  test("has type 'button'", () => {
+    const buttonText = "Submit";
+
+    render(<SubmitFullButton text={buttonText} />);
+
+    const buttonElement = screen.getByRole("button", { name: buttonText });
+    expect(buttonElement).toHaveAttribute("type", "button");
+  });
+});
