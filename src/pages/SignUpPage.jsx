5706a20b8520e1ea9670ded0f89c7ff6309f9b99
Jules <julesmoreno66@gmail.com>
2024-07-26 15:20:21 +0800

    src/pages/SignUpPage.jsx: update/fixed code

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,25 +1,19 @@
 import LoginSignUpContainer from "@/components/componentContainers/LoginSignUpContainer";
 import ReducerContextProvider from "@/contexts/ReducerContextProvider";
 import reducerMethod, {
-  INITIAL_STATE,
+	INITIAL_STATE,
 } from "@/contexts/SignUpPageContextReducer";
 import SignUpFeatures from "@/features/SignUpFeatures";
 
 export default function SignUpPage() {
-  return (
-    <LoginSignUpContainer
-      className="
-                  flex flex-col 
-                  m-auto mt-10 max-w-md p-10 rounded-md sm:p-10 
-                  dark:bg-gray-900 dark:text-gray-100 
-                "
-    >
-      <ReducerContextProvider
-        reducerMethod={reducerMethod}
-        INITIAL_STATE={INITIAL_STATE}
-      >
-        <SignUpFeatures />
-      </ReducerContextProvider>
-    </LoginSignUpContainer>
-  );
+	return (
+		<LoginSignUpContainer>
+			<ReducerContextProvider
+				reducerMethod={reducerMethod}
+				INITIAL_STATE={INITIAL_STATE}
+			>
+				<SignUpFeatures />
+			</ReducerContextProvider>
+		</LoginSignUpContainer>
+	);
 }
