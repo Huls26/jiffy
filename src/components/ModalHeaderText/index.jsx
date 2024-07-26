c50dfe4124d374d526e476debc8c19eb819b8c64
Jules <julesmoreno66@gmail.com>
2024-07-26 16:27:21 +0800

    src/components/ModalHeaderText/index.jsx: update style

--- src/components/ModalHeaderText/index.jsx
+++ src/components/ModalHeaderText/index.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
 export default function ModalHeaderText({ title, body }) {
-	return (
-		<header className="mb-5 sm500:mb-8 text-center cursor-not-allowed">
-			<h1 className="mt-0 text-xl font-bold sm500:text-3xl md:my-3 md:text-4xl">
-				{title}
-			</h1>
-			<p className="text-sm dark:text-gray-600">{body}</p>
-		</header>
-	);
+  return (
+    <header className="mb-5 sm500:mb-8 text-center cursor-not-allowed">
+      <h1 className="text-xl font-bold sm500:text-3xl md:my-3 md:text-4xl">
+        {title}
+      </h1>
+      <p className="text-sm dark:text-gray-600">{body}</p>
+    </header>
+  );
 }
 
 ModalHeaderText.propTypes = {
-	title: PropTypes.string.isRequired,
-	body: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
 };
