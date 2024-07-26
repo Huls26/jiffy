34d5339839d0a981929a6e8571f6092503278040
Jules <julesmoreno66@gmail.com>
2024-07-26 15:19:53 +0800

    src/pages/LoginPage.jsx: update/fixed code

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginSignUpContainer from "@/components/componentContainers/LoginSignUpCo
 import ReducerContextProvider from "@/contexts/ReducerContextProvider";
 import LoginForm from "@/features/LoginForm";
 import reducerMethod, {
-  INITIAL_STATE,
+	INITIAL_STATE,
 } from "@/features/LoginForm/context/LoginPageContextReducer";
 
 /**
@@ -12,24 +12,18 @@ import reducerMethod, {
  *
  * @returns {JSX.Element} - The JSX element for the login page.
  */ export default function LoginPage() {
-  return (
-    <LoginSignUpContainer
-      className="
-                  flex flex-col 
-                  m-auto mt-10 max-w-md p-10 rounded-md sm:p-10 
-                  dark:bg-gray-900 dark:text-gray-100 
-                "
-    >
-      <ReducerContextProvider
-        reducerMethod={reducerMethod}
-        INITIAL_STATE={INITIAL_STATE}
-      >
-        <ModalHeaderText
-          title={"Sign in"}
-          body={"Sign in to access your account"}
-        />
-        <LoginForm />
-      </ReducerContextProvider>
-    </LoginSignUpContainer>
-  );
+	return (
+		<LoginSignUpContainer>
+			<ReducerContextProvider
+				reducerMethod={reducerMethod}
+				INITIAL_STATE={INITIAL_STATE}
+			>
+				<ModalHeaderText
+					title={"Sign in"}
+					body={"Sign in to access your account"}
+				/>
+				<LoginForm />
+			</ReducerContextProvider>
+		</LoginSignUpContainer>
+	);
 }
