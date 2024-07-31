b90e0078be1ae2d7e715406b9b931a9d250dd135
Jules <julesmoreno66@gmail.com>
2024-07-31 14:55:01 +0800

    src/lib/fb.js: config firebase for firestore emulator line 16

--- src/lib/fb.js
+++ src/lib/fb.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FBK_KEY,
@@ -13,6 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+connectFirestoreEmulator(db, "127.0.0.1", 8080);
 
 export default app;
 export { db };
