e4fed2732c1d2f34011fe88d2cd681a7af807fe0
Jules <julesmoreno66@gmail.com>
2024-08-02 14:44:25 +0800

    src/pages/SignUpPage.jsx: add comment/document

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,19 +1,25 @@
 import LoginSignUpContainer from "@/components/componentContainers/LoginSignUpContainer";
 import ReducerContextProvider from "@/contexts/ReducerContextProvider";
 import reducerMethod, {
-	INITIAL_STATE,
+  INITIAL_STATE,
 } from "@/contexts/SignUpPageContextReducer";
 import SignUpFeatures from "@/features/SignUpFeatures";
 
+/**
+ * This function represents the SignUpPage component, which is a part of the application's user registration process.
+ * It renders the SignUpFeatures component within a LoginSignUpContainer, and provides the necessary context for the SignUp process.
+ *
+ * @returns {JSX.Element} - The SignUpPage component with its child components.
+ */
 export default function SignUpPage() {
-	return (
-		<LoginSignUpContainer>
-			<ReducerContextProvider
-				reducerMethod={reducerMethod}
-				INITIAL_STATE={INITIAL_STATE}
-			>
-				<SignUpFeatures />
-			</ReducerContextProvider>
-		</LoginSignUpContainer>
-	);
+  return (
+    <LoginSignUpContainer>
+      <ReducerContextProvider
+        reducerMethod={reducerMethod}
+        INITIAL_STATE={INITIAL_STATE}
+      >
+        <SignUpFeatures />
+      </ReducerContextProvider>
+    </LoginSignUpContainer>
+  );
 }
