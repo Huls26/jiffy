42f3632965e38698644d39b3a2ff6d380b04aa35
Jules <julesmoreno66@gmail.com>
2024-08-02 17:00:06 +0800

    src/main.jsx: add ContextProvider line 11

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import ContextProvider from "@/contexts/ContextProvider/index.jsx";
+import { FieldValue, app } from "@/lib/fb";
 
+console.log("update context value later for change field value");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider value={{ app, FieldValue }}>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
 );
