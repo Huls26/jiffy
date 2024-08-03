2e73c0cb81dac8c9200da74c223c4763d5283130
Jules <julesmoreno66@gmail.com>
2024-08-03 15:41:36 +0800

    src/main.jsx: change console.log()

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import ContextProvider from "@/contexts/ContextProvider/index.jsx";
 import { FieldValue, app } from "@/lib/fb";
 
-console.log("update context value later for change field value");
+console.log("update context value later, change FieldValue");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider value={{ app, FieldValue }}>
