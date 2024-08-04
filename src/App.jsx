d797267e830ab058c214c4464a189f07e0df4ee1
Jules <julesmoreno66@gmail.com>
2024-08-04 14:21:28 +0800

    src/App.jsx: add Suspence and fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Router from "@/router";
+import { Suspense } from "react";
 
 function App() {
-  return <Router />;
+  console.log("create loading component");
+  return (
+    <Suspense
+      fallback={
+        <h1 className="flex justify-center items-center h-svh text-3xl text-white">
+          ...Loading...
+        </h1>
+      }
+    >
+      <Router />
+    </Suspense>
+  );
 }
 
 export default App;
