4014c4632ee3fa883519ba6b5e18be12d6a200bc
Jules <julesmoreno66@gmail.com>
2024-08-05 14:34:27 +0800

    vitest.setup.js: added comment/documentation

--- vitest.setup.js
+++ vitest.setup.js
@@ -2,6 +2,18 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
+/**
+ * Cleans up the DOM after each test case in a React application using Vitest.
+ * This function is intended to be used as a part of the test setup.
+ * It ensures that each test case starts with a clean and isolated DOM environment.
+ *
+ * @returns {void} This function does not return any value.
+ *
+ * @example
+ * afterEach(() => {
+ *   cleanup();
+ * });
+ */
 afterEach(() => {
   cleanup();
 });
