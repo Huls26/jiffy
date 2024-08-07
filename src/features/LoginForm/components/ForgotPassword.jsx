c74e3315615cf7f5d2419efea27bbe9b659aa21e
Jules <julesmoreno66@gmail.com>
2024-08-07 16:15:47 +0800

    src/features/LoginForm/components/ForgotPassword.jsx: update Link line 14

--- src/features/LoginForm/components/ForgotPassword.jsx
+++ src/features/LoginForm/components/ForgotPassword.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 /**
  * This function renders a section with a label and a link for a password reset.
  *
@@ -9,13 +11,12 @@ export default function ForgotPassword() {
       <label htmlFor="password" className="text-sm">
         Password
       </label>
-      <a
-        rel="noopener noreferrer"
-        href="/"
+      <Link
+        to="/on-progrees"
         className="text-xs hover:underline dark:text-gray-600"
       >
         Forgot password?
-      </a>
+      </Link>
     </section>
   );
 }
