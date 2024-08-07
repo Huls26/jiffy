cacd05a760e77a2df2dc96f49eb935e34458f332
Jules <julesmoreno66@gmail.com>
2024-08-07 16:18:15 +0800

    src/pages/SignUpPage.jsx: add custom hooks that change page title line 20 & update comment/documentation

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,14 +4,21 @@ import reducerMethod, {
   INITIAL_STATE,
 } from "@/contexts/SignUpPageContextReducer";
 import SignUpFeatures from "@/features/SignUpFeatures";
+import usePageTitle from "@/hooks/usePageTitle";
 
 /**
  * This function represents the SignUpPage component, which is a part of the application's user registration process.
  * It renders the SignUpFeatures component within a LoginSignUpContainer, and provides the necessary context for the SignUp process.
  *
+ * @function SignUpPage
  * @returns {JSX.Element} - The SignUpPage component with its child components.
  */
 export default function SignUpPage() {
+  /**
+   * Sets the page title to "Signup".
+   */
+  usePageTitle("SignUp");
+
   return (
     <LoginSignUpContainer>
       <ReducerContextProvider
