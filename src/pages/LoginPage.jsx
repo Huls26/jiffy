d7d5c8ff894bd36f0063f9238aaa9e30c08b9d40
Jules <julesmoreno66@gmail.com>
2024-08-07 16:17:09 +0800

    src/pages/LoginPage.jsx: add custom hooks change page title line 21 & update comment/documentation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,13 +5,21 @@ import LoginForm from "@/features/LoginForm";
 import reducerMethod, {
   INITIAL_STATE,
 } from "@/features/LoginForm/context/LoginPageContextReducer";
+import usePageTitle from "@/hooks/usePageTitle";
 
 /**
  * This is the main component for the login page.
  * It renders a form for users to sign in.
  *
+ * @function LoginPage
  * @returns {JSX.Element} - The JSX element for the login page.
- */ export default function LoginPage() {
+ */
+export default function LoginPage() {
+  /**
+   * Sets the page title to "Login".
+   */
+  usePageTitle("Login");
+
   return (
     <LoginSignUpContainer>
       <ReducerContextProvider
