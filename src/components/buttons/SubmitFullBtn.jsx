69340c4a7ad2c981234b0ca11d9eef654eb42e59
Jules <julesmoreno66@gmail.com>
2024-08-09 14:19:12 +0800

    src/components/buttons/SubmitFullBtn.jsx: update style add valid and invalid style

--- src/components/buttons/SubmitFullBtn.jsx
+++ src/components/buttons/SubmitFullBtn.jsx
@@ -8,22 +8,30 @@ import PropTypes from "prop-types";
  * @param {string} props.text - The text to be displayed on the button.
  * @returns {JSX.Element} - A JSX element representing the button.
  */
-export default function SubmitFullButton({ text }) {
-  return (
-    <button
-      type="button"
-      className="
+export default function SubmitFullButton({ text, isInvalid = false }) {
+  // to be update in the future
+  const validStyle = `
         w-full px-8 py-3 
         font-semibold rounded-md 
         dark:text-gray-50 dark:bg-blue-600 
         hover:opacity-70 active:opacity-55
-      "
-    >
+      `;
+  const invalidStyle = `
+          w-full px-8 py-3 
+          font-semibold rounded-md 
+          dark:text-gray-50 dark:bg-blue-600 
+          opacity-70 cursor-no-drop
+        `;
+  const styles = !isInvalid ? validStyle : invalidStyle;
+
+  return (
+    <button disabled={isInvalid} type="button" className={styles}>
       {text}
     </button>
   );
 }
 
 SubmitFullButton.propTypes = {
+  isInvalid: PropTypes.bool,
   text: PropTypes.string.isRequired,
 };
