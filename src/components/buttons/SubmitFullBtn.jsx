ae87f0b9d403be9333f3c459c87481e2e6c9ac3a
Jules <julesmoreno66@gmail.com>
2024-08-10 14:14:13 +0800

    src/components/buttons/SubmitFullBtn.jsx: added aria-disbled & update style dynamics

--- src/components/buttons/SubmitFullBtn.jsx
+++ src/components/buttons/SubmitFullBtn.jsx
@@ -10,23 +10,25 @@ import PropTypes from "prop-types";
  * @returns {JSX.Element} - A JSX element representing the button.
  */
 export default function SubmitFullButton({ text, isInvalid = false }) {
-  // to be update in the future
-  const validStyle = `
-        w-full px-8 py-3 
-        font-semibold rounded-md 
-        dark:text-gray-50 dark:bg-blue-600 
-        hover:opacity-70 active:opacity-55
-      `;
-  const invalidStyle = `
-          w-full px-8 py-3 
-          font-semibold rounded-md 
-          dark:text-gray-50 dark:bg-blue-600 
-          opacity-70 cursor-no-drop
-        `;
-  const styles = !isInvalid ? validStyle : invalidStyle;
+  const baseStyle = `
+    w-full px-8 py-3 
+    font-semibold rounded-md 
+    dark:text-gray-50
+  `;
+
+  const dynamicStyle = isInvalid
+    ? "dark:bg-blue-600 opacity-70 cursor-not-allowed"
+    : "dark:bg-blue-600 hover:opacity-70 active:opacity-55";
+
+  const buttonClassNames = `${baseStyle} ${dynamicStyle}`;
 
   return (
-    <button disabled={isInvalid} type="button" className={styles}>
+    <button
+      disabled={isInvalid}
+      type="button"
+      className={buttonClassNames}
+      aria-disabled={isInvalid}
+    >
       {text}
     </button>
   );
