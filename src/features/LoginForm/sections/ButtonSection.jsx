1a010fbf23e60e8487d711294a374443b59a0cce
Jules <julesmoreno66@gmail.com>
2024-08-10 15:24:21 +0800

    src/features/LoginForm/sections/ButtonSection.jsx: add logic for valid input in button line 15 & line 19

--- src/features/LoginForm/sections/ButtonSection.jsx
+++ src/features/LoginForm/sections/ButtonSection.jsx
@@ -1,5 +1,7 @@
 import AuthPrompt from "@/components/AuthPrompt";
 import SubmitFullBtn from "@/components/buttons/SubmitFullBtn";
+import { reducerContext } from "@/contexts/ReducerContextProvider";
+import { useContext } from "react";
 
 /**
  * A functional component that renders a section with a sign-in button and an authentication prompt.
@@ -8,9 +10,13 @@ import SubmitFullBtn from "@/components/buttons/SubmitFullBtn";
  * @returns {JSX.Element} - A JSX element representing the button section.
  */
 export default function ButtonSection() {
+  const [loginInputState] = useContext(reducerContext);
+  const { email, password } = loginInputState;
+  const isValidInput = !(email && password);
+
   return (
     <section className="space-y-2">
-      <SubmitFullBtn text="Sign in" />
+      <SubmitFullBtn text="Sign in" isInvalid={isValidInput} />
       <AuthPrompt
         message="Don't have an account yet?"
         url="/signup"
