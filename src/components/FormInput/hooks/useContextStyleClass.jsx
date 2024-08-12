78a1f729f800f1f3e9c37cea7c6deddb16db7d7a
Jules <julesmoreno66@gmail.com>
2024-08-12 14:06:27 +0800

    src/components/FormInput/hooks/: created custom hooks

---
+++ src/components/FormInput/hooks/useContextStyleClass.jsx
@@ -0,0 +1,21 @@
+import { reducerContext } from "@/contexts/ReducerContextProvider";
+import { useContext } from "react";
+
+/**
+ * A function to generate a style class based on the context provided by the reducer.
+ *
+ * @function useContextStyleClass
+ * @param {string} defaultStyle - The default style class to be applied when there is no error.
+ * @param {string} errorStyle - The style class to be applied when there is an error.
+ * @returns {string} The combined style class based on the error state.
+ */
+export default function useContextStyleClass(defaultStyle, errorStyle) {
+  const [stateReducer] = useContext(reducerContext);
+  const inputStyleClasses = !stateReducer.isErrorAuth
+    ? defaultStyle
+    : `${defaultStyle} ${errorStyle}`;
+
+  console.log("write test");
+
+  return inputStyleClasses;
+}
