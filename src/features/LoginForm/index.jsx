d5fd66a5bb3f5392c0e741339e8a1dfe7d36e73f
Jules <julesmoreno66@gmail.com>
2024-08-14 15:02:28 +0800

    src/features/LoginForm/index.jsx: update hooks & comment

--- src/features/LoginForm/index.jsx
+++ src/features/LoginForm/index.jsx
@@ -1,43 +1,25 @@
-import { reducerContext } from "@/contexts/ReducerContextProvider";
 import ErrorMessage from "./components/ErrorMessage";
+import useHandleSubmitAndError from "./hooks/useHandleSubmitAndError";
 import ButtonSection from "./sections/ButtonSection";
 import InputSection from "./sections/InputSection";
-import loginUser from "./utils/loginUser";
-
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 
 /**
-​ * The main component for handling user login.
-​ *
-​ * @returns {JSX.Element} - The JSX element for the login form.
-​ */
+ * The main component for handling user login.
+ *
+ * @returns {JSX.Element} - The JSX element for the login form.
+ */
 export default function LoginForm() {
-  const [loginState, dispatch] = useContext(reducerContext);
-  const { email, password, isErrorAuth } = loginState;
-  const navigate = useNavigate();
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    loginUser(email, password, dispatch, navigate)
-      .then((res) => {
-        if (res) {
-          dispatch({ type: "UPDATE_VALIDAUTH" });
-          navigate("/");
-        }
-      })
-      .catch(() => {
-        console.error(
-          "Login failed. Please check your credentials and try again.",
-        );
-        dispatch({ type: "UPDATE_INVALIDAUTH" });
-      });
-  }
+  // Deconstruct the handleSubmit and isErrorAuth from the useHandleSubmitAndError hook
+  const { handleSubmit, isErrorAuth } = useHandleSubmitAndError();
 
   return (
+    // Render a form with the onSubmit event handler and space-y-12 class
     <form onSubmit={handleSubmit} className="space-y-12">
+      // Render the ErrorMessage component with the isError prop
       <ErrorMessage isError={isErrorAuth} />
+      // Render the InputSection component
       <InputSection />
+      // Render the ButtonSection component
       <ButtonSection />
     </form>
   );
