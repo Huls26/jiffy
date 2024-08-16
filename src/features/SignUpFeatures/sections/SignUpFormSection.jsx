5ff6ae3d449d3785b968dad244cebad0dc53a882
Jules <julesmoreno66@gmail.com>
2024-08-16 18:41:42 +0800

    src/features/SignUpFeatures/sections/SignUpFormSection.jsx: error message added line 14

--- src/features/SignUpFeatures/sections/SignUpFormSection.jsx
+++ src/features/SignUpFeatures/sections/SignUpFormSection.jsx
@@ -1,9 +1,17 @@
+import ErrorMessage from "@/components/ErrorMessage";
 import SignUpBtnSection from "./SignUpBtnSection";
 import SignUpInputSection from "./SignUpInputSection";
 
 export default function SignUpFormSection() {
+  console.log("display error when password and confirm password is not match");
+  function handleSignUp(event) {
+    event.preventDefault();
+
+    // Handle form submission
+  }
   return (
-    <form action="POST" className="space-y-12">
+    <form onSubmit={handleSignUp} className="space-y-12">
+      <ErrorMessage isError={true} message="Something went wrong Try again" />
       <SignUpInputSection />
       <SignUpBtnSection />
     </form>
