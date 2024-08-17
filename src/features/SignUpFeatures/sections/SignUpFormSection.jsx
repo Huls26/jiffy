8e6ba75ecefee1eb39eef94fd38f3434c44a821e
Jules <julesmoreno66@gmail.com>
2024-08-17 15:16:21 +0800

    src/features/SignUpFeatures/sections/SignUpFormSection.jsx: update components

--- src/features/SignUpFeatures/sections/SignUpFormSection.jsx
+++ src/features/SignUpFeatures/sections/SignUpFormSection.jsx
@@ -1,17 +1,27 @@
-import ErrorMessage from "@/components/ErrorMessage";
+import { reducerContext } from "@/contexts/ReducerContextProvider";
 import SignUpBtnSection from "./SignUpBtnSection";
 import SignUpInputSection from "./SignUpInputSection";
 
+import { useContext } from "react";
+
 export default function SignUpFormSection() {
-  console.log("display error when password and confirm password is not match");
+  const [signUpState, dispatch] = useContext(reducerContext);
+  const { password, confirmPassword } = signUpState;
+
   function handleSignUp(event) {
     event.preventDefault();
 
-    // Handle form submission
+    if (password !== confirmPassword) {
+      dispatch({
+        type: "UPDATE_ERROR",
+        isError: true,
+        message: "Check Password",
+      });
+    }
   }
+
   return (
     <form onSubmit={handleSignUp} className="space-y-12">
-      <ErrorMessage isError={true} message="Something went wrong Try again" />
       <SignUpInputSection />
       <SignUpBtnSection />
     </form>
