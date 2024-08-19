8756be29fb7a59e270b2d3d2a555fd37390948a0
Jules <julesmoreno66@gmail.com>
2024-08-19 17:20:15 +0800

    src/features/SignUpFeatures/hooks/useSignUpState.jsx: add comment/doc

--- src/features/SignUpFeatures/hooks/useSignUpState.jsx
+++ src/features/SignUpFeatures/hooks/useSignUpState.jsx
@@ -1,6 +1,18 @@
 import { reducerContext } from "@/contexts/ReducerContextProvider";
 import { useContext } from "react";
 
+/**
+ * A custom React hook that provides access to the sign-up state and dispatch function from the ReducerContextProvider.
+ *confirmed password entered by the user.
+ * @property {boolean} isErrorAuth - A flag indicating whether there is an authentication error.
+ * @property {string} errorMessage - The error message, if any, related to authentication.
+ * @returns {Object} An object containing the sign-up state and dispatch function.
+ * @property {Function} dispatch - The dispatch function for updating the sign-up state.
+ * @property {string} email - The email entered by the user.
+ * @property {string} username - The username entered by the user.
+ * @property {string} password - The password entered by the user.
+ * @property {string} confirmPassword - The
+ */
 export default function useSignUpState() {
   const [signUpState, dispatch] = useContext(reducerContext);
   const {
