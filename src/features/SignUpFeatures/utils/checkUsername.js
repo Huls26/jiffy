2a718255a60ae7578817b12f7e01c092d3e0b11d
Jules <julesmoreno66@gmail.com>
2024-08-22 15:28:46 +0800

    src/features/SignUpFeatures/utils/checkUsername.js: created

---
+++ src/features/SignUpFeatures/utils/checkUsername.js
@@ -0,0 +1,26 @@
+import { db } from "@/lib/fb";
+import { collection, getDocs, query, where } from "firebase/firestore";
+
+/**
+ * Asynchronously checks if the given username already exists in the database.
+ *
+ * @param {string} username - The username to check.
+ * @returns {Promise<number>} - A promise that resolves to the number of documents found with the given username.
+ */
+export async function checkUsername(username) {
+  try {
+    // Check if the username already exists in the database.
+    const usersRef = collection(db, "users");
+    const userDoc = query(usersRef, where("username", "==", username));
+
+    const querySnapshot = await getDocs(userDoc);
+
+    return querySnapshot.docs.length > 0;
+    // biome-ignore lint/correctness/noUnusedVariables: <explanation>
+  } catch (error) {
+    // biome-ignore lint/nursery/noConsole: <explanation>
+    console.log("Error checking username:");
+    // You can return a default value or rethrow the error based on your needs.
+    return true;
+  }
+}
