7cfcb60cf04742ceb16a987fc0072a3e64b10734
Jules <julesmoreno66@gmail.com>
2024-08-23 14:01:33 +0800

    src/pages/MainPage.jsx: added usePageTitle hooks line 4

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,8 @@
+import usePageTitle from "@/hooks/usePageTitle";
+
 export default function MainPage() {
+  usePageTitle("J I F F Y");
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline m-3 text-blue-300">
