9da26bed9c25682085df8b27b8a9ac0ee5a8ed87
Jules <julesmoreno66@gmail.com>
2024-08-23 13:59:39 +0800

    src/features/SignUpFeatures/sections/SignUpFormSection.jsx: update code

--- src/features/SignUpFeatures/sections/SignUpFormSection.jsx
+++ src/features/SignUpFeatures/sections/SignUpFormSection.jsx
@@ -1,8 +1,6 @@
-import { auth } from "@/lib/fb";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import {} from "firebase/firestore";
 import useSignUpState from "../hooks/useSignUpState";
 import { checkUsername } from "../utils/checkUsername";
+import createUser from "../utils/createUser";
 import SignUpBtnSection from "./SignUpBtnSection";
 import SignUpInputSection from "./SignUpInputSection";
 
@@ -26,52 +24,6 @@ export default function SignUpFormSection() {
   async function handleSignUp(event) {
     event.preventDefault();
 
-    function createUser(email, password) {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed up
-          const user = userCredential.user;
-
-          return user;
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          let errorMessage = error.message;
-
-          // Handle specific errors based on errorCode
-          switch (errorCode) {
-            case "auth/email-already-in-use":
-              errorMessage = "Email is already taken";
-              break;
-            case "auth/invalid-email":
-              errorMessage = "The email address is not valid.";
-              break;
-            case "auth/operation-not-allowed":
-              errorMessage = "Email/password accounts are not enabled.";
-              break;
-            case "auth/weak-password":
-              errorMessage = "The password is too weak.";
-              break;
-            default:
-              console.error("An unknown error occurred:", errorMessage);
-          }
-
-          console.error(errorMessage);
-          dispatch({
-            type: "UPDATE_ERROR",
-            isError: true,
-            message: errorMessage,
-          });
-        });
-      // .finally((u) => {
-      //   const auth = getAuth();
-      //   const user = auth.currentUser;
-
-      //   console.log(user);
-      //   console.log(u);
-      // });
-    }
-
     if (password !== confirmPassword) {
       dispatch({
         type: "UPDATE_ERROR",
@@ -91,12 +43,7 @@ export default function SignUpFormSection() {
         message: "Username is already taken",
       });
     } else {
-      createUser(email, password);
-      dispatch({
-        type: "UPDATE_ERROR",
-        isError: false,
-        message: "",
-      });
+      createUser(email, password, dispatch);
     }
   }
 
