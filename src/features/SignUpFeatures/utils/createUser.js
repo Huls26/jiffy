323ac624d470b476407a9b561f1eb2f26b8e6acf
Jules <julesmoreno66@gmail.com>
2024-08-23 15:08:14 +0800

    src/features/SignUpFeatures/utils/createUser.js: from .then to async/await & add updateProfile line 22

--- src/features/SignUpFeatures/utils/createUser.js
+++ src/features/SignUpFeatures/utils/createUser.js
@@ -1,5 +1,5 @@
 import { auth } from "@/lib/fb";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 /**
  * This function creates a new user in Firebase Authentication using the provided email and password.
@@ -12,50 +12,57 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
  * @returns {Promise<firebase.User|null>} - A Promise that resolves with the newly created user if successful,
  * or rejects with an error object if an error occurs.
  */
-export default async function createUser(email, password, dispatch) {
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed up
-      const user = userCredential.user;
-      dispatch({
-        type: "UPDATE_ERROR",
-        isError: false,
-        message: "",
-      });
-
-      return user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      let errorMessage = error.message;
-
-      // Handle specific errors based on errorCode
-      switch (errorCode) {
-        case "auth/email-already-in-use":
-          errorMessage = "Email is already taken";
-          break;
-        case "auth/invalid-email":
-          errorMessage = "The email address is not valid.";
-          break;
-        case "auth/missing-email":
-          errorMessage = "Please enter your email address.";
-          break;
-        case "auth/operation-not-allowed":
-          errorMessage = "Email/password accounts are not enabled.";
-          break;
-        case "auth/weak-password":
-          errorMessage = "The password is too weak.";
-          break;
-        default:
-          // biome-ignore lint/nursery/noConsole: <explanation>
-          console.error("An unknown error occurred:", errorMessage);
-      }
-
-      // Optional: Log a more concise error message
-      dispatch({
-        type: "UPDATE_ERROR",
-        isError: true,
-        message: errorMessage,
-      });
+export default async function createUser(email, username, password, dispatch) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: username,
     });
+
+    dispatch({
+      type: "UPDATE_ERROR",
+      isError: false,
+      message: "",
+    });
+
+    return userCredential;
+  } catch (error) {
+    const errorCode = error.code;
+    let errorMessage = error.message;
+
+    // Handle specific errors based on errorCode
+    switch (errorCode) {
+      case "auth/email-already-in-use":
+        errorMessage = "Email is already taken";
+        break;
+      case "auth/invalid-email":
+        errorMessage = "The email address is not valid.";
+        break;
+      case "auth/missing-email":
+        errorMessage = "Please enter your email address.";
+        break;
+      case "auth/operation-not-allowed":
+        errorMessage = "Email/password accounts are not enabled.";
+        break;
+      case "auth/weak-password":
+        errorMessage = "The password is too weak.";
+        break;
+      default:
+        // biome-ignore lint/nursery/noConsole: <explanation>
+        console.error("An unknown error occurred:", errorMessage);
+    }
+
+    // biome-ignore lint/nursery/noConsole: <explanation>
+    console.error(errorMessage);
+    // Optional: Log a more concise error message
+    dispatch({
+      type: "UPDATE_ERROR",
+      isError: true,
+      message: errorMessage,
+    });
+  }
 }
