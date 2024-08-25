ea26822542ffd288cc01d267e840c642c007ebfc
Jules <julesmoreno66@gmail.com>
2024-08-25 16:01:46 +0800

    src/features/SignUpFeatures/utils/createUserFirestore.js: created

---
+++ src/features/SignUpFeatures/utils/createUserFirestore.js
@@ -0,0 +1,31 @@
+import { db } from "@/lib/fb";
+import { doc, setDoc } from "firebase/firestore";
+
+export default async function createUserFirestore({
+  dispatch,
+  isError,
+  uid,
+  email,
+  username,
+  fullName,
+  password,
+}) {
+  if (!isError) {
+    try {
+      await setDoc(doc(db, "users", uid), {
+        email,
+        username,
+        fullName,
+        password,
+      });
+    } catch (err) {
+      // biome-ignore lint/nursery/noConsole: <explanation>
+      console.log("Error adding document: ", err.message);
+      dispatch({
+        type: "UPDATE_ERROR",
+        isError: true,
+        message: "Oops! something went wrong",
+      });
+    }
+  }
+}
