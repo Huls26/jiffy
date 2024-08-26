fd673839669e0481df2fa813c93facaba97168bd
Jules <julesmoreno66@gmail.com>
2024-08-26 13:25:40 +0800

    src/features/SignUpFeatures/index.jsx: added loading component

--- src/features/SignUpFeatures/index.jsx
+++ src/features/SignUpFeatures/index.jsx
@@ -1,7 +1,15 @@
 import ModalHeaderText from "@/components/ModalHeaderText";
 import SignUpFormSection from "@/features/SignUpFeatures/sections/SignUpFormSection";
+import useSignUpState from "./hooks/useSignUpState";
 
 export default function SignUpFeatures() {
+  const { isLoading } = useSignUpState();
+
+  console.log("update loading components");
+  if (isLoading) {
+    return <h1>...Loading</h1>; // Show a loading component while waiting for the API response.
+  }
+
   return (
     <>
       <ModalHeaderText
