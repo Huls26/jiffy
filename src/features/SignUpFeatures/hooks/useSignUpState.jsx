ce2ec720bff11f0980ddb6c331a15f6600d06830
Jules <julesmoreno66@gmail.com>
2024-08-26 13:23:41 +0800

    src/features/SignUpFeatures/hooks/useSignUpState.jsx: updated comment/doc

--- src/features/SignUpFeatures/hooks/useSignUpState.jsx
+++ src/features/SignUpFeatures/hooks/useSignUpState.jsx
@@ -5,16 +5,17 @@ import { useContext } from "react";
  * A custom React hook that provides access to the sign-up state and dispatch function from the ReducerContextProvider.
  * This hook is used to manage the sign-up process and handle user input.
  *
- * @property {boolean} isErrorAuth - A flag indicating whether there is an authentication error.
- * @property {string} errorMessage - The error message, if any, related to authentication.
- *
  * @returns {Object} An object containing the sign-up state and dispatch function.
- * @property {Function} dispatch - The dispatch function for updating the sign-up state.
- * @property {string} email - The email entered by the user.
- * @property {string} username - The username entered by the user.
- * @property {string} fullName - The full name entered by the user.
- * @property {string} password - The password entered by the user.
- * @property {string} confirmPassword - The confirmed password entered by the user.
+ * @property {Function} dispatch - The dispatch function for updating the
+ * @property {Object} signUpState - The state object containing the sign-up data.
+ * @property {string} signUpState.email - The email entered by the user.
+ * @property {string} signUpState.username - The username entered by the user.
+ * @property {string} signUpState.fullName - The full name entered by the user.
+ * @property {string} signUpState.password - The password entered by the user.
+ * @property {string} signUpState.confirmPassword - The confirmed password entered by the user.
+ * @property {boolean} signUpState.isErrorAuth - A flag indicating whether there is an authentication error.
+ * @property {string} signUpState.errorMessage - The error message, if any, related to authentication.
+ * @property {boolean} signUpState.isLoading - A flag indicating whether the sign-up process is currently in progress.
  */
 export default function useSignUpState() {
   const [signUpState, dispatch] = useContext(reducerContext);
