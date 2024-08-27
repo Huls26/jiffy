d431b50407e5ef2bf95133b1e1bcd0e1890e34ed
Jules <julesmoreno66@gmail.com>
2024-08-27 16:23:31 +0800

    src/components/LoadingSkeleton/components/InputSkeleton.jsx: created

---
+++ src/components/LoadingSkeleton/components/InputSkeleton.jsx
@@ -0,0 +1,8 @@
+export default function InputSkeleton() {
+  return (
+    <>
+      <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2.5" />
+      <div className="h-10 bg-gray-200 rounded-xl dark:bg-gray-700 w-full mb-4" />
+    </>
+  );
+}
