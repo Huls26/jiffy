b172f43019193d074381907f00247734a575c17b
Jules <julesmoreno66@gmail.com>
2024-08-27 16:38:04 +0800

    src/features/LoginFeatures/index.jsx: update import use code-splitting

--- src/features/LoginFeatures/index.jsx
+++ src/features/LoginFeatures/index.jsx
@@ -1,11 +1,21 @@
-import LoadingSkeleton from "@/components/LoadingSkeleton";
-import InputSkeleton from "@/components/LoadingSkeleton/components/InputSkeleton";
-import SubmitBtnSkeleton from "@/components/LoadingSkeleton/components/SubmitBtnSkeleton";
 import ModalHeaderText from "@/components/ModalHeaderText";
 
 import { Suspense, lazy } from "react";
 const LoginForm = lazy(() => import("@/features/LoginForm"));
+const LoadingSkeleton = lazy(() => import("@/components/LoadingSkeleton"));
+const InputSkeleton = lazy(
+  () => import("@/components/LoadingSkeleton/components/InputSkeleton"),
+);
+const SubmitBtnSkeleton = lazy(
+  () => import("@/components/LoadingSkeleton/components/SubmitBtnSkeleton"),
+);
 
+/**
+ * This function renders the login features, including a loading skeleton,
+ * modal header text, and the lazy-loaded login form.
+ *
+ * @returns {JSX.Element} - The JSX element representing the login features.
+ */
 export default function LoginFeatures() {
   return (
     <Suspense
