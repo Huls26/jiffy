25d64cb5ce0eceb6db6cbeeb517c3e81b2190df5
Jules <julesmoreno66@gmail.com>
2024-08-27 16:03:46 +0800

    src/features/SignUpFeatures/index.jsx: removed loading component

--- src/features/SignUpFeatures/index.jsx
+++ src/features/SignUpFeatures/index.jsx
@@ -1,15 +1,7 @@
 import ModalHeaderText from "@/components/ModalHeaderText";
 import SignUpFormSection from "@/features/SignUpFeatures/sections/SignUpFormSection";
-import useSignUpState from "./hooks/useSignUpState";
 
 export default function SignUpFeatures() {
-  const { isLoading } = useSignUpState();
-
-  console.log("update loading components");
-  if (isLoading) {
-    return <h1>...Loading</h1>; // Show a loading component while waiting for the API response.
-  }
-
   return (
     <>
       <ModalHeaderText
