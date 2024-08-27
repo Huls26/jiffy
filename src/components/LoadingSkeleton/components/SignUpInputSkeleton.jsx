4f8eae61319c27b0ff4bb966da57defd01f03146
Jules <julesmoreno66@gmail.com>
2024-08-27 16:20:23 +0800

    src/components/LoadingSkeleton/components/SignUpInputSkeleton.jsx: removed

--- src/components/LoadingSkeleton/components/SignUpInputSkeleton.jsx
+++
@@ -1,8 +0,0 @@
-export default function SignUpInputSkeleton() {
-  return (
-    <>
-      <div className="h-5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2.5" />
-      <div className="h-10 bg-gray-200 rounded-xl dark:bg-gray-700 w-full mb-4" />
-    </>
-  );
-}
