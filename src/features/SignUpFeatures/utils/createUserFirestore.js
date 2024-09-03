4c280eb379464c7476bff9cfb58457740af5f8fa
Jules <julesmoreno66@gmail.com>
2024-09-03 14:09:34 +0800

    src/features/SignUpFeatures/utils/createUserFirestore.js: add dataCreated line 21

--- src/features/SignUpFeatures/utils/createUserFirestore.js
+++ src/features/SignUpFeatures/utils/createUserFirestore.js
@@ -18,6 +18,7 @@ export default async function createUserFirestore({
         username,
         fullName,
         password,
+        dateCreated: Date.now(),
       });
     } catch (err) {
       // biome-ignore lint/nursery/noConsole: <explanation>
