ba679520c708860dfe82777623bb94635d209eae
Jules <julesmoreno66@gmail.com>
2024-09-05 15:47:16 +0800

    src/App.jsx: ReducerContext added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Router from "@/router";
 import { Suspense } from "react";
+import reducerMethod, {
+  INITIAL_STATE,
+} from "./contexts/AppGlobalContextReducer";
+import ReducerContextProvider from "./contexts/ReducerContextProvider";
 
 function App() {
   console.log("create loading component");
@@ -11,7 +15,12 @@ function App() {
         </h1>
       }
     >
-      <Router />
+      <ReducerContextProvider
+        reducerMethod={reducerMethod}
+        INITIAL_STATE={INITIAL_STATE}
+      >
+        <Router />
+      </ReducerContextProvider>
     </Suspense>
   );
 }
