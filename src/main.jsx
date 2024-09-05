fb5524724df1cba0991ddc84bd9adc77de4f3a19
Jules <julesmoreno66@gmail.com>
2024-09-05 15:48:22 +0800

    src/main.jsx: removed context provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ContextProvider from "@/contexts/ContextProvider/index.jsx";
-import { FieldValue, app } from "@/lib/fb";
 
-console.log("update context value later, change FieldValue");
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ContextProvider value={{ app, FieldValue }}>
-      <App />
-    </ContextProvider>
+    <App />
   </React.StrictMode>,
 );
 
