3d660d5b58b38bdc5203b9a7f404418e66349629
Jules <julesmoreno66@gmail.com>
2024-09-05 15:48:57 +0800

    src/pages/MainPage.jsx: context added testing context

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,15 @@
 import MainPageHeader from "@/features/MainPageFeatures/sections/MainPageHeader";
 import MainPageTimeline from "@/features/MainPageFeatures/sections/MainPageTimeline";
 import usePageTitle from "@/hooks/usePageTitle";
+import { useContext } from "react";
+import { reducerContext } from "../contexts/ReducerContextProvider";
 import MainPageSidebar from "../features/MainPageFeatures/sections/MainPageSidebar";
 
 export default function MainPage() {
   usePageTitle("J I F F Y");
+  const context = useContext(reducerContext);
+
+  console.log(context);
 
   // highlight dark:bg-gray-900
   return (
