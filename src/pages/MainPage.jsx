e9ec44568673780805622b3c6f48face6bdb351a
Jules <julesmoreno66@gmail.com>
2024-09-06 16:16:09 +0800

    src/pages/MainPage.jsx: useAuthListener() added line 12

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,17 @@
 import MainPageHeader from "@/features/MainPageFeatures/sections/MainPageHeader";
 import MainPageTimeline from "@/features/MainPageFeatures/sections/MainPageTimeline";
+import useAuthListener from "@/hooks/useAuthListener";
 import usePageTitle from "@/hooks/usePageTitle";
-import { useContext } from "react";
-import { reducerContext } from "../contexts/ReducerContextProvider";
+// import { useContext } from "react";
+// import { reducerContext } from "../contexts/ReducerContextProvider";
 import MainPageSidebar from "../features/MainPageFeatures/sections/MainPageSidebar";
 
 export default function MainPage() {
   usePageTitle("J I F F Y");
-  const context = useContext(reducerContext);
+  // const context = useContext(reducerContext);
+  const globalStateContext = useAuthListener();
 
-  console.log(context);
+  console.log(globalStateContext);
 
   // highlight dark:bg-gray-900
   return (
