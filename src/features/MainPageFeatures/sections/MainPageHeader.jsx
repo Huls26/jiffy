9dcb905ddd84bdb6a2bb0dca4cf990d74754f42b
Jules <julesmoreno66@gmail.com>
2024-09-06 16:14:31 +0800

    src/features/MainPageFeatures/sections/MainPageHeader.jsx: globalContext added

--- src/features/MainPageFeatures/sections/MainPageHeader.jsx
+++ src/features/MainPageFeatures/sections/MainPageHeader.jsx
@@ -1,3 +1,10 @@
+import { reducerContext } from "@/contexts/ReducerContextProvider";
+import { useContext } from "react";
+
 export default function MainPageHeader() {
+  const [globalStateContext] = useContext(reducerContext);
+
+  console.log(globalStateContext);
+
   return <div>MainPageHeader</div>;
 }
