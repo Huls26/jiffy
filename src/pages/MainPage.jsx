42b92d415a15e031fb3394b8bb7711204def2072
Jules <julesmoreno66@gmail.com>
2024-09-06 16:21:10 +0800

    src/pages/MainPage.jsx: removed some code

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,11 @@
 import MainPageHeader from "@/features/MainPageFeatures/sections/MainPageHeader";
+import MainPageSidebar from "@/features/MainPageFeatures/sections/MainPageSidebar";
 import MainPageTimeline from "@/features/MainPageFeatures/sections/MainPageTimeline";
 import useAuthListener from "@/hooks/useAuthListener";
 import usePageTitle from "@/hooks/usePageTitle";
-// import { useContext } from "react";
-// import { reducerContext } from "../contexts/ReducerContextProvider";
-import MainPageSidebar from "../features/MainPageFeatures/sections/MainPageSidebar";
 
 export default function MainPage() {
   usePageTitle("J I F F Y");
-  // const context = useContext(reducerContext);
   const globalStateContext = useAuthListener();
 
   console.log(globalStateContext);
