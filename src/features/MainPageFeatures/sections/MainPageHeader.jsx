e5a57992da7fc812b82aeb52b86ecb3078ae2d6e
Jules <julesmoreno66@gmail.com>
2024-09-06 16:20:26 +0800

    src/features/MainPageFeatures/sections/MainPageHeader.jsx: add to do list

--- src/features/MainPageFeatures/sections/MainPageHeader.jsx
+++ src/features/MainPageFeatures/sections/MainPageHeader.jsx
@@ -4,6 +4,10 @@ import { useContext } from "react";
 export default function MainPageHeader() {
   const [globalStateContext] = useContext(reducerContext);
 
+  // to do
+  // add navigation
+  // add logout button
+  // display username
   console.log(globalStateContext);
 
   return <div>MainPageHeader</div>;
