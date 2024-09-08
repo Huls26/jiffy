2112919090e287f492ab20b9d9b9db6c158aeedb
Jules <julesmoreno66@gmail.com>
2024-09-08 16:06:01 +0800

    src/components/Header/MainHeader/index.jsx: add text underline

--- src/components/Header/MainHeader/index.jsx
+++ src/components/Header/MainHeader/index.jsx
@@ -6,8 +6,9 @@ export default function MainHeader() {
   const [globalStateContext] = useContext(reducerContext);
 
   // to do
-  // add navigation
+  // active link
   // add logout button
+  // login button when logged out
   // display username
   console.log(globalStateContext);
 
@@ -20,17 +21,26 @@ export default function MainHeader() {
         <nav>
           <ul className="flex space-x-4">
             <li>
-              <Link to="/" className="hover:text-gray-200">
+              <Link
+                to="/"
+                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
+              >
                 Home
               </Link>
             </li>
             <li>
-              <Link to="/profile" className="hover:text-gray-200">
+              <Link
+                to="/profile"
+                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
+              >
                 Profile
               </Link>
             </li>
             <li>
-              <Link to="/login" className="hover:text-gray-200">
+              <Link
+                to="/login"
+                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
+              >
                 Login
               </Link>
             </li>
