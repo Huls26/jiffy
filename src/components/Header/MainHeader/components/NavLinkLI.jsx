70990da17ef8c1c050bce9d6241eb1e818ebed00
Jules <julesmoreno66@gmail.com>
2024-09-08 16:37:21 +0800

    src/components/Header/MainHeader/components/NavLinkLI.jsx: created

---
+++ src/components/Header/MainHeader/components/NavLinkLI.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+/**
+ * A React functional component that renders a list item containing a navigation link.
+ * The link's style changes based on whether it is active or not.
+ *
+ * @param {Object} props - The component's props.
+ * @param {string} props.text - The text to display for the navigation link.
+ * @param {string} props.to - The URL to navigate to when the link is clicked.
+ *
+ * @returns {JSX.Element} - A React functional component that renders a list item with a navigation link.
+ */
+export default function NavLinkLI({ text, to }) {
+  const defaultStyle =
+    "pb-1 border-gray-600 hover:border-b-2 hover:text-gray-400 transition";
+  const activeStyle =
+    "pb-1 border-gray-400 border-b-2 hover:text-gray-400 hover:border-gray-600 transition";
+
+  return (
+    <li>
+      <NavLink
+        to={to}
+        className={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+      >
+        {text}
+      </NavLink>
+    </li>
+  );
+}
+
+NavLinkLI.propTypes = {
+  text: PropTypes.string.isRequired,
+  to: PropTypes.string.isRequired,
+};
