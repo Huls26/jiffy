f6df67ef2be95713001752b84fdeb84cf8198f36
Jules <julesmoreno66@gmail.com>
2024-09-08 16:36:33 +0800

    src/components/Header/MainHeader/index.jsx: added NavLinkLi line 23

--- src/components/Header/MainHeader/index.jsx
+++ src/components/Header/MainHeader/index.jsx
@@ -1,49 +1,28 @@
 import { reducerContext } from "@/contexts/ReducerContextProvider";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
+import NavLinkLI from "./components/NavLinkLI";
 
 export default function MainHeader() {
   const [globalStateContext] = useContext(reducerContext);
 
   // to do
-  // active link
   // add logout button
   // login button when logged out
   // display username
   console.log(globalStateContext);
 
   return (
-    <header className="bg-gray-900 text-white p-4">
+    <header className="bg-gray-900 text-gray-50 p-4">
       <div className="container mx-auto flex justify-between items-center md:max-w-5xl">
         <h1 className="text-2xl font-bold cursor-pointer">
           <Link to="/">JIFFY</Link>
         </h1>
         <nav>
           <ul className="flex space-x-4">
-            <li>
-              <Link
-                to="/"
-                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/profile"
-                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
-              >
-                Profile
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/login"
-                className="pb-1 border-gray-400 hover:text-gray-200 hover:border-b-2 transition"
-              >
-                Login
-              </Link>
-            </li>
+            <NavLinkLI text="Home" to="/" />
+            <NavLinkLI text="Profile" to="profile" />
+            <NavLinkLI text="Login" to="login" />
           </ul>
         </nav>
       </div>
