a150246998c29a7267f20f9d8d8a9d225301ffd0
Jules <julesmoreno66@gmail.com>
2024-09-09 16:42:45 +0800

    src/components/Header/MainHeader/index.jsx: comment/docString added

--- src/components/Header/MainHeader/index.jsx
+++ src/components/Header/MainHeader/index.jsx
@@ -2,26 +2,31 @@ import { Link } from "react-router-dom";
 import AuthNavLink from "./components/AuthNavLink";
 import NavLinkLI from "./components/NavLinkLI";
 
+/**
+ * MainHeader is a React functional component that renders the main header of the application.
+ * It includes a logo, navigation links, and user authentication features.
+ *
+ * @returns {JSX.Element} - The JSX representation of the main header.
+ */
 export default function MainHeader() {
-  // to do
-  // add logout button
-  // login button when logged out
-  // display username
+	// to do
+	// add logout button
+	// display username
 
-  return (
-    <header className="bg-gray-900 text-gray-50 p-4">
-      <div className="container mx-auto flex justify-between items-center md:max-w-5xl">
-        <h1 className="text-2xl font-bold cursor-pointer">
-          <Link to="/">JIFFY</Link>
-        </h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <NavLinkLI text="Home" to="/" />
-            <NavLinkLI text="Profile" to="profile" />
-            <AuthNavLink />
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+	return (
+		<header className="bg-gray-900 text-gray-50 p-4">
+			<div className="container mx-auto flex justify-between items-center md:max-w-5xl">
+				<h1 className="text-2xl font-bold cursor-pointer">
+					<Link to="/">JIFFY</Link>
+				</h1>
+				<nav>
+					<ul className="flex space-x-4">
+						<NavLinkLI text="Home" to="/" />
+						<NavLinkLI text="Profile" to="profile" />
+						<AuthNavLink />
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 }
