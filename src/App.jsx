46993b6dbc785ca053ed72e93cfd959133d78f1e
Jules <julesmoreno66@gmail.com>
2024-09-09 16:01:24 +0800

    to be updated later

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
 import Router from "@/router";
 import { Suspense } from "react";
 import reducerMethod, {
-  INITIAL_STATE,
+	INITIAL_STATE,
 } from "./contexts/AppGlobalContextReducer";
 import ReducerContextProvider from "./contexts/ReducerContextProvider";
 
 function App() {
-  console.log("create loading component");
-  return (
-    <Suspense
-      fallback={
-        <h1 className="flex justify-center items-center h-svh text-3xl text-white">
-          ...Loading...
-        </h1>
-      }
-    >
-      <ReducerContextProvider
-        reducerMethod={reducerMethod}
-        INITIAL_STATE={INITIAL_STATE}
-      >
-        <Router />
-      </ReducerContextProvider>
-    </Suspense>
-  );
+	console.log("create loading component");
+
+	return (
+		<Suspense
+			fallback={
+				<h1 className="flex justify-center items-center h-svh text-3xl text-white">
+					...Loading...
+				</h1>
+			}
+		>
+			<ReducerContextProvider
+				reducerMethod={reducerMethod}
+				INITIAL_STATE={INITIAL_STATE}
+			>
+				<Router />
+			</ReducerContextProvider>
+		</Suspense>
+	);
 }
 
 export default App;
