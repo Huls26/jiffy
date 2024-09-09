db376a132feba98ae2eca3515fa7f84646171195
Jules <julesmoreno66@gmail.com>
2024-09-09 16:04:04 +0800

    src/pages/MainPage.jsx: removed auth listener

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,9 @@
 import MainPageSidebar from "@/features/MainPageFeatures/sections/MainPageSidebar";
 import MainPageTimeline from "@/features/MainPageFeatures/sections/MainPageTimeline";
-import useAuthListener from "@/hooks/useAuthListener";
 import usePageTitle from "@/hooks/usePageTitle";
 
 export default function MainPage() {
   usePageTitle("J I F F Y");
-  const globalStateContext = useAuthListener();
-
-  console.log(globalStateContext);
 
   // highlight dark:bg-gray-900
   return (
