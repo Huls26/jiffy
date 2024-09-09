68832890b04dd7fd86731ae6b74c152923f3952a
Jules <julesmoreno66@gmail.com>
2024-09-09 17:02:17 +0800

    src/components/Header/MainHeader/components/AuthNavLink.jsx: added isLoadin condition & comment/docString

--- src/components/Header/MainHeader/components/AuthNavLink.jsx
+++ src/components/Header/MainHeader/components/AuthNavLink.jsx
@@ -1,14 +1,24 @@
 import useAuthListener from "@/hooks/useAuthListener";
+import LogoutButton from "./LogoutButton";
 import NavLinkLI from "./NavLinkLI";
 
+/**
+ * AuthNavLink is a React functional component that renders a navigation link based on the user's authentication status.
+ * It uses the `useAuthListener` hook to listen for changes in the user's login status.
+ *
+ * @returns {JSX.Element} - A JSX element representing the navigation link.
+ */
 export default function AuthNavLink() {
-  const globalStateContext = useAuthListener();
-  const { userLogin } = globalStateContext;
+	const globalStateContext = useAuthListener();
+	const { userLogin, isLoading } = globalStateContext;
 
-  console.log("add loading when checking userLogin");
-  return !userLogin ? (
-    <NavLinkLI text="<Login />" to="login" />
-  ) : (
-    <NavLinkLI text="<Logout />" to="logout" />
-  );
+	if (isLoading) {
+		return <h1>{"< ... />"}</h1>;
+	}
+
+	return !userLogin ? (
+		<NavLinkLI text="<Login />" to="login" />
+	) : (
+		<LogoutButton />
+	);
 }
