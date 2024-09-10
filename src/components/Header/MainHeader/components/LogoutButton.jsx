0b400e8ec7f5bf702b93e6955a9c4bc945d50b89
Jules <julesmoreno66@gmail.com>
2024-09-10 16:15:06 +0800

    src/components/Header/MainHeader/components/LogoutButton.jsx: added comment & docstring

--- src/components/Header/MainHeader/components/LogoutButton.jsx
+++ src/components/Header/MainHeader/components/LogoutButton.jsx
@@ -2,37 +2,47 @@ import { reducerContext } from "@/contexts/ReducerContextProvider";
 import { getAuth, signOut } from "firebase/auth";
 import { useContext } from "react";
 
+/**
+ * A React functional component that renders a logout button.
+ * It uses the Firebase Authentication SDK to sign out the user and updates the global state.
+ *
+ * @returns {JSX.Element} - A logout button component.
+ */
 export default function LogoutButton() {
-	const [globalStateContext, dispatch] = useContext(reducerContext);
+  const [globalStateContext, dispatch] = useContext(reducerContext);
 
-	const handleLogout = () => {
-		const auth = getAuth();
-		signOut(auth)
-			.then(() => {
-				// Update the global state to reflect the user is logged out
-				dispatch({
-					type: "UPDATE_USERLOGIN",
-					isUserLoggedIn: false,
-					email: null,
-					username: null,
-					fullName: null,
-				});
-				dispatch({ type: "UPDATE_LOADING", isLoading: false });
-			})
-			.catch((error) => {
-				// Handle any errors during sign out
-				console.error("Error signing out: ", error);
-				dispatch({ type: "UPDATE_LOADING", isLoading: false });
-			});
-	};
+  /**
+   * Handles the logout process by signing out the user using Firebase Authentication.
+   * Updates the global state to reflect the user's logout status.
+   */
+  const handleLogout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Update the global state to reflect the user is logged out
+        dispatch({
+          type: "UPDATE_USERLOGIN",
+          isUserLoggedIn: false,
+          email: null,
+          username: null,
+          fullName: null,
+        });
+        dispatch({ type: "UPDATE_LOADING", isLoading: false });
+      })
+      .catch((error) => {
+        // Handle any errors during sign out
+        console.error("Error signing out: ", error);
+        dispatch({ type: "UPDATE_LOADING", isLoading: false });
+      });
+  };
 
-	return (
-		<button
-			type="button"
-			onClick={handleLogout}
-			disabled={globalStateContext.isLoading}
-		>
-			{"<Logout />"}
-		</button>
-	);
+  return (
+    <button
+      type="button"
+      onClick={handleLogout}
+      disabled={globalStateContext.isLoading}
+    >
+      {"<Logout />"}
+    </button>
+  );
 }
