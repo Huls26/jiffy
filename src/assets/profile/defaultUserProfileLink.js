0f785148a8bbe398ba2386b14f15cf49db5ded84
Jules <julesmoreno66@gmail.com>
2024-09-14 16:21:53 +0800

    src/assets/profile/defaultUserProfileLink.js: added

--- src/assets/profile/defaultUserProfileLink.js
+++ src/assets/profile/defaultUserProfileLink.js
@@ -2,5 +2,7 @@ export default function defaultProfile() {
   return {
     image:
       "https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg",
+    firebaseUrl:
+      "gs://jiffy-ver-2.appspot.com/assets/defaultProfile/default-user-profile.webp",
   };
 }
