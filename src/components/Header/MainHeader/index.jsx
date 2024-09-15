6adeeae4fecb335cfa7ef4ad2f0315152661b4f2
Jules <julesmoreno66@gmail.com>
2024-09-15 16:11:25 +0800

    src/components/Header/MainHeader/index.jsx: update ul layout and logo font-size

--- src/components/Header/MainHeader/index.jsx
+++ src/components/Header/MainHeader/index.jsx
@@ -14,13 +14,13 @@ export default function MainHeader() {
   return (
     <header className="bg-gray-900 text-gray-50 p-4">
       <div className="container mx-auto flex justify-between items-center md:max-w-5xl">
-        <h1 className="text-xl font-bold cursor-pointer">
+        <h1 className="text-lg font-bold cursor-pointer">
           <Link to="/" aria-label="jiffy simple logo">
             JIFFY
           </Link>
         </h1>
         <nav>
-          <ul className="flex space-x-2 md:space-x-4">
+          <ul className="flex items-center space-x-2 md:space-x-4 select-none">
             {/* removed for now tobe decided on */}
             {/* <NavLinkLI text="Home" to="/" /> */}
             <HeaderHomeButton />
