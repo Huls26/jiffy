08ae261486d6d159c156975dfee340cecdbd670f
Jules <julesmoreno66@gmail.com>
2024-09-15 16:21:07 +0800

    src/pages/MainPage.jsx: update something testing firestorage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,7 +11,14 @@ export default function MainPage() {
 
   return (
     <main className="text-white text-center">
-      {globalState.userLogin && <img src="" />}
+      {globalState.userLogin && (
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/jiffy-ver-2.appspot.com/o/assets%2FdefaultProfile%2Fdefault-user-profile.webp?alt=media&token=a696e916-7833-484a-b2a2-d339734cbc80"
+          width="100"
+          height="100"
+          className="block mx-auto"
+        />
+      )}
       <MainPageTimeline />
       <MainPageSidebar />
     </main>
