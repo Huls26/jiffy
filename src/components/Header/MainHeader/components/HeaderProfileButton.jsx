600c74c5759143598981117563b74f89d5dee2fa
Jules <julesmoreno66@gmail.com>
2024-09-16 14:14:12 +0800

    src/components/Header/MainHeader/components/HeaderProfileButton.jsx: update navlink active style

--- src/components/Header/MainHeader/components/HeaderProfileButton.jsx
+++ src/components/Header/MainHeader/components/HeaderProfileButton.jsx
@@ -8,7 +8,12 @@ export default function HeaderProfileButton() {
   // to do nav links highlight profile when clicked
   return (
     userLogin && (
-      <NavLink to="profile">
+      <NavLink
+        to="profile"
+        className={({ isActive }) =>
+          isActive ? "text-green-400 font-bold" : ""
+        }
+      >
         <div className="h-full flex items-center space-x-1 hover:text-gray-400 transition">
           <h1 className="text-xs md:text-lg">{username}</h1>
           <DefaultUserProfile />
