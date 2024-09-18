f07683bb0de22616f394a7f8e21b71dc5e705521
Jules <julesmoreno66@gmail.com>
2024-09-18 15:34:00 +0800

    src/pages/ProfilePage.jsx: added usePageTitle line 6

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,9 @@
+import useAuthListener from "@/hooks/useAuthListener";
+import usePageTitle from "@/hooks/usePageTitle";
+
 export default function ProfilePage() {
+  const { username } = useAuthListener();
+  usePageTitle(`${username} 👀`);
+
   return <div className="text-center text-3xl text-gray-50">ProfilePage</div>;
 }
