d44f2f47a15fbbd0a373f7d6c54b88a85bc24c4e
Jules <julesmoreno66@gmail.com>
2024-09-20 14:16:51 +0800

    src/features/MainPageFeatures/sections/MainPageSidebar.jsx: styled sidebar

--- src/features/MainPageFeatures/sections/MainPageSidebar.jsx
+++ src/features/MainPageFeatures/sections/MainPageSidebar.jsx
@@ -3,9 +3,9 @@ import MainPageUserInfo from "../components/MainPageUserInfo";
 
 export default function MainPageSidebar() {
   return (
-    <>
+    <section className="min-w-72 border-white border-2">
       <MainPageUserInfo />
       <MainPageAccountSuggestions />
-    </>
+    </section>
   );
 }
