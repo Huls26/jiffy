a0e64bf1bcff7c38239c7af7489f2f78d2de7f03
Jules <julesmoreno66@gmail.com>
2024-09-20 14:11:44 +0800

    src/features/MainPageFeatures/index.jsx: created, compose timeline and sidebase

---
+++ src/features/MainPageFeatures/index.jsx
@@ -0,0 +1,11 @@
+import MainPageSidebar from "./sections/MainPageSidebar";
+import MainPageTimeline from "./sections/MainPageTimeline";
+
+export default function MainPageFeatures() {
+  return (
+    <main className="max-w-6xl m-auto flex flex-col-reverse md:flex-row">
+      <MainPageTimeline />
+      <MainPageSidebar />
+    </main>
+  );
+}
