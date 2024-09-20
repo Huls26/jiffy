1d7b1a066f7981c719cbc19b4b333187570a0a5d
Jules <julesmoreno66@gmail.com>
2024-09-20 14:19:05 +0800

    src/pages/MainPage.jsx: updated

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,4 @@
-import MainPageSidebar from "@/features/MainPageFeatures/sections/MainPageSidebar";
-import MainPageTimeline from "@/features/MainPageFeatures/sections/MainPageTimeline";
+import MainPageFeatures from "@/features/MainPageFeatures";
 import useAuthListener from "@/hooks/useAuthListener";
 import usePageTitle from "@/hooks/usePageTitle";
 
@@ -11,16 +10,7 @@ export default function MainPage() {
 
   return (
     <main className="text-white text-center">
-      {globalState.userLogin && (
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/jiffy-ver-2.appspot.com/o/assets%2FdefaultProfile%2Fdefault-user-profile.webp?alt=media&token=a696e916-7833-484a-b2a2-d339734cbc80"
-          width="100"
-          height="100"
-          className="block mx-auto"
-        />
-      )}
-      <MainPageTimeline />
-      <MainPageSidebar />
+      <MainPageFeatures />
     </main>
   );
 }
