dc564f5d04013be7fd8896fa4115cb17d8f2c48e
Jules <julesmoreno66@gmail.com>
2024-09-20 14:14:49 +0800

    src/features/MainPageFeatures/components/MainPageUserInfo.jsx: added user profile, email and usernam

--- src/features/MainPageFeatures/components/MainPageUserInfo.jsx
+++ src/features/MainPageFeatures/components/MainPageUserInfo.jsx
@@ -1,3 +1,21 @@
+import useAuthListener from "@/hooks/useAuthListener";
+
 export default function MainPageUserInfo() {
-  return <div>MainPageUserInfo</div>;
+  const { email, username, photoURL } = useAuthListener();
+
+  return (
+    <section className="p-1 pl-2 flex">
+      <div className="grid place-self-center mr-2">
+        <img
+          className="w-8 rounded-full"
+          src={photoURL}
+          alt={`${username} user profile`}
+        />
+      </div>
+      <div className="text-left -space-y-1">
+        <h2 className="text-sky-400 font-semibold">{username}</h2>
+        <p className="font-mono text-xs text-gray-300">{email}</p>
+      </div>
+    </section>
+  );
 }
