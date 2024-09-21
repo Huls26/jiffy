e84f965e82feaca1d144f5941e44e1310ac9b9fe
Jules <julesmoreno66@gmail.com>
2024-09-21 16:42:57 +0800

    src/features/MainPageFeatures/components/MainPageUserInfo.jsx: added link to user"s info

--- src/features/MainPageFeatures/components/MainPageUserInfo.jsx
+++ src/features/MainPageFeatures/components/MainPageUserInfo.jsx
@@ -1,4 +1,5 @@
 import useAuthListener from "@/hooks/useAuthListener";
+import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 export default function MainPageUserInfo() {
@@ -6,19 +7,20 @@ export default function MainPageUserInfo() {
 
   return (
     // to decided add margin-auto or not...
-    <section className="max-w-80 p-1 pl-2 flex">
-      <div className="grid place-self-center mr-2">
+    // add link to='profile/:username
+    <section className="max-w-80 p-1 pl-2 flex cursor-pointer">
+      <Link to={`profile/${username}`} className="grid place-self-center mr-2">
         <img
-          className="w-8 rounded-full"
+          className="w-8 rounded-full cur"
           src={photoURL}
           alt={`${username} user profile`}
         />
-      </div>
-      <div className="text-left -space-y-1">
+      </Link>
+      <Link to={`profile/${username}`} className="text-left -space-y-1">
         <h2 className="text-sky-400 font-semibold">{username}</h2>
         <p className="font-mono text-xs text-gray-300">{email}</p>
-      </div>
-      <NavLink className="ml-auto">
+      </Link>
+      <NavLink to="creatpost" className="ml-auto">
         <button
           type="button"
           className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-2"
