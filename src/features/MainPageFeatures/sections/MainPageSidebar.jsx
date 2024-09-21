349e8749a57524847afbb716c8e423b21ac6fef3
Jules <julesmoreno66@gmail.com>
2024-09-21 16:41:30 +0800

    src/features/MainPageFeatures/sections/MainPageSidebar.jsx: added condition statement when user is login

--- src/features/MainPageFeatures/sections/MainPageSidebar.jsx
+++ src/features/MainPageFeatures/sections/MainPageSidebar.jsx
@@ -1,11 +1,18 @@
+import useAuthListener from "@/hooks/useAuthListener";
 import MainPageAccountSuggestions from "../components/MainPageAccountSuggestions";
 import MainPageUserInfo from "../components/MainPageUserInfo";
 
 export default function MainPageSidebar() {
-  return (
-    <section className="min-w-72 border-white border-2">
-      <MainPageUserInfo />
-      <MainPageAccountSuggestions />
-    </section>
-  );
+  const { userLogin } = useAuthListener();
+
+  if (userLogin) {
+    return (
+      <section className="min-w-72 border-white border-2">
+        <MainPageUserInfo />
+        <MainPageAccountSuggestions />
+      </section>
+    );
+  }
+
+  return null;
 }
