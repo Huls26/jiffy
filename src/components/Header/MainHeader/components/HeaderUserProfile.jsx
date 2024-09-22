18496cf44897b4cdb2124041747a436907e26327
Jules <julesmoreno66@gmail.com>
2024-09-22 17:51:17 +0800

    src/components/Header/MainHeader/components/HeaderUserProfile.jsx: implement LazyLoadImage line 7

--- src/components/Header/MainHeader/components/HeaderUserProfile.jsx
+++ src/components/Header/MainHeader/components/HeaderUserProfile.jsx
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function HeaderUserProfile({ photoURL }) {
   return (
     <div className="w-5 h-5 md:w-7 md:h-7 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 cursor-pointer">
-      <img
+      <LazyLoadImage
         src={photoURL}
         alt="user profile"
         className="block w-full h-full object-contain rounded-full"
+        effect="blur"
       />
     </div>
   );
