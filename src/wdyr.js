985401525b71bf6a34f27d2d16977f332ae801c0
Jules <julesmoreno66@gmail.com>
2024-09-23 15:08:04 +0800

    src/wdyr.js: created

---
+++ src/wdyr.js
@@ -0,0 +1,9 @@
+import whyDidYouRender from "@welldone-software/why-did-you-render";
+import React from "react";
+
+if (process.env.NODE_ENV === "development") {
+  // const whyDidYouRender = require("@welldone-software/why-did-you-render");
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
