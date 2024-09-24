05ecfa5e34b413b0264267254aa679a05dc0aa20
Jules <julesmoreno66@gmail.com>
2024-09-24 15:04:12 +0800

    src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx: add comment/To do:

--- src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
@@ -1,3 +1,8 @@
 export default function MainPageAccountSuggestions() {
+  // Display different users
+  // fetch all users that the current user is not following
+  // recommend at least 3-5 users
+  // display 3-5 random users for know
+
   return <div>MainPageAccountSuggestions</div>;
 }
