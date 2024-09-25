c75e4716798aa02c2a10b631ced5478ea6d29d58
Jules <julesmoreno66@gmail.com>
2024-09-25 16:01:12 +0800

    src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx: created

--- src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
@@ -1,8 +1,31 @@
+import { reducerContext } from "@/contexts/ReducerContextProvider";
+import { db } from '@/lib/fb';
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useContext } from "react";
+
 export default function MainPageAccountSuggestions() {
   // Display different users
   // fetch all users that the current user is not following
   // recommend at least 3-5 users
   // display 3-5 random users for know
+  const [globalState] = useContext(reducerContext);
+  const { userId } = globalState;
+
+  // fetch Data 
+  async function fetchUsers() {
+    const q = query(collection(db, 'users'), where('following', 'not-in', [userId]),);
+    const querySnapshot = await getDocs(q);
+    const filteredUsers = querySnapshot.docs.filter(doc => doc.data().userId !== userId);
+
+    for (const doc of filteredUsers) {
+      console.log(doc.data())
+    }
+  }
+
+  fetchUsers()
+
 
   return <div>MainPageAccountSuggestions</div>;
 }
+
+MainPageAccountSuggestions.whyDidYouRender = true;
