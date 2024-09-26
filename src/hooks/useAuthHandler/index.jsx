d524f59848e2e22272ff6f4f89187183fd369dbf
Jules <julesmoreno66@gmail.com>
2024-09-26 16:15:00 +0800

    src/hooks/useAuthHandler/: to be implement later, this will changee thee state when user is login manually

---
+++ src/hooks/useAuthHandler/index.jsx
@@ -0,0 +1,8 @@
+// import { reducerContext } from "@/contexts/ReducerContextProvider";
+
+// export default function useAuthHandler() {
+//   const [globalStateContext, dispatch] = useContext(reducerContext);
+//   return (
+//     <div>useAuthHandler</div>
+//   )
+// }
