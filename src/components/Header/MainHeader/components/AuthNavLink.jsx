263cd55c7061e820d4704e3122737c4b0f89707d
Jules <julesmoreno66@gmail.com>
2024-09-27 15:54:32 +0800

    src/components/Header/MainHeader/components/AuthNavLink.jsx: update context line 13

--- src/components/Header/MainHeader/components/AuthNavLink.jsx
+++ src/components/Header/MainHeader/components/AuthNavLink.jsx
@@ -1,7 +1,8 @@
-import useAuthListener from "@/hooks/useAuthListener";
+import { GlobalContext } from "@/contexts/GlobalContextProvider";
 import LogoutButton from "./LogoutButton";
 import NavLinkLI from "./NavLinkLI";
 
+import { useContext } from "react";
 /**
  * AuthNavLink is a React functional component that renders a navigation link based on the user's authentication status.
  * It uses the `useAuthListener` hook to listen for changes in the user's login status.
@@ -9,7 +10,8 @@ import NavLinkLI from "./NavLinkLI";
  * @returns {JSX.Element} - A JSX element representing the navigation link.
  */
 export default function AuthNavLink() {
-  const { userLogin, isLoading } = useAuthListener();
+  const globalState = useContext(GlobalContext);
+  const { userLogin, isLoading } = globalState;
 
   if (isLoading) {
     return <h1>{"< ... />"}</h1>;
