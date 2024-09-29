238ab3d2f518472bb562ab019c09263429c557e1
Jules <julesmoreno66@gmail.com>
2024-09-29 12:34:28 +0800

    src/contexts/GlobalAuthContextProvider/index.jsx: added comment/doc

--- src/contexts/GlobalAuthContextProvider/index.jsx
+++ src/contexts/GlobalAuthContextProvider/index.jsx
@@ -4,10 +4,21 @@ import { createContext } from 'react';
 const GlobalAuthContext = createContext();
 export { GlobalAuthContext };
 
+/**
+ * A React context provider component that manages authentication state.
+ * It uses the custom hook `useAuthListener` to fetch and update the authentication state.
+ *
+ * @function GlobalAuthContextProvider
+ * @param {Object} props - The component props.
+ * @param {React.ReactNode} props.children - The child components to be wrapped by the provider.
+ * @returns {React.ReactElement} - The wrapped child components with the authentication context.
+ */
 export default function GlobalAuthContextProvider({ children }) {
+  // Fetch and update the authentication state using the custom hook
   const globalContextState = useAuthListener();
 
   return (
+    // Provide the authentication state to the child components
     <GlobalAuthContext.Provider value={globalContextState}>
       {children}
     </GlobalAuthContext.Provider>
