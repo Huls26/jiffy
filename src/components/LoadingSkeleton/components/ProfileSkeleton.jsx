9ae078f736742be61c148f7a1889125adf4ed491
Jules <julesmoreno66@gmail.com>
2024-10-01 16:13:33 +0800

    src/components/LoadingSkeleton/components/ProfileSkeleton.jsx: created

---
+++ src/components/LoadingSkeleton/components/ProfileSkeleton.jsx
@@ -0,0 +1,7 @@
+export default function ProfileSkeleton() {
+  return (
+    <div className="w-8 h-8 bg-gray-200 rounded-full dark:bg-gray-700 mb-2">
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+}
