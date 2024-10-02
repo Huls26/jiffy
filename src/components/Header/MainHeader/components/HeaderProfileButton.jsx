e3778297b9ed94083b5b1b479b6683f58c90fcab
Jules <julesmoreno66@gmail.com>
2024-10-02 14:20:00 +0800

    src/components/Header/MainHeader/components/HeaderProfileButton.jsx: added UserProfile

--- src/components/Header/MainHeader/components/HeaderProfileButton.jsx
+++ src/components/Header/MainHeader/components/HeaderProfileButton.jsx
@@ -1,5 +1,5 @@
+import UserProfile from "@/components/UserProfile";
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
-import HeaderUserProfile from "./HeaderUserProfile";
 
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
@@ -18,7 +18,7 @@ export default function HeaderProfileButton() {
       >
         <div className="h-full flex items-center space-x-1 hover:text-gray-400 transition">
           <h1 className="text-xs md:text-lg">{username}</h1>
-          <HeaderUserProfile photoURL={photoURL} />
+          <UserProfile photoURL={photoURL} addedClassName={"w-5 h-5 md:w-7 md:h-7"} />
         </div>
       </NavLink>
     )
