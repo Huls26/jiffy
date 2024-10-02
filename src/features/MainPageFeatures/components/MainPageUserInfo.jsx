6506fe65f4ee8857709b4da16822a972098a3844
Jules <julesmoreno66@gmail.com>
2024-10-02 14:38:03 +0800

    src/features/MainPageFeatures/components/MainPageUserInfo.jsx: composed MainPageCreatePostBtn

--- src/features/MainPageFeatures/components/MainPageUserInfo.jsx
+++ src/features/MainPageFeatures/components/MainPageUserInfo.jsx
@@ -1,11 +1,11 @@
 import UserProfile from "@/components/UserProfile";
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
+import MainPageCreatePostBtn from "./MainPageCreatePostBtn";
 import MainPageUserInfoSkeleton from "./MainPageUserInfoSkeleton";
 import MainPageUserProfileLink from "./MainPageUserProfileLink";
 
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 
 export default function MainPageUserInfo() {
   const [globalState] = useContext(GlobalContext);
@@ -22,14 +22,7 @@ export default function MainPageUserInfo() {
         <UserProfile photoURL={photoURL} addedClassName={'w-8 h-8 mr-2'} />
       </Link>
       <MainPageUserProfileLink username={username} email={email} />
-      <NavLink to="creatpost" className="ml-auto">
-        <button
-          type="button"
-          className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:text-gray-600 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-2"
-        >
-          Post
-        </button>
-      </NavLink>
+      <MainPageCreatePostBtn />
     </section>
   );
 }
