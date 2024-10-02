2ed88eff796dc14df6deccbfdcff681f5f178043
Jules <julesmoreno66@gmail.com>
2024-10-02 14:22:21 +0800

    src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx: update

--- src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
@@ -5,14 +5,14 @@ import TextSkeleton from '@/components/LoadingSkeleton/components/TextSkeleton';
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
 import { db } from '@/lib/fb';
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { useContext } from "react";
+import { useContext, useEffect } from 'react';
 
 export default function MainPageAccountSuggestions() {
   // Display different users
   // fetch all users that the current user is not following
   // recommend at least 3-5 users
   // display 3-5 random users for know
-  const [globalState] = useContext(GlobalContext);
+  const [globalState, dispatch] = useContext(GlobalContext);
   const { userId } = globalState;
 
   // fetch Data 
@@ -31,8 +31,24 @@ export default function MainPageAccountSuggestions() {
     }
   }
 
-  fetchUsers()
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    const loadUsers = async () => {
+      dispatch({
+        type: "UPDATE_LOADING",
+        isLoading: true,
+      });
 
+      await fetchUsers(); // Waits for the fetch to complete
+
+      dispatch({
+        type: "UPDATE_LOADING",
+        isLoading: false,
+      });
+    };
+
+    loadUsers();
+  }, [])
 
   return (
     <LoadingSkeleton>
