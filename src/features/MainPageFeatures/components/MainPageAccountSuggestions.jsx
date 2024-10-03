33ec6f495e10755b924327a3972f6cadde527dcf
Jules <julesmoreno66@gmail.com>
2024-10-03 14:02:45 +0800

    src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx: updated components

--- src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/MainPageAccountSuggestions.jsx
@@ -1,11 +1,8 @@
-import LoadingSkeleton from '@/components/LoadingSkeleton';
-import ProfileSkeleton from '@/components/LoadingSkeleton/components/ProfileSkeleton';
-import TextSkeleton from '@/components/LoadingSkeleton/components/TextSkeleton';
-
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
 import { db } from '@/lib/fb';
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import MainPageUserProfileLink from "./MainPageUserProfileLink";
 
 export default function MainPageAccountSuggestions() {
   // Display different users
@@ -14,6 +11,7 @@ export default function MainPageAccountSuggestions() {
   // display 3-5 random users for know
   const [globalState, dispatch] = useContext(GlobalContext);
   const { userId } = globalState;
+  const [suggestedUsers, setSuggestedUsers] = useState(null);
 
   // fetch Data 
   // create a button new suggestion
@@ -22,13 +20,22 @@ export default function MainPageAccountSuggestions() {
     const q = query(collection(db, 'users'), where('following', 'not-in', [userId]),);
     const querySnapshot = await getDocs(q);
     const filteredUsers = querySnapshot.docs.filter(doc => doc.data().userId !== userId);
+    console.log(filteredUsers)
     // randomly select users
     const randomIdx = Array.from({ length: filteredUsers.length }, (_, index) => index).sort(() => Math.random() - 0.5).slice(0, 3);
+    const suggestionProfiles = randomIdx.map(idx => {
+      const userProfile = filteredUsers[idx].data();
 
-    console.log(randomIdx);
-    for (const doc of filteredUsers) {
-      console.log(doc.data())
-    }
+      return {
+        userId: userProfile.userId,
+        username: userProfile.username,
+        email: userProfile.email,
+        photoURL: userProfile.photoURL,
+        followersCount: userProfile.followersCount,
+      }
+    });
+
+    return suggestionProfiles;
   }
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
@@ -39,7 +46,9 @@ export default function MainPageAccountSuggestions() {
         isLoading: true,
       });
 
-      await fetchUsers(); // Waits for the fetch to complete
+      const suggestionUsers = await fetchUsers(); // Waits for the fetch to complete
+
+      setSuggestedUsers(() => suggestionUsers);
 
       dispatch({
         type: "UPDATE_LOADING",
@@ -51,17 +60,18 @@ export default function MainPageAccountSuggestions() {
   }, [])
 
   return (
-    <LoadingSkeleton>
-      <h1>MainPageSuggestions</h1>
-      <div className='p-1 pl-2 flex'>
-        <ProfileSkeleton />
-        <div className='space-y-1'>
-          <TextSkeleton width={"w-20"} />
-          <TextSkeleton width={"w-32"} />
+    <section className="text-gray-200">
+      <h1>Suggested acounts to follow</h1>
+      {
+        // list of suggested acounts to follow 
+      }
+      {suggestedUsers?.map((user) => (
+        <div key={user.userId}>
+          <MainPageUserProfileLink to={`profile/${user.userId}`} photoURL={user.photoURL} username={user.username} email={user.email} />
         </div>
-      </div>
-    </LoadingSkeleton >
-  );
+      ))}
+    </section>
+  )
 }
 
 MainPageAccountSuggestions.whyDidYouRender = true;
