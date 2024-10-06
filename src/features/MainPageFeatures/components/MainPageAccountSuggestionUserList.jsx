4b29ea11008c8238305469d25aff4dac14faa36f
Jules <julesmoreno66@gmail.com>
2024-10-06 13:38:19 +0800

    src/features/MainPageFeatures/components/MainPageAccountSuggestionUserList.jsx: created

---
+++ src/features/MainPageFeatures/components/MainPageAccountSuggestionUserList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import MainPageAccountSuggestionProfile from "./MainpageAccountSuggestionProfile";
+
+
+export default function MainPageAccountSuggestionUserList({ list }) {
+  return (
+    list?.map((user) => (
+      <MainPageAccountSuggestionProfile key={user.userId} user={user} />
+    ))
+  )
+}
+
+MainPageAccountSuggestionUserList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    userId: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  })).isRequired,
+}
