7a42e35275721961ec13b9e80b86692b4b85a18b
Jules <julesmoreno66@gmail.com>
2024-10-07 14:29:43 +0800

    src/features/MainPageFeatures/context/MainPageSidebarContextReducer.js: added initial state

--- src/features/MainPageFeatures/context/MainPageSidebarContextReducer.js
+++ src/features/MainPageFeatures/context/MainPageSidebarContextReducer.js
@@ -7,19 +7,18 @@ export const INITIAL_STATE = {
   errorMessage: "",
   isError: false,
   isLoading: false,
+  suggestedUsersList: null,
 };
 
 /**
- * A reducer function for managing user authentication state.
+ * A reducer function for managing the main page sidebar context state.
  *
  * @function reducerMethod
- * @param {Object} state - The current state of the application.
+ * @param {Object} state - The current state of the main page sidebar context.
  * @param {Object} action - The action object dispatched to the reducer.
- * @param {string} state.email - The current email of the user.
- * @param {string} state.password - The current password of the user.
  * @param {string} action.type - The type of action being performed.
  * @param {string} action.payload - The payload associated with the action.
- * @returns {Object} - The updated state of the application.
+ * @returns {Object} - The updated state of the main page sidebar context.
  * @throws {Error} - Throws an error if an invalid action type is encountered.
  */
 // biome-ignore lint/style/useDefaultParameterLast: <explanation>
@@ -32,6 +31,11 @@ export default function reducerMethod(state = INITIAL_STATE, action) {
         ...state,
         isErrorAuth: action.isError,
         errorMessage: action.message,
+      };
+    case "UPDATE_LIST":
+      return {
+        ...state,
+        suggestedUsersList: action.suggestedUsersList,
       };
     default:
       throw new Error(`Invalid action type: ${action.type}`);
