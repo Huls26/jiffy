bd90e87cae8c6e1bb6fa2333d8d7436adc2c68b2
Jules <julesmoreno66@gmail.com>
2024-10-07 13:45:59 +0800

    src/features/MainPageFeatures/sections/MainPageSidebar.jsx: added reducer context line 15

--- src/features/MainPageFeatures/sections/MainPageSidebar.jsx
+++ src/features/MainPageFeatures/sections/MainPageSidebar.jsx
@@ -1,7 +1,10 @@
-import { GlobalContext } from "@/contexts/GlobalContextProvider";
-import { useContext } from "react";
+import ReducerContextProvider from "@/contexts/ReducerContextProvider";
 import MainPageAccountSuggestions from "../components/accountSuggestions/MainPageAccountSuggestions";
 import MainPageUserCard from "../components/userInfo/MainPageUserCard";
+
+import { GlobalContext } from "@/contexts/GlobalContextProvider";
+import { useContext } from "react";
+import reducerMethod, { INITIAL_STATE } from "../context/MainPageSidebarContextReducer";
 
 export default function MainPageSidebar() {
   const [globalState] = useContext(GlobalContext);
@@ -9,10 +12,16 @@ export default function MainPageSidebar() {
 
   if (userLogin) {
     return (
-      <section className="py-1 min-w-[280px] sm:border-gray-300 sm:border-2 sm:m-2 rounded-lg">
-        <MainPageUserCard />
-        <MainPageAccountSuggestions />
-      </section>
+      <ReducerContextProvider
+        reducerMethod={reducerMethod}
+        INITIAL_STATE={INITIAL_STATE}
+      >
+        <section className="py-1 min-w-[280px] sm:border-gray-300 sm:border-2 sm:m-2 rounded-lg">
+          <MainPageUserCard />
+          <MainPageAccountSuggestions />
+        </section>
+      </ReducerContextProvider>
+
     );
   }
 
