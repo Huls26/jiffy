7325d53edef9a7a5d647ee445f69c6fce1199a59
Jules <julesmoreno66@gmail.com>
2024-10-08 13:40:07 +0800

    src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx: removed some code used useFetchUserSuggestion() line 8

--- src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
@@ -1,37 +1,11 @@
-import { GlobalContext } from "@/contexts/GlobalContextProvider";
-import { reducerContext } from "@/contexts/ReducerContextProvider";
-import fetchRandomUsers from '../../utils/accountSuggestion/fetchRandomUsers';
+import useFetchUserSuggestions from "../../hooks/useFetchUserSuggestions";
 import SuggestedUsersCloseBtn from "../buttons/SuggestedUsersCloseBtn";
 import MainPageAccountSuggestionListDisplay from "./MainPageAccountSuggestionListDisplay";
 import MainPageAccountSuggestionTitle from './MainPageAccountSuggestionTitle';
 import MainPageNoSuggestedUsersMessage from './MainPageNoSuggestedUsersMessage';
 
-import { useContext, } from 'react';
-
 export default function MainPageAccountSuggestions() {
-  const [globalState] = useContext(GlobalContext);
-  const [sidebarState, dispatch] = useContext(reducerContext)
-  const { userId } = globalState;
-  const { suggestedUsersList } = sidebarState;
-
-  async function fetchUserSuggestions() {
-    dispatch({
-      type: "UPDATE_LOADING",
-      isLoading: true,
-    });
-    const suggestionUsers = await fetchRandomUsers(userId); // Waits for the fetch to complete
-
-    dispatch({
-      type: "UPDATE_LIST",
-      suggestedUsersList: suggestedUsersList
-        ? null : suggestionUsers,
-    })
-
-    dispatch({
-      type: "UPDATE_LOADING",
-      isLoading: false,
-    });
-  }
+  const { suggestedUsersList, fetchUserSuggestions } = useFetchUserSuggestions()
 
   return (
     <section className="pt-2 pb-2 text-gray-200">
