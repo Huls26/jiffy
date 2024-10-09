d83806fbca216f281744625b81fea2f135f6facc
Jules <julesmoreno66@gmail.com>
2024-10-09 13:32:52 +0800

    src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionProfile.jsx: added followUser func line 13

--- src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionProfile.jsx
+++ src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionProfile.jsx
@@ -1,12 +1,46 @@
-import PropTypes from 'prop-types';
+import { GlobalContext } from '@/contexts/GlobalContextProvider';
+import { db } from '@/lib/fb';
 import MainPageUserProfileLink from "../userInfo/MainPageUserProfileLink";
 
-export default function MainPageAccountSuggestionProfile({ user }) {
-  return (
-    <div key={user.userId} className="flex justify-between items-center m-2 px-2 py-1 border-2 border-gray-400 rounded-xl">
-      <MainPageUserProfileLink to={`profile/${user.userId}`} photoURL={user.photoURL} username={user.username} email={user.email} />
+import { arrayUnion, doc, increment, updateDoc } from "firebase/firestore";
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
 
-      <button type='button' className="font-semibold text-gray-400 hover:text-gray-200 active:text-green-300">follow</button>
+export default function MainPageAccountSuggestionProfile({ user }) {
+  const [globalState] = useContext(GlobalContext)
+  const { userId } = globalState;
+
+  async function followUser() {
+    const currentUserRef = doc(db, "users", userId);
+    const usersFollowingRef = doc(db, "users", user.userId)
+
+    await updateDoc(currentUserRef, {
+      following: arrayUnion(user.userId)
+    });
+    await updateDoc(usersFollowingRef, {
+      followers: arrayUnion(userId),
+      followersCount: increment(1)
+    });
+  }
+
+  return (
+    <div key={user.userId}
+      className="flex justify-between items-center m-2 px-2 py-1 border-2 border-gray-400 rounded-xl"
+    >
+      <MainPageUserProfileLink
+        to={`profile/${user.userId}`}
+        photoURL={user.photoURL}
+        username={user.username}
+        email={user.email}
+      />
+
+      <button
+        type='button'
+        className="font-semibold text-gray-400 hover:text-gray-200 active:text-green-300 focus:text-green-300"
+        onClick={followUser}
+      >
+        follow
+      </button>
     </div>
   )
 }
