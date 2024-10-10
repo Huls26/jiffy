fab23066715d895948a0e577d1fe327f7e538798
Jules <julesmoreno66@gmail.com>
2024-10-10 12:39:53 +0800

    src/features/MainPageFeatures/utils/accountSuggestion/followUser.js: update data stype from following and followers change from [] to {}

--- src/features/MainPageFeatures/utils/accountSuggestion/followUser.js
+++ src/features/MainPageFeatures/utils/accountSuggestion/followUser.js
@@ -1,16 +1,16 @@
 import { db } from "@/lib/fb";
 
-import { arrayUnion, doc, increment, updateDoc } from "firebase/firestore";
+import { doc, increment, updateDoc } from "firebase/firestore";
 
 export default async function followUser(userId, user) {
   const currentUserRef = doc(db, "users", userId);
   const usersFollowingRef = doc(db, "users", user.userId);
 
   await updateDoc(currentUserRef, {
-    following: arrayUnion(user.userId),
+    following: { [user.userId]: user.userId },
   });
   await updateDoc(usersFollowingRef, {
-    followers: arrayUnion(userId),
+    followers: { userId },
     followersCount: increment(1),
   });
 }
