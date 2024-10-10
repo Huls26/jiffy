8fdb07050ca2b8a39a459f5a7ee4a5cfb0b64882
Jules <julesmoreno66@gmail.com>
2024-10-10 13:23:29 +0800

    src/features/MainPageFeatures/utils/accountSuggestion/followUser.js: added userSuggestion followeers and currentUser following data

--- src/features/MainPageFeatures/utils/accountSuggestion/followUser.js
+++ src/features/MainPageFeatures/utils/accountSuggestion/followUser.js
@@ -1,16 +1,21 @@
 import { db } from "@/lib/fb";
 
-import { doc, increment, updateDoc } from "firebase/firestore";
+import { doc, getDoc, increment, updateDoc } from "firebase/firestore";
 
-export default async function followUser(userId, user) {
+export default async function followUser(userId, suggestionUser) {
   const currentUserRef = doc(db, "users", userId);
-  const usersFollowingRef = doc(db, "users", user.userId);
+  const usersFollowingRef = doc(db, "users", suggestionUser.userId);
+
+  const docSnap = (await getDoc(currentUserRef)).data();
 
   await updateDoc(currentUserRef, {
-    following: { [user.userId]: user.userId },
+    following: {
+      ...docSnap.following,
+      [suggestionUser.userId]: suggestionUser.userId,
+    },
   });
   await updateDoc(usersFollowingRef, {
-    followers: { userId },
+    followers: { ...suggestionUser.followers, [userId]: userId },
     followersCount: increment(1),
   });
 }
