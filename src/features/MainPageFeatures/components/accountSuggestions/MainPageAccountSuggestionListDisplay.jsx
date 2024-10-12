229266330e5e2fbb784d40fe14cf1c1f8dd53745
Jules <julesmoreno66@gmail.com>
2024-10-12 14:34:39 +0800

    src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionListDisplay.jsx: added loading skeleton line 12

--- src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionListDisplay.jsx
+++ src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestionListDisplay.jsx
@@ -1,9 +1,25 @@
+import { reducerContext } from '@/contexts/ReducerContextProvider';
+import { useContext } from 'react';
+import MainPageUserInfoSkeleton from '../MainPageUserInfoSkeleton';
 import DiscoverUsersBtn from '../buttons/DiscoverUsersBtn';
 import MainPageAccountSuggestionUserList from './MainPageAccountSuggestionUserList';
 
 import PropTypes from 'prop-types';
 
 export default function MainPageAccountSuggestionListDisplay({ suggestedUsersList, onClick }) {
+  const [sidebarState] = useContext(reducerContext);
+
+  if (sidebarState.isLoading) {
+    return (
+      <div className='px-2'>
+        <MainPageUserInfoSkeleton />
+        <MainPageUserInfoSkeleton />
+        <MainPageUserInfoSkeleton />
+      </div>
+
+    )
+  }
+
   if (suggestedUsersList !== null) {
     return (
       <MainPageAccountSuggestionUserList
