ee81a28cc73e541c62c240fd14009d1447948035
Jules <julesmoreno66@gmail.com>
2024-10-12 14:33:29 +0800

    src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx: removed loading skeleton

--- src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx
+++ src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx
@@ -1,6 +1,6 @@
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
-import { reducerContext } from "@/contexts/ReducerContextProvider";
-import MainPageUserInfoSkeleton from "../MainPageUserInfoSkeleton";
+// import { reducerContext } from "@/contexts/ReducerContextProvider";
+// import MainPageUserInfoSkeleton from "../MainPageUserInfoSkeleton";
 import MainPageCreatePostBtn from "../buttons/MainPageCreatePostBtn";
 import MainPageUserProfileLink from "./MainPageUserProfileLink";
 
@@ -8,16 +8,16 @@ import { useContext } from "react";
 
 export default function MainPageUserCard() {
   const [globalState] = useContext(GlobalContext);
-  const [sidebarState] = useContext(reducerContext)
   const { email, username, photoURL } = globalState;
-
-  if (sidebarState.isLoading) {
-    return <MainPageUserInfoSkeleton />
-  }
 
   return (
     <section className="sm:max-w-80 p-1 pl-2 flex cursor-pointer">
-      <MainPageUserProfileLink to={`profile/${username}`} photoURL={photoURL} username={username} email={email} />
+      <MainPageUserProfileLink
+        to={`profile/${username}`}
+        photoURL={photoURL}
+        username={username}
+        email={email}
+      />
       <MainPageCreatePostBtn />
     </section>
   );
