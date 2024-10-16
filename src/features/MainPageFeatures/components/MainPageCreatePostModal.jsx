0d864c1ccf093c8e68e855e2b780088f820e3ccd
Jules <julesmoreno66@gmail.com>
2024-10-16 14:47:56 +0800

    src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx: added style

--- src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx
+++ src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx
@@ -1,7 +1,20 @@
+import PropTypes from "prop-types";
+import { createPortal } from "react-dom";
+
 export default function MainPageCreatePostModal({ displayModal }) {
   if (!displayModal) return null;
 
-  return (
-    <div>MainPageCreatePostModal</div>
+  // add style and context
+  return createPortal(
+    <div className="
+    fixed top-28 left-2/4 text-xl -translate-x-1/2 
+    text-white border">
+      MainPageCreatePostModal
+    </div>,
+    document.getElementById('root')
   )
+}
+
+MainPageCreatePostModal.propTypes = {
+  displayModal: PropTypes.bool.isRequired,
 }
