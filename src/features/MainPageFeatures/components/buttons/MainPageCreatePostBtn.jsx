fc86dc80846d773a77aa3c704c8af8e6df657ae5
Jules <julesmoreno66@gmail.com>
2024-10-16 14:04:49 +0800

    src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx: removed links & added onClick event line 9

--- src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
+++ src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types"
 
-export default function MainPageCreatePostBtn() {
+export default function MainPageCreatePostBtn({ onClick }) {
   return (
-    <Link to="creatpost" className="ml-auto">
+    <div className="ml-auto">
       <button
         type="button"
         className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:text-gray-600 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-2"
+        onClick={onClick}
       >
         Post
       </button>
-    </Link>
+    </div>
   )
+}
+
+MainPageCreatePostBtn.propTypes = {
+  onClick: PropTypes.func.isRequired,
 }
