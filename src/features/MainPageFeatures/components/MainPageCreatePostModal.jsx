b0bc650623e190f502c0942d43554896678ff6f7
Jules <julesmoreno66@gmail.com>
2024-10-17 15:03:31 +0800

    src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx: update style and added input text tobe fix later

--- src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx
+++ src/features/MainPageFeatures/components/MainPageCreatePostModal.jsx
@@ -1,15 +1,38 @@
+import MainPageUserProfileLink from "../components/userInfo/MainPageUserProfileLink";
+
 import PropTypes from "prop-types";
 import { createPortal } from "react-dom";
 
-export default function MainPageCreatePostModal({ displayModal }) {
+export default function MainPageCreatePostModal({ displayModal, userInfo }) {
+  const { username, photoURL } = userInfo;
+
   if (!displayModal) return null;
 
   // add style and context
   return createPortal(
     <div className="
-    fixed top-28 left-2/4 text-xl -translate-x-1/2 
-    text-white border">
-      MainPageCreatePostModal
+      fixed top-24 left-2/4 -translate-x-1/2 
+      px-3 py-2
+      w-10/12 max-w-3xl
+    bg-slate-950
+      border-2 border-gray-300 rounded-lg
+      space-y-2
+    ">
+      <MainPageUserProfileLink
+        to={`profile/${username}`}
+        photoURL={photoURL}
+        username={username}
+        email={''}
+      />
+      <input
+        type="text"
+        className="bg-slate-950 outline-none font-semibold text-gray-400"
+        placeholder="Publish a Post"
+      />
+      <div className="flex justify-between text-white">
+        <h1>imageLogo</h1>
+        <button type="button" className="">Post</button>
+      </div>
     </div>,
     document.getElementById('root')
   )
@@ -17,4 +40,9 @@ export default function MainPageCreatePostModal({ displayModal }) {
 
 MainPageCreatePostModal.propTypes = {
   displayModal: PropTypes.bool.isRequired,
+  userInfo: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+  }).isRequired,
 }
