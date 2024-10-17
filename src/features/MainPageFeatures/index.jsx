6f7df23a966d099efd698652989f7839f298ee32
Jules <julesmoreno66@gmail.com>
2024-10-17 14:14:52 +0800

    src/features/MainPageFeatures/index.jsx: added padding-right line 6

--- src/features/MainPageFeatures/index.jsx
+++ src/features/MainPageFeatures/index.jsx
@@ -3,7 +3,7 @@ import MainPageTimeline from "./sections/MainPageTimeline";
 
 export default function MainPageFeatures() {
   return (
-    <main className="max-w-6xl m-auto flex flex-col-reverse sm:flex-row">
+    <main className="max-w-6xl m-auto sm:pr-2 flex flex-col-reverse sm:flex-row">
       <MainPageTimeline />
       <MainPageSidebar />
     </main>
