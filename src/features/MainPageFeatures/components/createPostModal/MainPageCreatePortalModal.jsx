fa1183995d5eaba88acf3e2737320d91dbfab788
Jules <julesmoreno66@gmail.com>
2024-10-21 15:28:43 +0800

    src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx: update fallback component style

--- src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx
+++ src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx
@@ -18,13 +18,12 @@ export default function MainPageCreatePortalModal() {
 
   if (!isDisplayPostModalOpen) return null;
 
-  console.log("code split this component")
   return createPortal(
-    <Suspense fallback={<div class='flex space-x-2 justify-center items-center bg-white h-screen dark:invert'>
+    <Suspense fallback={<div className='fixed top-20 sm:top-32 left-1/2 -translate-x-1/2 flex space-x-2 justify-center items-center bg-none'>
+      <div className='h-3 w-3 bg-red-400 rounded-full animate-bounce [animation-delay:-0.3s]' />
+      <div className='h-3 w-3 bg-red-400 rounded-full animate-bounce [animation-delay:-0.15s]' />
+      <div className='h-3 w-3 bg-red-400 rounded-full animate-bounce' />
       <span class='sr-only'>Loading...</span>
-      <div clasName='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]' />
-      <div className='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]' />
-      <div className='h-8 w-8 bg-black rounded-full animate-bounce' />
     </div>}>
       <MainPagePostCreationModal />
     </Suspense>,
