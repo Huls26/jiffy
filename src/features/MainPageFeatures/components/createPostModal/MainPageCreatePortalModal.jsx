fc17f304f15bcd0791092d95c5da4b81979b212d
Jules <julesmoreno66@gmail.com>
2024-10-21 15:00:03 +0800

    src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx: implement code splitting and added fallback line 23

--- src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx
+++ src/features/MainPageFeatures/components/createPostModal/MainPageCreatePortalModal.jsx
@@ -1,7 +1,16 @@
 import { reducerContext } from "@/contexts/ReducerContextProvider";
-import { useContext } from "react";
+// import MainPagePostCreationModal from "./MainPagePostCreationModal";
+
+import { Suspense, lazy, useContext } from "react";
 import { createPortal } from "react-dom";
-import MainPagePostCreationModal from "./MainPagePostCreationModal";
+
+// const MainPagePostCreationModal = lazy(() => import("./MainPagePostCreationModal"));
+
+const MainPagePostCreationModal = lazy(() =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(import('./MainPagePostCreationModal')), 10000); // Delay of 10 seconds
+  })
+);
 
 export default function MainPageCreatePortalModal() {
   const [sidebarContext] = useContext(reducerContext)
@@ -11,7 +20,14 @@ export default function MainPageCreatePortalModal() {
 
   console.log("code split this component")
   return createPortal(
-    <MainPagePostCreationModal />,
+    <Suspense fallback={<div class='flex space-x-2 justify-center items-center bg-white h-screen dark:invert'>
+      <span class='sr-only'>Loading...</span>
+      <div clasName='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]' />
+      <div className='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]' />
+      <div className='h-8 w-8 bg-black rounded-full animate-bounce' />
+    </div>}>
+      <MainPagePostCreationModal />
+    </Suspense>,
     document.getElementById('root')
   )
 }
