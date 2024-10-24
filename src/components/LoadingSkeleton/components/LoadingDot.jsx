0d5efe83b363d6a1dc95e00436ff4882d914195e
Jules <julesmoreno66@gmail.com>
2024-10-24 13:25:07 +0800

    src/components/LoadingSkeleton/components/LoadingDot.jsx: update bg color

--- src/components/LoadingSkeleton/components/LoadingDot.jsx
+++ src/components/LoadingSkeleton/components/LoadingDot.jsx
@@ -1,9 +1,9 @@
 export default function LoadingDot() {
   return (
     <div className='fixed top-20 sm:top-32 left-1/2 -translate-x-1/2 flex space-x-2 justify-center items-center bg-none'>
-      <div className='h-3 w-3 bg-teal-400 rounded-full animate-bounce [animation-delay:-0.3s]' />
-      <div className='h-3 w-3 bg-teal-400 rounded-full animate-bounce [animation-delay:-0.15s]' />
-      <div className='h-3 w-3 bg-teal-400 rounded-full animate-bounce' />
+      <div className='h-3 w-3 bg-teal-200 rounded-full animate-bounce [animation-delay:-0.3s]' />
+      <div className='h-3 w-3 bg-teal-200 rounded-full animate-bounce [animation-delay:-0.15s]' />
+      <div className='h-3 w-3 bg-teal-200 rounded-full animate-bounce' />
       <span className='sr-only'>Loading...</span>
     </div>
   )
