3543b0d5bc3e13e6fab4c1453e3e8bfe9d4e2f24
Jules <julesmoreno66@gmail.com>
2024-10-29 14:36:38 +0800

    src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx: added loading dispatch line 25 & 47

--- src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx
+++ src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx
@@ -22,6 +22,7 @@ export default function MainPagePostModalBtnSection() {
       return; // Exit if there is no image
     }
 
+    dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: true });
     const postRef = doc(collection(db, "userPosts"));
     const storageRef = ref(storage, `contentImages/${globalState.userId}/${postRef.id}`); // Use postRef.id for the file name
 
@@ -43,6 +44,7 @@ export default function MainPagePostModalBtnSection() {
 
       // Set the document in Firestore
       await setDoc(postRef, newPost);
+      dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: false });
       console.log("Post published successfully:", newPost);
     } catch (error) {
       console.error("Error publishing post:", error);
@@ -78,7 +80,7 @@ export default function MainPagePostModalBtnSection() {
           htmlFor="postInputFile"
           className="hover:opacity-75 active:opacity-60"
         >
-          <div className="sm:ml-1 m:scale-125">
+          <div className="sm:ml-1 md:scale-125">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none">
               <path fillRule="evenodd" clipRule="evenodd" d="M7 7C5.34315 7 4 8.34315 4 10C4 11.6569 5.34315 13 7 13C8.65685 13 10 11.6569 10 10C10 8.34315 8.65685 7 7 7ZM6 10C6 9.44772 6.44772 9 7 9C7.55228 9 8 9.44772 8 10C8 10.5523 7.55228 11 7 11C6.44772 11 6 10.5523 6 10Z" fill="currentColor" />
               <path fillRule="evenodd" clipRule="evenodd" d="M3 3C1.34315 3 0 4.34315 0 6V18C0 19.6569 1.34315 21 3 21H21C22.6569 21 24 19.6569 24 18V6C24 4.34315 22.6569 3 21 3H3ZM21 5H3C2.44772 5 2 5.44772 2 6V18C2 18.5523 2.44772 19 3 19H7.31374L14.1924 12.1214C15.364 10.9498 17.2635 10.9498 18.435 12.1214L22 15.6863V6C22 5.44772 21.5523 5 21 5ZM21 19H10.1422L15.6066 13.5356C15.9971 13.145 16.6303 13.145 17.0208 13.5356L21.907 18.4217C21.7479 18.7633 21.4016 19 21 19Z" fill="currentColor" />
@@ -87,7 +89,7 @@ export default function MainPagePostModalBtnSection() {
         </label>
         <p
           className='
-          text-gray-400 text-xs sm:text-sm 
+          text-gray-400 text-xs sm:text-sm
             whitespace-nowrap overflow-hidden text-ellipsis
           '
         >
