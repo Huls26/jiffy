f858d112c0f34980604f72c7e8b2f8619b2e4ca3
Jules <julesmoreno66@gmail.com>
2024-10-30 13:49:38 +0800

    src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx: removed some codes & compose MainPagePublishButton line 71

--- src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx
+++ src/features/MainPageFeatures/components/createPostModal/MainPagePostModalBtnSection.jsx
@@ -1,55 +1,11 @@
-import { GlobalContext } from '@/contexts/GlobalContextProvider';
 import { reducerContext } from "@/contexts/ReducerContextProvider";
-import { db, storage } from '@/lib/fb';
+import MainPagePublishButton from './MainPagePublishButton';
 
-import { Timestamp, collection, doc, setDoc } from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useContext } from "react";
 
 export default function MainPagePostModalBtnSection() {
-  const [globalState] = useContext(GlobalContext);
   const [sidebarState, dispatch] = useContext(reducerContext);
-  const { imageName, imageFile, postContentText } = sidebarState;
-
-  console.log('clean up');
-  const defaultPublishStyle = "px-2 py-0.5 bg-sky-950 font-semibold rounded-md opacity-70"
-  const activePublishStyle = "px-2 py-0.5 bg-sky-950 font-semibold rounded-md hover:bg-sky-900 active:text-green-400"
-  const publishBtnStyle = imageFile ? activePublishStyle : defaultPublishStyle;
-
-  const handlePublishPost = async () => {
-    if (!imageFile) {
-      console.error("No image file to upload.");
-      return; // Exit if there is no image
-    }
-
-    dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: true });
-    const postRef = doc(collection(db, "userPosts"));
-    const storageRef = ref(storage, `contentImages/${globalState.userId}/${postRef.id}`); // Use postRef.id for the file name
-
-    try {
-      // Upload the image file
-      await uploadBytes(storageRef, imageFile);
-      // Get the download URL
-      const downloadURL = await getDownloadURL(storageRef);
-
-      const newPost = {
-        postId: postRef.id,
-        userId: globalState.userId,
-        content: downloadURL, // Store the image URL
-        textContent: postContentText, // Add any text content here if needed
-        dateCreated: Timestamp.fromDate(new Date()), // Converts to Firestore Timestamp
-        likes: 0,
-        comments: []
-      };
-
-      // Set the document in Firestore
-      await setDoc(postRef, newPost);
-      dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: false });
-      console.log("Post published successfully:", newPost);
-    } catch (error) {
-      console.error("Error publishing post:", error);
-    }
-  };
+  const { imageName } = sidebarState;
 
 
   const handleFileChange = (event) => {
@@ -112,14 +68,7 @@ export default function MainPagePostModalBtnSection() {
         >
           Cancel
         </button>
-        <button
-          type="button"
-          className={publishBtnStyle}
-          onClick={handlePublishPost}
-          disabled={!imageFile || !postContentText}
-        >
-          Publish
-        </button>
+        <MainPagePublishButton />
       </div>
     </div>
   )
