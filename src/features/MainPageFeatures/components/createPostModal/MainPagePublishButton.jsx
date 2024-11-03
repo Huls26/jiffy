75c2774c33223fdc549ed2e2e656c661f466dfd3
Jules <julesmoreno66@gmail.com>
2024-11-03 14:36:58 +0800

    src/features/MainPageFeatures/components/createPostModal/MainPagePublishButton.jsx: removed some code compose utilities

--- src/features/MainPageFeatures/components/createPostModal/MainPagePublishButton.jsx
+++ src/features/MainPageFeatures/components/createPostModal/MainPagePublishButton.jsx
@@ -1,19 +1,15 @@
 import { GlobalContext } from '@/contexts/GlobalContextProvider';
 import { reducerContext } from "@/contexts/ReducerContextProvider";
-import { db, storage } from '@/lib/fb';
 
-import { Timestamp, collection, doc, setDoc } from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useContext } from "react";
+import publishBtnStyle from '../../styles/publishBtnStyle';
+import postStoreRef from '../../utils/createPostModal/postStorageRef';
+import uploadImageWithURL from '../../utils/createPostModal/uploadImageWithURL';
 
 export default function MainPagePublishButton() {
   const [globalState] = useContext(GlobalContext);
   const [sidebarState, dispatch] = useContext(reducerContext);
   const { imageFile, postContentText } = sidebarState;
-
-  const defaultPublishStyle = ("px-2 py-0.5 bg-sky-950 font-semibold rounded-md opacity-70");
-  const activePublishStyle = ("px-2 py-0.5 bg-sky-950 font-semibold rounded-md hover:bg-sky-900 active:text-green-400");
-  const publishBtnStyle = imageFile ? activePublishStyle : defaultPublishStyle;
 
   const publishContent = async () => {
     if (!imageFile) {
@@ -22,27 +18,10 @@ export default function MainPagePublishButton() {
     }
 
     dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: true });
-    const postRef = doc(collection(db, "userPosts"));
-    const storageRef = ref(storage, `contentImages/${globalState.userId}/${postRef.id}`);
+    const { postRef, storageRef } = postStoreRef(globalState);
 
     try {
-      // Upload the image file
-      await uploadBytes(storageRef, imageFile);
-      // Get the download URL
-      const downloadURL = await getDownloadURL(storageRef);
-
-      const newPost = {
-        postId: postRef.id,
-        userId: globalState.userId,
-        content: downloadURL, // Store the image URL
-        textContent: postContentText, // Add any text content here if needed
-        dateCreated: Timestamp.fromDate(new Date()), // Converts to Firestore Timestamp
-        likes: 0,
-        comments: []
-      };
-
-      // Set the document in Firestore
-      await setDoc(postRef, newPost);
+      uploadImageWithURL({ storageRef, postRef, sidebarState, userId: globalState.userId })
       dispatch({ type: "UPDATE_POST_MODAL_LOADING", payload: false });
     } catch (error) {
       console.error("Error publishing post:", error);
@@ -52,7 +31,7 @@ export default function MainPagePublishButton() {
   return (
     <button
       type="button"
-      className={publishBtnStyle}
+      className={publishBtnStyle(imageFile)}
       onClick={publishContent}
       disabled={!imageFile || !postContentText}
     >
