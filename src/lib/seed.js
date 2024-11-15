39ab6487a5dea485cd81062c3e0a51adb23064de
Jules <julesmoreno66@gmail.com>
2024-11-15 16:06:03 +0800

    src/lib/seed.js: added imports

--- src/lib/seed.js
+++ src/lib/seed.js
@@ -14,8 +14,8 @@ export function seedDatabase(db) {
       fullName: "Karl Hadwen",
       email: "karlhadwen@gmail.com",
       password: "123456",
-      following: ["2"],
-      followers: ["2", "3", "4"],
+      following: { 2: "2" }, // No change needed here
+      followers: { 2: "2", 3: "3", 4: "4" },
       followersCount: 3,
       dateCreated: Date.now(),
       photoURL:
@@ -27,8 +27,10 @@ export function seedDatabase(db) {
       fullName: "Raffaello Sanzio da Urbino",
       email: "raphael@sanzio.com",
       password: "123456",
-      following: [], // No users followed yet
-      followers: ["NvPY9M9MzFTARQ6M816YAzDJxZ72"],
+      following: {}, // No users followed yet
+      followers: {
+        NvPY9M9MzFTARQ6M816YAzDJxZ72: "NvPY9M9MzFTARQ6M816YAzDJxZ72",
+      },
       followersCount: 1,
       dateCreated: Date.now(),
       photoURL:
@@ -40,8 +42,10 @@ export function seedDatabase(db) {
       fullName: "Salvador Dalí",
       email: "salvador@dali.com",
       password: "123456",
-      following: ["4"],
-      followers: ["NvPY9M9MzFTARQ6M816YAzDJxZ72"],
+      following: { 4: "4" }, // Updated from array to object
+      followers: {
+        NvPY9M9MzFTARQ6M816YAzDJxZ72: "NvPY9M9MzFTARQ6M816YAzDJxZ72",
+      }, // Updated from array to object
       followersCount: 1,
       dateCreated: Date.now(),
       photoURL:
@@ -53,8 +57,10 @@ export function seedDatabase(db) {
       fullName: "George Orwell",
       email: "george@orwell.com",
       password: "123456",
-      following: [],
-      followers: ["NvPY9M9MzFTARQ6M816YAzDJxZ72"],
+      following: {}, // Updated from array to object
+      followers: {
+        NvPY9M9MzFTARQ6M816YAzDJxZ72: "NvPY9M9MzFTARQ6M816YAzDJxZ72",
+      }, // Updated from array to object
       followersCount: 1,
       dateCreated: Date.now(),
       photoURL:
@@ -66,8 +72,8 @@ export function seedDatabase(db) {
       fullName: "Vincent van Gogh",
       email: "vincent@vangogh.com",
       password: "123456",
-      following: ["3"],
-      followers: ["2", "4"],
+      following: { 3: "3" }, // Updated from array to object
+      followers: { 2: "2", 4: "4" }, // Updated from array to object
       followersCount: 2,
       dateCreated: Date.now(),
       photoURL:
@@ -79,8 +85,8 @@ export function seedDatabase(db) {
       fullName: "Ernest Hemingway",
       email: "ernest@hemingway.com",
       password: "123456",
-      following: ["4"],
-      followers: ["1"],
+      following: { 4: "4" }, // Updated from array to object
+      followers: { 1: "1" }, // Updated from array to object
       followersCount: 1,
       dateCreated: Date.now(),
       photoURL:
@@ -92,8 +98,8 @@ export function seedDatabase(db) {
       fullName: "William Shakespeare",
       email: "william@shakespeare.com",
       password: "123456",
-      following: [],
-      followers: ["1", "2"],
+      following: {},
+      followers: { 1: "1", 2: "2" },
       followersCount: 2,
       dateCreated: Date.now(),
       photoURL:
@@ -105,8 +111,8 @@ export function seedDatabase(db) {
       fullName: "Jane Austen",
       email: "jane@austen.com",
       password: "123456",
-      following: ["2", "6"],
-      followers: ["1"],
+      following: { 2: "2", 6: "6" }, // Updated from array to object
+      followers: { 1: "1" }, // Updated from array to object
       followersCount: 1,
       dateCreated: Date.now(),
       photoURL:
