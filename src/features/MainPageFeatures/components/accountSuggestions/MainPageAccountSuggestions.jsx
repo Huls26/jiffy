88c0c3515f888624c4f91755693c47406ec648f3
Jules <julesmoreno66@gmail.com>
2024-11-21 14:04:29 +0800

    src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx: destructured sidebarLoading line 8

--- src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
@@ -5,11 +5,12 @@ import MainPageAccountSuggestionTitle from './MainPageAccountSuggestionTitle';
 import MainPageNoSuggestedUsersMessage from './MainPageNoSuggestedUsersMessage';
 
 export default function MainPageAccountSuggestions() {
-  const { suggestedUsersList, fetchUserSuggestions } = useFetchUserSuggestions()
+  const { suggestedUsersList, fetchUserSuggestions, sidebarIsLoading } = useFetchUserSuggestions()
 
   console.log("check why this re-render");
   console.log("Lazy load unecessary files during the first render");
   console.log("check inspect network");
+  console.log(sidebarIsLoading);
   return (
     <section className="pt-2 pb-2 text-gray-200">
       <MainPageAccountSuggestionTitle
@@ -23,7 +24,10 @@ export default function MainPageAccountSuggestions() {
       <MainPageAccountSuggestionListDisplay
         suggestedUsersList={suggestedUsersList}
         onClick={fetchUserSuggestions}
+        sidebarIsLoading={sidebarIsLoading}
       />
+
+      {/* <MainPageAccountSuggestionListDisplay /> */}
 
       <SuggestedUsersCloseBtn
         onClick={fetchUserSuggestions}
