ed120154ab81f94c2174091ac5a03d8658e0626b
Jules <julesmoreno66@gmail.com>
2024-11-22 14:11:03 +0800

    src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx: update components added context

--- src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
+++ src/features/MainPageFeatures/components/accountSuggestions/MainPageAccountSuggestions.jsx
@@ -1,16 +1,20 @@
+import { reducerContext } from "@/contexts/ReducerContextProvider";
 import useFetchUserSuggestions from "../../hooks/useFetchUserSuggestions";
 import SuggestedUsersCloseBtn from "../buttons/SuggestedUsersCloseBtn";
 import MainPageAccountSuggestionListDisplay from "./MainPageAccountSuggestionListDisplay";
 import MainPageAccountSuggestionTitle from './MainPageAccountSuggestionTitle';
 import MainPageNoSuggestedUsersMessage from './MainPageNoSuggestedUsersMessage';
 
+import { useContext } from "react";
+
 export default function MainPageAccountSuggestions() {
-  const { suggestedUsersList, fetchUserSuggestions, sidebarIsLoading } = useFetchUserSuggestions()
+  const [sidebarState, dispatch] = useContext(reducerContext);
+  const { suggestedUsersList, sidebarIsLoading } = sidebarState
+  const { fetchUserSuggestions } = useFetchUserSuggestions(dispatch);
 
   console.log("check why this re-render");
   console.log("Lazy load unecessary files during the first render");
   console.log("check inspect network");
-  console.log(sidebarIsLoading);
   return (
     <section className="pt-2 pb-2 text-gray-200">
       <MainPageAccountSuggestionTitle
@@ -26,8 +30,6 @@ export default function MainPageAccountSuggestions() {
         onClick={fetchUserSuggestions}
         sidebarIsLoading={sidebarIsLoading}
       />
-
-      {/* <MainPageAccountSuggestionListDisplay /> */}
 
       <SuggestedUsersCloseBtn
         onClick={fetchUserSuggestions}
