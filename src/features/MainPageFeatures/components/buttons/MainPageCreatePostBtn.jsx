6363d568ef1483cbacb92e96d833a3de7146c886
Jules <julesmoreno66@gmail.com>
2024-11-23 14:23:24 +0800

    src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx: added context && displayPostModal() line 8

--- src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
+++ src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
@@ -1,12 +1,19 @@
+import { reducerContext } from "@/contexts/ReducerContextProvider";
 import PropTypes from "prop-types"
+import { useContext } from "react";
 
-export default function MainPageCreatePostBtn({ onClick }) {
+export default function MainPageCreatePostBtn() {
+  const [, dispatch] = useContext(reducerContext)
+
+  function displayPostModal() {
+    dispatch({ type: "UPDATE_DISPLAY_POST_MODAL" })
+  }
   return (
     <div className="ml-auto">
       <button
         type="button"
         className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:text-gray-600 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-2"
-        onClick={onClick}
+        onClick={displayPostModal}
       >
         Post
       </button>
