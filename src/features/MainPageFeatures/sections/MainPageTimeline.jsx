69417b74c781dba73f39ea5352709a71a874a9f9
Jules <julesmoreno66@gmail.com>
2024-11-26 14:20:03 +0800

    src/features/MainPageFeatures/sections/MainPageTimeline.jsx: implement getDocs line 10

--- src/features/MainPageFeatures/sections/MainPageTimeline.jsx
+++ src/features/MainPageFeatures/sections/MainPageTimeline.jsx
@@ -1,3 +1,19 @@
+import { db } from '@/lib/fb';
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+
 export default function MainPageTimeline() {
+  const [userPosts, setUserPosts] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const querySnapshot = await getDocs(collection(db, "userPosts"));
+
+      console.log(querySnapshot.docs);
+    }
+
+    fetchData();
+  }, [])
+
   return <div className="flex-1 border-white border-2">MainPageTimeline</div>;
 }
