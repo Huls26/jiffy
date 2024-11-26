608036c9ff0c57951c3f7f5e2049b2da57512ae5
Jules <julesmoreno66@gmail.com>
2024-11-26 14:56:35 +0800

    src/features/MainPageFeatures/sections/MainPageTimeline.jsx: render users posts good for now to fix later

--- src/features/MainPageFeatures/sections/MainPageTimeline.jsx
+++ src/features/MainPageFeatures/sections/MainPageTimeline.jsx
@@ -3,17 +3,33 @@ import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from 'react';
 
 export default function MainPageTimeline() {
-  const [userPosts, setUserPosts] = useState(null);
+  const [userPosts, setUserPostsSnapshot] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       const querySnapshot = await getDocs(collection(db, "userPosts"));
-
-      console.log(querySnapshot.docs);
+      setUserPostsSnapshot(() => querySnapshot.docs);
     }
 
     fetchData();
   }, [])
 
-  return <div className="flex-1 border-white border-2">MainPageTimeline</div>;
+  console.log(userPosts && userPosts[0].data());
+
+  return (
+    <div className="flex-1 border-white border-2">
+      MainPageTimeline
+      {userPosts?.map((u) => {
+        const userPost = u.data();
+
+        return (
+          <div
+            key={userPost.postId}
+            className='border-2 border-black'
+          >
+            <h1>{userPost.textContent}</h1>
+            <img src={userPost.content} alt={`users post text content ${userPost.textContent}`} />
+          </div>);
+      })}
+    </div>);
 }
