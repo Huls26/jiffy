cbd0cb9161d8df8e33e02a153e685dc9472ebce6
Jules <julesmoreno66@gmail.com>
2024-11-28 15:21:18 +0800

    src/pages/MainPage.jsx: added remainders

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,8 +5,11 @@ import usePageTitle from "@/hooks/usePageTitle";
 export default function MainPage() {
   usePageTitle("J I F F Y");
 
+  // remainders:
+  // dont remove text-center className
+  // it will affect some buttons
   return (
-    <main className="">
+    <main className="text-center">
       <MainPageFeatures />
     </main>
   );
