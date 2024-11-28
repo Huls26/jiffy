9b6825a0f59ff1a7fd49bcddaee9e6793eeb544d
Jules <julesmoreno66@gmail.com>
2024-11-28 15:00:05 +0800

    src/hooks/useAuthHandler/index.jsx: removed file

--- src/hooks/useAuthHandler/index.jsx
+++
@@ -1,8 +0,0 @@
-// import { reducerContext } from "@/contexts/ReducerContextProvider";
-
-// export default function useAuthHandler() {
-//   const [globalStateContext, dispatch] = useContext(reducerContext);
-//   return (
-//     <div>useAuthHandler</div>
-//   )
-// }
