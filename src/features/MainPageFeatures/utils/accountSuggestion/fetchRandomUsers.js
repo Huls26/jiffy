98fe6eb61ad7597c5ea48c31843ab26bacdf0ba4
Jules <julesmoreno66@gmail.com>
2024-11-30 14:00:59 +0800

    src/features/MainPageFeatures/utils/accountSuggestion/fetchRandomUsers.js: update

--- src/features/MainPageFeatures/utils/accountSuggestion/fetchRandomUsers.js
+++ src/features/MainPageFeatures/utils/accountSuggestion/fetchRandomUsers.js
@@ -1,5 +1,13 @@
 import { db } from "@/lib/fb";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  where,
+} from "firebase/firestore";
 
 /**
  * Fetches random user profiles from the Firestore collection 'users' excluding the current user.
@@ -10,36 +18,19 @@ import { collection, getDocs, query, where } from "firebase/firestore";
  * If no suggestions are available, an empty array is returned.
  */
 export default async function fetchUsers(userId) {
-  console.log("where(userId, not-in, [...following, userId]");
-  console.log("where(following, array-contains, userId)");
-  const q = query(
-    collection(db, "users"),
-    where("following", "array-contains", userId),
-  );
-  const docSnap = await getDocs(q);
-
-  console.log("testing");
-  console.log(docSnap.docs);
   try {
-    const q = query(collection(db, "users"), where("userId", "!=", userId));
-    // const q2 = query(collection(db, "users"), where("following", "", );
+    const docRef = doc(db, "users", userId);
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data();
+
+    const q = query(
+      collection(db, "users"),
+      where("userId", "not-in", [...docData.following, userId]),
+      limit(10),
+    );
+
     const querySnapshot = await getDocs(q);
-
-    // Filter users who do not have userId in their followers array
-    const filteredUsers = querySnapshot.docs.filter((doc) => {
-      const docUser = doc.data();
-
-      // Check if userId is not included in followers
-      console.log("update this filter");
-      return !docUser.followers.includes(userId);
-    });
-
-    if (filteredUsers.length === 0) {
-      return []; // Return an empty array if no suggestions available
-    }
-
-    // Randomly select up to 3 users
-    const randomUsers = filteredUsers
+    const randomUsers = querySnapshot.docs
       .sort(() => Math.random() - 0.5) // Shuffle array
       .slice(0, 3) // Select first 3 users from shuffled list
       .map((doc) => {
@@ -55,8 +46,48 @@ export default async function fetchUsers(userId) {
       });
 
     return randomUsers;
-  } catch (error) {
-    console.error("Error fetching user suggestions:", error);
-    return [];
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    return []; // Return an empty array if an error occurs
   }
+
+  // try {
+  //   const q = query(collection(db, "users"), where("userId", "!=", userId));
+  //   // const q2 = query(collection(db, "users"), where("following", "", );
+  //   const querySnapshot = await getDocs(q);
+
+  //   // Filter users who do not have userId in their followers array
+  //   const filteredUsers = querySnapshot.docs.filter((doc) => {
+  //     const docUser = doc.data();
+
+  //     // Check if userId is not included in followers
+  //     console.log("update this filter");
+  //     return !docUser.followers.includes(userId);
+  //   });
+
+  //   if (filteredUsers.length === 0) {
+  //     return []; // Return an empty array if no suggestions available
+  //   }
+
+  //   // Randomly select up to 3 users
+  //   const randomUsers = filteredUsers
+  //     .sort(() => Math.random() - 0.5) // Shuffle array
+  //     .slice(0, 3) // Select first 3 users from shuffled list
+  //     .map((doc) => {
+  //       const userProfile = doc.data();
+  //       return {
+  //         userId: userProfile.userId,
+  //         username: userProfile.username,
+  //         email: userProfile.email,
+  //         photoURL: userProfile.photoURL,
+  //         followersCount: userProfile.followersCount,
+  //         followers: userProfile.followers,
+  //       };
+  //     });
+
+  //   return randomUsers;
+  // } catch (error) {
+  //   console.error("Error fetching user suggestions:", error);
+  //   return [];
+  // }
 }
