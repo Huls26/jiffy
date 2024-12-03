301757aea834657ceace0c3ec9899b8c138d209d
Jules <julesmoreno66@gmail.com>
2024-12-03 14:46:59 +0800

    src/features/MainPageFeatures/components/timeline/: created

---
+++ src/features/MainPageFeatures/components/timeline/MainPageFilterQuery.jsx
@@ -0,0 +1,24 @@
+export default function MainPageFilterQuery() {
+  return (
+    <section section className='inline-flex' >
+      <button
+        type='button'
+        className="bg-sky-500 bg-gray-900 hover:bg-gray-700 text-gray-200 font-bold py-1 px-3 rounded-l"
+      >
+        All
+      </button>
+      <button
+        type='button'
+        className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3"
+      >
+        Likes
+      </button>
+      <button
+        type='button'
+        className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3 rounded-r"
+      >
+        Following
+      </button>
+    </section >
+  )
+}
