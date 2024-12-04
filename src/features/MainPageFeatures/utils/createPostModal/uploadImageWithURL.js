6d182d315c9bd74ac20c3377b267303832eaef00
Jules <julesmoreno66@gmail.com>
2024-12-04 13:55:09 +0800

    src/features/MainPageFeatures/utils/createPostModal/uploadImageWithURL.js: added parameter and update newPost line 16

--- src/features/MainPageFeatures/utils/createPostModal/uploadImageWithURL.js
+++ src/features/MainPageFeatures/utils/createPostModal/uploadImageWithURL.js
@@ -5,22 +5,28 @@ export default async function uploadImageWithURL({
   storageRef,
   postRef,
   sidebarState,
-  userId,
+  userContext,
 }) {
   const { imageFile, postContentText } = sidebarState;
+  const { photoURL, email, username, userId } = userContext;
   await uploadBytes(storageRef, imageFile);
   // Get the download URL
   const downloadURL = await getDownloadURL(storageRef);
 
   const newPost = {
     postId: postRef.id,
-    userId: userId,
+    userId,
     content: downloadURL, // Store the image URL
     textContent: postContentText, // Add any text content here if needed
     dateCreated: Timestamp.fromDate(new Date()), // Converts to Firestore Timestamp
     likes: 0,
     comments: [],
+    photoURL,
+    username,
+    email,
   };
+
+  console.log(newPost);
 
   await setDoc(postRef, newPost);
 }
