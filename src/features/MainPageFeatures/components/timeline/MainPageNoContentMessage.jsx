ef896d43487754aff7d38af108c20d0b384503d6
Jules <julesmoreno66@gmail.com>
2024-12-08 14:14:53 +0800

    src/features/MainPageFeatures/components/timeline/MainPageNoContentMessage.jsx: created

---
+++ src/features/MainPageFeatures/components/timeline/MainPageNoContentMessage.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types"
+
+export default function MainPageNoContentMessage({ userPosts }) {
+  if (userPosts?.length === 0) {
+    return (
+      <h1 className='mt-5 font-bold sm:text-xl text-cyan-500 text-pretty'>Sorry No Content Right Now! :(</h1>
+    )
+  }
+}
+
+MainPageNoContentMessage.propTypes = {
+  userPosts: PropTypes.arrayOf(PropTypes.object),
+}
