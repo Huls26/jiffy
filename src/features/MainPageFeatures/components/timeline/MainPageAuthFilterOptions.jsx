a3eae6e44314205d29667c6f13050146756349b8
Jules <julesmoreno66@gmail.com>
2024-12-08 14:06:24 +0800

    src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx: update bottom margin line 11

--- src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
@@ -8,7 +8,7 @@ export default function MainPageAuthFilterOptions({ isDisplay = [] }) {
 
   if (!globalState.userLogin) {
     return (
-      <h1 className="mb-3 px-4 font-semibold text-sm sm:text-lg text-gray-100 leading-5">
+      <h1 className="mb-5 px-4 font-semibold text-sm sm:text-lg text-gray-100 leading-5">
         Welcome! Create an account or log in to explore the app and share your
         posts.
       </h1>);
