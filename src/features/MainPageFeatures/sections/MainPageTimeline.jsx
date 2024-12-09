6b069b5fdee2037049c69f469a6ae99387b69830
Jules <julesmoreno66@gmail.com>
2024-12-09 14:32:55 +0800

    src/features/MainPageFeatures/sections/MainPageTimeline.jsx: implement useRealTimeUserPosts

--- src/features/MainPageFeatures/sections/MainPageTimeline.jsx
+++ src/features/MainPageFeatures/sections/MainPageTimeline.jsx
@@ -1,53 +1,11 @@
 import MainPageAuthFilterOptions from '../components/timeline/MainPageAuthFilterOptions';
 import MainPageNoContentMessage from '../components/timeline/MainPageNoContentMessage';
 import MainPageUserProfileLink from '../components/userInfo/MainPageUserProfileLink';
+import useRealtimeUserPosts from '../hooks/useRealtimeUserPosts';
 import formatRelativeTime from '../utils/timeline/formatRelativeTime';
 
-import { db } from '@/lib/fb';
-import { collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
-import { useEffect, useState } from 'react';
-
 export default function MainPageTimeline() {
-  const [userPosts, setUserPostsSnapshot] = useState([]);
-
-  useEffect(() => {
-    // Create a query with constraints
-    const myQuery = query(
-      collection(db, "userPosts"),
-      orderBy('dateCreated', 'desc'),
-      limit(10),
-    );
-    // Listen for real-time updates using the query
-    const unsubscribe = onSnapshot(myQuery, (snapshot) => {
-      if (snapshot.empty) {
-        console.log("No matching documents.");
-        setUserPostsSnapshot([]);
-      }
-
-      const posts = snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        postId: doc.id, // Use document ID as postId
-      }));
-      setUserPostsSnapshot(posts);
-      // for (const change of snapshot.docChanges()) {
-      //   if (change.type === "added") {
-      //     setUserPostsSnapshot((prevDocs) => [change.doc, ...prevDocs]); // Save document data
-      //     console.log("New document added: ", change.doc.data());
-      //   } else if (change.type === "modified") {
-      //     console.log("Document modified: ", change.doc.data());
-      //     // Update logic for modified documents, if needed
-      //   } else if (change.type === "removed") {
-      //     console.log("Document removed: ", change.doc.data());
-      //     // Update logic for removed documents, if needed
-      //   }
-      // }
-    });
-
-    // Clean up the listener on component unmount or dependency change
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  const userPosts = useRealtimeUserPosts();
 
   return (
     <main className="mt-1 sm:mx-3 pt-3 flex-1 text-gray-100">
