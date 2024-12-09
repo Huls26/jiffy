17a079de94830aa625291442b785f12396fba740
Jules <julesmoreno66@gmail.com>
2024-12-09 14:50:15 +0800

    src/features/MainPageFeatures/sections/MainPageTimeline.jsx: implement MainPagePostsFeed line 12

--- src/features/MainPageFeatures/sections/MainPageTimeline.jsx
+++ src/features/MainPageFeatures/sections/MainPageTimeline.jsx
@@ -1,8 +1,7 @@
 import MainPageAuthFilterOptions from '../components/timeline/MainPageAuthFilterOptions';
 import MainPageNoContentMessage from '../components/timeline/MainPageNoContentMessage';
-import MainPageUserProfileLink from '../components/userInfo/MainPageUserProfileLink';
-import useRealtimeUserPosts from '../hooks/useRealtimeUserPosts';
-import formatRelativeTime from '../utils/timeline/formatRelativeTime';
+import MainPagePostsFeed from '../components/timeline/MainPagePostsFeed';
+import useRealtimeUserPosts from '../hooks/useRealtimeUserPosts';;
 
 export default function MainPageTimeline() {
   const userPosts = useRealtimeUserPosts();
@@ -10,34 +9,7 @@ export default function MainPageTimeline() {
   return (
     <main className="mt-1 sm:mx-3 pt-3 flex-1 text-gray-100">
       <MainPageAuthFilterOptions isDisplay={userPosts} />
-
-      <section className='grid place-self-center'>
-        {userPosts?.map((userPost) => {
-          const relativeTime = formatRelativeTime(userPost?.dateCreated);
-
-          return (
-            <div
-              key={userPost.postId}
-              className='my-1 space-y-2 bg-slate-950 text-start min-w-[270px] max-w-xl sm:rounded-lg border-4 border-gray-950'
-            >
-              <div className='mx-2 m-1 flex justify-between'>
-                <MainPageUserProfileLink
-                  to={`profile/${userPost.username}`}
-                  photoURL={userPost.photoURL}
-                  username={userPost.username}
-                  email={userPost.email}
-                />
-                <h1 className='font-semibold text-xs text-gray-400'>{relativeTime} ago</h1>
-              </div>
-              <h1 className='ml-2 sm:text-xl'>{userPost.textContent}</h1>
-              <img
-                src={userPost.content}
-                alt={`users post content: ${userPost.textContent}`}
-              />
-            </div>);
-        })}
-      </section>
-
+      <MainPagePostsFeed userPosts={userPosts} />
       <MainPageNoContentMessage userPosts={userPosts} />
     </main>);
 }
