42912705fc53b7a472480114b031ebf07623b14f
Jules <julesmoreno66@gmail.com>
2024-12-10 15:00:22 +0800

    src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx: added media queries line 22

--- src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
@@ -19,7 +19,7 @@ export default function MainPageAuthFilterOptions({ isDisplay = [] }) {
   }
 
   return (
-    <section className='mb-3 inline-flex' >
+    <section className='text-sm sm:text-base mb-3 inline-flex'>
       <button
         type='button'
         className="bg-sky-500 bg-gray-900 hover:bg-gray-700 text-gray-200 font-bold py-1 px-3 rounded-l"
