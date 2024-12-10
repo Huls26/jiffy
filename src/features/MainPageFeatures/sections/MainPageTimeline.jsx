cda4b6560ade04e225afc4719a656deec2c3e208
Jules <julesmoreno66@gmail.com>
2024-12-10 15:03:07 +0800

    src/features/MainPageFeatures/sections/MainPageTimeline.jsx: added new task to do line 8

--- src/features/MainPageFeatures/sections/MainPageTimeline.jsx
+++ src/features/MainPageFeatures/sections/MainPageTimeline.jsx
@@ -5,13 +5,15 @@ import useRealtimeUserPosts from '../hooks/useRealtimeUserPosts';;
 
 export default function MainPageTimeline() {
   const userPosts = useRealtimeUserPosts();
+  console.log("set image min-height user posts")
 
   return (
     <main className="mt-1 sm:mx-3 pt-3 flex-1 text-gray-100">
       <MainPageAuthFilterOptions isDisplay={userPosts} />
       <MainPagePostsFeed userPosts={userPosts} />
       <MainPageNoContentMessage userPosts={userPosts} />
-    </main>);
+    </main>
+  );
 }
 
 MainPageTimeline.whyDidYouRender = true;
