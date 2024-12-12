54f5d8473d4b2e9f1f103711ef693f1d461acb50
Jules <julesmoreno66@gmail.com>
2024-12-12 13:47:52 +0800

    src/features/MainPageFeatures/components/timeline/MainPagePostsFeed.jsx: converse MainPagePostCard line 11

--- src/features/MainPageFeatures/components/timeline/MainPagePostsFeed.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPagePostsFeed.jsx
@@ -1,5 +1,4 @@
-import formatRelativeTime from '../../utils/timeline/formatRelativeTime';
-import MainPageUserProfileLink from '../userInfo/MainPageUserProfileLink';
+import MainPagePostCard from './MainPagePostCard';
 
 import PropTypes from "prop-types";
 
@@ -7,45 +6,13 @@ export default function MainPagePostsFeed({ userPosts }) {
   return (
     <section className='grid place-self-center space-y-3'>
       {userPosts?.map((userPost) => {
-        const relativeTime = formatRelativeTime(userPost?.dateCreated);
 
         return (
-          <div
+          <MainPagePostCard
             key={userPost.postId}
-            className='space-y-2 bg-slate-950 text-start min-w-[270px] max-w-xl sm:rounded-lg border-4 border-gray-950'
-          >
-            <div className='mx-2 m-1 flex justify-between'>
-              <MainPageUserProfileLink
-                to={`profile/${userPost.username}`}
-                photoURL={userPost.photoURL}
-                username={userPost.username}
-                email={userPost.email}
-              />
-              <h1 className='font-semibold text-xs text-gray-400'>{relativeTime} ago</h1>
-            </div>
-            <h1 className='ml-2 sm:text-xl'>{userPost.textContent}</h1>
-            <img
-              className="min-h-[180px] aspect-video object-cover"
-              src={userPost.content}
-              alt={`users post content: ${userPost.textContent}`}
-            />
-            <div className='p-2 text-sm sm:text-base flex justify-between'>
-              <button
-                type='button'
-                className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500  text-gray-200 font-bold py-1 px-3 rounded-l"
-                aria-label="Show All Posts"
-              >
-                Like
-              </button>
-              <button
-                type='button'
-                className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3 rounded-r"
-                aria-label="Show Liked Posts"
-              >
-                Comment
-              </button>
-            </div>
-          </div>);
+            userPost={userPost}
+          />
+        );
       })}
     </section>
   )
