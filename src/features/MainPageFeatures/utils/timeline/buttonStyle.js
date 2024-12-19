38087e3703447d60d061b31dc10d2bf92da75374
Jules <julesmoreno66@gmail.com>
2024-12-19 14:20:21 +0800

    src/features/MainPageFeatures/utils/timeline/buttonStyle.js: added comment/docString

--- src/features/MainPageFeatures/utils/timeline/buttonStyle.js
+++ src/features/MainPageFeatures/utils/timeline/buttonStyle.js
@@ -1,3 +1,13 @@
+/**
+ * A utility function that returns a different CSS style based on the isActiveButton parameter.
+ *
+ * @param {boolean} isActiveButton - A boolean indicating whether the button is active or not.
+ *
+ * @returns {string} The CSS style to be applied to the button.
+ * @returns {string} defaultBtnStyle - The default CSS style for a non-active button.
+ * @returns {string} activeBtnStyle - The CSS style for an active button.
+ */
+
 export default function buttonStyle(isActiveButton) {
   const defaultBtnStyle =
     "bg-gray-900 active:bg-sky-500 hover:opacity-75 text-gray-200 font-bold py-1 px-3 rounded-l";
