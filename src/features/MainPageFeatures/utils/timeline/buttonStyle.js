c3c7983d4a2eb6503d60ef4b2f1a95d5083bfbbd
Jules <julesmoreno66@gmail.com>
2024-12-22 07:45:20 +0800

    src/features/MainPageFeatures/utils/timeline/buttonStyle.js: update button style

--- src/features/MainPageFeatures/utils/timeline/buttonStyle.js
+++ src/features/MainPageFeatures/utils/timeline/buttonStyle.js
@@ -10,9 +10,9 @@
 
 export default function buttonStyle(isActiveButton) {
   const defaultBtnStyle =
-    "bg-gray-900 active:bg-sky-500 hover:opacity-75 text-gray-200 font-bold py-1 px-3 rounded-l";
+    "bg-gray-900 active:bg-sky-500 hover:opacity-75 text-gray-200 font-bold py-1 px-3 rounded";
   const activeBtnStyle =
-    "bg-sky-500 hover:opacity-75 active:bg-sky-500  text-gray-200 font-bold py-1 px-3 rounded-l";
+    "bg-sky-500 hover:opacity-75 active:bg-sky-500  text-gray-200 font-bold py-1 px-3 rounded";
   const buttonStyle = isActiveButton ? activeBtnStyle : defaultBtnStyle;
 
   return buttonStyle;
