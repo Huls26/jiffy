a3466e7d09e3fa158a6602d8d8a6e99fe42af2fd
Jules <julesmoreno66@gmail.com>
2024-12-24 13:29:27 +0800

    src/features/MainPageFeatures/components/timeline/MainPageUserPostCard.jsx: created

---
+++ src/features/MainPageFeatures/components/timeline/MainPageUserPostCard.jsx
@@ -0,0 +1,40 @@
+import formatRelativeTime from '../../utils/timeline/formatRelativeTime';
+import MainPageUserProfileLink from '../userInfo/MainPageUserProfileLink';
+
+import PropTypes from "prop-types";
+
+export default function MainPageUserPostCard({ userPost }) {
+  const relativeTime = formatRelativeTime(userPost?.dateCreated);
+
+  return (
+    <>
+      <div className='mx-2 m-1 flex justify-between'>
+        <MainPageUserProfileLink
+          to={`profile/${userPost.username}`}
+          photoURL={userPost.photoURL}
+          username={userPost.username}
+          email={userPost.email}
+        />
+        <h1 className='font-semibold text-xs text-gray-400 select-none'>{relativeTime} ago</h1>
+      </div >
+      <h1 className='ml-2 sm:text-xl'>{userPost.textContent}</h1>
+      <img
+        className="min-h-[180px] aspect-video object-cover"
+        src={userPost.content}
+        alt={`users post content: ${userPost.textContent}`}
+      />
+    </>
+  )
+}
+
+MainPageUserPostCard.propTypes = {
+  userPost: PropTypes.shape({
+    postId: PropTypes.string.isRequired,
+    dateCreated: PropTypes.any.isRequired,
+    textContent: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    photoURL: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+}
