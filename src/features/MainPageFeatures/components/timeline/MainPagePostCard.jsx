47265460d091eff69c1668e509cdad14af6d4ed7
Jules <julesmoreno66@gmail.com>
2025-01-19 13:42:32 +0800

    src/features/MainPageFeatures/components/timeline/MainPagePostCard.jsx: update component removed state

--- src/features/MainPageFeatures/components/timeline/MainPagePostCard.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPagePostCard.jsx
@@ -8,7 +8,6 @@ import MainPageUserPostCard from './MainPageUserPostCard';
 import "./style/MainPagePostCard.css";
 
 import PropTypes from "prop-types";
-import { useState } from "react";
 
 /**
  * MainPagePostCard component renders a user's post on the main page timeline.
@@ -29,14 +28,7 @@ export default function MainPagePostCard({ userPost }) {
     dispatch,
     currentUserId,
   } = usePostInteraction(userPost);
-  const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
-
-  console.log("Update classname line 87 & 98 create a variable for default style or readmore and readless style variable");
-  console.log("create a component for user's comment and separate state for toggle expand state");
   return (
     <section
       className='relative space-y-2 bg-slate-950 text-start min-w-[270px] max-w-xl sm:rounded-lg border-4 border-gray-950 cursor-pointer'
@@ -82,28 +74,8 @@ export default function MainPagePostCard({ userPost }) {
 
         {/* Comment */}
         <MainPageUserComment authUserPhoto={authUserPhoto} />
-        <div className="mb-4 flex items-center space-x-2">
-          {console.log("change photoURL to the user's who is commenting")}
-          <UserProfile photoURL={authUserPhoto} addedClassName={'w-10 h-10 hover:scale-110 shrink-0'} />
-          <div className="w-full">
-            <h1 className="mb-1 flex items-center justify-between font-semibold text-sky-400 leading-4">Username || email {<span className="text-gray-300 text-xs leading-3">date created</span>}</h1>
-            <p className="text-sm font-mono leading-4">The harmonic notes are flying and I'm happy with it. sample text right random words</p>
-            <button type="button" onClick={toggleExpand} className="text-blue-500 text-xs">
-              {isExpanded ? 'Read Less' : 'Read More'}
-            </button>
-          </div>
-        </div>
-        <div className="mb-4 flex items-center space-x-2">
-          {console.log("change photoURL to the user's who is commenting")}
-          <UserProfile photoURL={authUserPhoto} addedClassName={'w-10 h-10 hover:scale-110 shrink-0'} />
-          <div className="w-full">
-            <h1 className="mb-1 flex items-center justify-between font-semibold text-sky-400 leading-4">Username || email {<span className="text-gray-300 text-xs leading-3">date created</span>}</h1>
-            <p className={`text-sm font-mono leading-4 ${isExpanded ? '' : 'truncate-multiline'}`}>The harmonic notes are flying and I'm happy with it. sample text right random words added more words sample text testing is this correct.</p>
-            <button type="button" onClick={toggleExpand} className="text-blue-500 text-xs">
-              {isExpanded ? 'Read Less' : 'Read More'}
-            </button>
-          </div>
-        </div>
+        <MainPageUserComment authUserPhoto={authUserPhoto} />
+        <MainPageUserComment authUserPhoto={authUserPhoto} />
       </section>
     </section>);
 }
