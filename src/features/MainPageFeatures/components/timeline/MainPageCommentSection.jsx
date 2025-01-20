a1982322721d5331639cba0e645b38c4d6c3698e
Jules <julesmoreno66@gmail.com>
2025-01-20 14:06:19 +0800

    src/features/MainPageFeatures/components/timeline/MainPageCommentSection.jsx: added proptype

--- src/features/MainPageFeatures/components/timeline/MainPageCommentSection.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPageCommentSection.jsx
@@ -1,10 +1,13 @@
 import UserProfile from "@/components/UserProfile";
 import MainPageUserComment from "./MainPageUserComment";
 
+import PropTypes from "prop-types";
+
 export default function MainPageCommentSection({ authUserPhoto }) {
   return (
     <section className='p-2 pb-4 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 bg-slate-950 rounded-lg z-50 text-gray-300'>
       <h1 className="mb-2 text-gray-200 text-lg font-semibold">Comments [number of comments]</h1>
+
       {/* current user Comment input */}
       <label htmlFor="timeline-comment-input" className='mb-6 grid grid-cols-12 gap-2'>
         <UserProfile photoURL={authUserPhoto} addedClassName={'w-8 h-8 hover:scale-110'} />
@@ -23,4 +26,8 @@ export default function MainPageCommentSection({ authUserPhoto }) {
       <MainPageUserComment authUserPhoto={authUserPhoto} />
     </section>
   )
+}
+
+MainPageCommentSection.propTypes = {
+  authUserPhoto: PropTypes.string.isRequired,
 }
