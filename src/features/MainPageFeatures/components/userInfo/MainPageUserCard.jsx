d94e3458888b545feccefe0fd78b099d1f7d8758
Jules <julesmoreno66@gmail.com>
2025-01-22 13:34:49 +0800

    src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx: update component max-width line 14

--- src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx
+++ src/features/MainPageFeatures/components/userInfo/MainPageUserCard.jsx
@@ -11,7 +11,7 @@ export default function MainPageUserCard() {
   const { email, username, photoURL } = globalState;
 
   return (
-    <section className="md:max-w-80 p-1 pl-2 flex cursor-pointer">
+    <section className="md:max-w-md p-1 pl-2 flex cursor-pointer">
       <MainPageUserProfileLink
         to={`profile/${username}`}
         photoURL={photoURL}
