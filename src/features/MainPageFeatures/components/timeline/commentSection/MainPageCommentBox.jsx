9f99874de9643c208fd1abf3d88d5d6105b6f3ac
Jules <julesmoreno66@gmail.com>
2025-01-25 12:49:15 +0800

    created: src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentBox.jsx

---
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentBox.jsx
@@ -0,0 +1,38 @@
+import LoadingDot from "@/components/LoadingSkeleton/components/LoadingDot";
+import UserProfile from "@/components/UserProfile";
+
+import { useState } from "react";
+
+export default function MainPageCommentBox({ authUserPhoto }) {
+  const [commentValue, setCommentValue] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  return (
+    <>
+      {isLoading && <LoadingDot />}
+      <label htmlFor="timeline-comment-input" className='mb-6 flex space-x-2'>
+        <div>
+          <UserProfile photoURL={authUserPhoto} addedClassName={'w-8 h-8 hover:scale-110'} />
+        </div>
+        <input
+          name='timeline-comment-input'
+          type="text"
+          placeholder="Write a comment..."
+          className="w-full px-3 py-1 font-medium text-gray-950 text-sm sm:text-base rounded-full border-gray-950 focus:outline-none focus:ring-2 focus:ring-slate-600"
+          id='timeline-comment-input'
+          value={commentValue}
+          onChange={(e) => setCommentValue(e.target.value)}
+          maxLength="150"
+        />
+        <button
+          type="button"
+          className="px-2 bg-blue-500 rounded-full text-gray-300"
+          onClick={submitComment}
+          disabled={(commentValue.length === 0) || isLoading}
+        >
+          &#10149;
+        </button>
+      </label>
+    </>
+  )
+}
