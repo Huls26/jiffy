f7f01d677dc1e21161a18a964785913fed88ad7d
Jules <julesmoreno66@gmail.com>
2025-01-25 13:19:36 +0800

    created: src/features/MainPageFeatures/utils/commentSection/handleSubmitComment.js

---
+++ src/features/MainPageFeatures/utils/commentSection/handleSubmitComment.js
@@ -0,0 +1,33 @@
+import { db } from "@/lib/fb";
+
+import { Timestamp, collection, doc, setDoc } from "firebase/firestore";
+
+export async function submitComment(
+  setLoading,
+  setCommentValue,
+  postId,
+  commentValue,
+  userId,
+) {
+  setLoading(true);
+  try {
+    // Generate a document reference with an auto-generated ID
+    const commentRef = doc(collection(db, "userPosts", postId, "comments"));
+
+    // Get the auto-generated ID
+    const commentId = commentRef.id;
+
+    // Use the generated ID to create a new document
+    await setDoc(commentRef, {
+      commentId,
+      content: commentValue,
+      createdAt: Timestamp.fromDate(new Date()),
+      userId,
+    });
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  } finally {
+    setLoading(false);
+    setCommentValue("");
+  }
+}
