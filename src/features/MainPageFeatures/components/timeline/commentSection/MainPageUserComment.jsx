a490203840314028c2f2e9d24bc865da65d5d301
Jules <julesmoreno66@gmail.com>
2025-01-28 13:38:41 +0800

    refactor: display readmore button if multiline of paragraph

--- src/features/MainPageFeatures/components/timeline/commentSection/MainPageUserComment.jsx
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageUserComment.jsx
@@ -1,9 +1,9 @@
 import UserProfile from "@/components/UserProfile";
 import { db } from "@/lib/fb";
-import { useState } from "react";
 
 import { doc, getDoc } from "firebase/firestore";
 import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export default function MainPageUserComment({ userId, commentData }) {
@@ -11,6 +11,11 @@ export default function MainPageUserComment({ userId, commentData }) {
   const [searchParams] = useSearchParams();
   const commentId = searchParams.get("comment");
 
+  const paragraphRef = useRef(null);
+  const [isMultiLine, setIsMultiLine] = useState(false);
+
+
+  // toggle expand comment
   const toggleExpand = (event) => {
     event.stopPropagation();
     setIsExpanded(!isExpanded);
@@ -28,18 +33,34 @@ export default function MainPageUserComment({ userId, commentData }) {
     }
   }
 
+  // make this feature responsive
+  useEffect(() => {
+    if (paragraphRef.current) {
+      const paragraph = paragraphRef.current;
+
+      // Check if the element is truncated
+      const isElementTruncated = paragraph.scrollHeight > paragraph.clientHeight ||
+        paragraph.scrollWidth > paragraph.clientWidth;
+
+      // Check if the paragraph is multiline
+      setIsMultiLine(isElementTruncated);
+    }
+  }, []);
+
   return (
     <div className="mb-4 flex items-center space-x-2">
       {console.log("change photoURL to the user's who is commenting")}
-      <UserProfile photoURL={authUserPhoto} addedClassName={'w-10 h-10 hover:scale-110 shrink-0'} />
+      <UserProfile photoURL={''} addedClassName={'w-10 h-10 hover:scale-110 shrink-0'} />
       <div className="w-full">
         <h1 className="mb-1 flex items-center justify-between font-semibold text-sky-400 leading-4">Username || email {<span className="text-gray-300 text-xs leading-3">date created</span>}</h1>
-        <p className={`text-sm font-mono leading-4 ${isExpanded ? '' : 'truncate-multiline'}`}>
+        <p ref={paragraphRef} className={`text-sm font-mono leading-4 ${isExpanded ? '' : 'truncate-multiline'}`}>
           {commentData}
         </p>
-        <button type="button" onClick={toggleExpand} className="text-blue-500 text-xs">
-          {isExpanded ? 'Read Less' : 'Read More'}
-        </button>
+        {
+          isMultiLine && <button type="button" onClick={toggleExpand} className="text-blue-500 text-xs">
+            {isExpanded ? 'Read Less' : 'Read More'}
+          </button>
+        }
       </div>
     </div>
   )
