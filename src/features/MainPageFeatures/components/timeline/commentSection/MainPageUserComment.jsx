4aa71896e507d4d043dee226a7fba7c644d185d2
Jules <julesmoreno66@gmail.com>
2025-02-06 14:14:26 +0800

    refactor: update component added custom hooks to fetch user data

--- src/features/MainPageFeatures/components/timeline/commentSection/MainPageUserComment.jsx
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageUserComment.jsx
@@ -1,48 +1,20 @@
 import UserProfile from "@/components/UserProfile";
-import { db } from "@/lib/fb";
 import formatRelativeTime from "../../../utils/formatRelativeTime";
+import MainPageReadMoreBtn from "./MainPageReadMoreBtn";
+import useFetchUserData from "../../../hooks/commentSection/useFetchUserData";
 
-import { doc, getDoc } from "firebase/firestore";
 import PropTypes from "prop-types";
 import { useEffect, useRef, useState } from "react";
-import MainPageReadMoreBtn from "./MainPageReadMoreBtn";
+
 
 export default function MainPageUserComment({ userId, content, createdAt }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const paragraphRef = useRef(null); // Create a reference to the paragraph element
   const [isMultiLine, setIsMultiLine] = useState(false);// State to track if the comment is multiline
-  const [userInfo, setUserInfo] = useState({});
+  const userInfo = useFetchUserData(userId)
   const relativeTime = formatRelativeTime(createdAt);
 
-  useEffect(() => {
-    if (!userId) return; // Prevent fetching if userId is missing
-
-    async function fetchUserData() {
-      try {
-        const docRef = doc(db, "users", userId); // Create a reference to the user document in the database
-        const docSnap = await getDoc(docRef); // Fetch the document snapshot
-
-        // Check if the document exists
-        if (docSnap.exists()) {
-          // If it exists, set the user info state with the document data
-          setUserInfo(docSnap.data());
-        } else {
-          // If it doesn't exist, log an error and set the user info state to an empty object
-          console.error("No such document!");
-          setUserInfo({});
-        }
-      } catch (error) {
-        // Catch and log any errors that occur during the fetch operation
-        console.error("Error fetching user data:", error);
-        // Set the user info state to an empty object in case of an error
-        setUserInfo({});
-      }
-    }
-
-    // Call the fetchUserData function to fetch the user data
-    fetchUserData();
-  }, [userId]); // Runs when userId changes
 
   // Effect to make the feature responsive
   useEffect(() => {
