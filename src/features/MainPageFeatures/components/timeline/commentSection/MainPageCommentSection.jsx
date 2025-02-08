f9ff49f2c45a375cdcc37b14b272dca3df792803
Jules <julesmoreno66@gmail.com>
2025-02-08 13:31:39 +0800

    refactor: code clean up import useComment()

--- src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentSection.jsx
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentSection.jsx
@@ -1,51 +1,19 @@
-import { db } from "@/lib/fb";
 import MainPageCommentBox from "./MainPageCommentBox";
 import MainPageUserComment from "./MainPageUserComment";
+import useComment from "../../../hooks/commentSection/useComment";
 
-import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+
 
 export default function MainPageCommentSection({ authUserPhoto }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [usersComment, setUsersComment] = useState([]);
-  const [commentSize, setCommentSize] = useState("");
-  const commentId = searchParams.get('comment');
+  const { usersComment, commentSize } = useComment();
 
-  function fetchUserData(queryRef) {
-    if (!commentId) return;
-
-    // queryRef will use to make it customable later
-    // const q = query(collection(db, "userPosts"), where("postId", "==", commentId), collection(db, "userPosts", commentId, "comments"));
-
-    // add query order by createdAt
-    const q = query(collection(db, "userPosts", commentId, "comments"), orderBy("createdAt", "desc"));
-
-    // Subscribe to real-time updates
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      setUsersComment(querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-      setCommentSize(querySnapshot.size)
-    });
-
-    return unsubscribe; // Return the unsubscribe function to clean up
-  }
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    const unsubscribe = fetchUserData();
-
-    return () => {
-      if (unsubscribe) unsubscribe(); // Cleanup listener on unmount
-    };
-  }, [commentId]);
-
-  // If there is no comment query parameter, return null
   if (!searchParams.get('comment')) {
     return null;
   }
 
-  console.log("code clean up")
   return (
     <section className='p-2 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 bg-slate-950 rounded-lg z-50 text-gray-300 text-left md:block md:static md:-translate-x-0 md:translate-y-0 md:w-full md:max-w-md md:top-0 md:left-0 md:mt-5'>
       <div className="flex justify-between items-start">
