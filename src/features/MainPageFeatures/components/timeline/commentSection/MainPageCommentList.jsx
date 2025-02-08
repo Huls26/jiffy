6c4461893c97c2831103464126e1b1b41a8fbf39
Jules <julesmoreno66@gmail.com>
2025-02-08 13:43:36 +0800

    created: display users comment

---
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentList.jsx
@@ -0,0 +1,28 @@
+import MainPageUserComment from "./MainPageUserComment";
+import PropTypes from "prop-types";
+
+export default function MainPageCommentList({ usersCommentList }) {
+  return (
+    <>
+      {usersCommentList?.map(({ commentId, userId, content, createdAt }) => (
+        <MainPageUserComment
+          key={commentId}
+          userId={userId}
+          content={content}
+          createdAt={createdAt}
+        />
+      ))}
+    </>
+  )
+}
+
+MainPageCommentList.propTypes = {
+  usersCommentList: PropTypes.arrayOf(
+    PropTypes.shape({
+      commentId: PropTypes.string.isRequired,
+      userId: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      createdAt: PropTypes.any.isRequired
+    })
+  ),
+};
