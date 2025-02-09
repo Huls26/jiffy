476fcdae3087ee90e99d64bcbf5e0cb55c777777
Jules <julesmoreno66@gmail.com>
2025-02-09 15:39:19 +0800

    refactor: added active and inactive button style

--- src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
+++ src/features/MainPageFeatures/components/timeline/MainPageAuthFilterOptions.jsx
@@ -2,9 +2,13 @@ import { GlobalContext } from "@/contexts/GlobalContextProvider";
 
 import PropTypes from "prop-types";
 import { useContext } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export default function MainPageAuthFilterOptions({ isDisplay = [] }) {
   const [globalState] = useContext(GlobalContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const activeStyle = "bg-sky-500 hover:bg-gray-700 text-gray-200 font-bold py-1 px-3 first:rounded-l last:rounded-r";
+  const inactiveStyle = "bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3 first:rounded-l last:rounded-r"
 
   if (!globalState.userLogin) {
     return (
@@ -22,22 +26,25 @@ export default function MainPageAuthFilterOptions({ isDisplay = [] }) {
     <section className='text-sm sm:text-base mb-3 inline-flex'>
       <button
         type='button'
-        className="bg-sky-500 hover:bg-gray-700 text-gray-200 font-bold py-1 px-3 rounded-l"
+        className={!searchParams.get("filter") ? activeStyle : inactiveStyle}
         aria-label="Show All Posts"
+        onClick={() => setSearchParams({})}
       >
         All
       </button>
       <button
         type='button'
-        className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3"
+        className={searchParams.get("filter") === "likes" ? activeStyle : inactiveStyle}
         aria-label="Show Liked Posts"
+        onClick={() => setSearchParams({ filter: "likes" })}
       >
         Likes
       </button>
       <button
         type='button'
-        className="bg-gray-900 hover:bg-gray-700 active:bg-sky-500 text-gray-200 font-bold py-1 px-3 rounded-r"
+        className={searchParams.get("filter") === "following" ? activeStyle : inactiveStyle}
         aria-label="Show Following Posts"
+        onClick={() => setSearchParams({ filter: "following" })}
       >
         Following
       </button>
