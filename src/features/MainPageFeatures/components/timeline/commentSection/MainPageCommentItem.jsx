8f64359d585112fbff7ff90f5c3cf472c8ad4977
Jules <julesmoreno66@gmail.com>
2025-02-11 14:13:43 +0800

    refactor: added feature delete user comment

--- src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentItem.jsx
+++ src/features/MainPageFeatures/components/timeline/commentSection/MainPageCommentItem.jsx
@@ -1,19 +1,28 @@
 import useCheckMultiline from '../../../hooks/commentSection/useCheckMultiline';
 import MainPageReadMoreBtn from './MainPageReadMoreBtn';
 import formatRelativeTime from "../../../utils/formatRelativeTime";
+import { db } from '@/lib/fb';
 
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { doc, deleteDoc } from "firebase/firestore";
 
 export default function MainPageCommentItem({ username, content, createdAt, commentId }) {
   const { paragraphRef, isMultiLine, } = useCheckMultiline()
   const [isExpanded, setIsExpanded] = useState(false);
+  const [searchParams] = useSearchParams();
   const relativeTime = formatRelativeTime(createdAt);
   const truncatedStyle = "text-sm font-mono leading-4 truncate-multiline";
   const defaultStyle = "text-sm font-mono leading-4";
   const isTruncatedStyle = isExpanded ? defaultStyle : truncatedStyle;
 
-  console.log(commentId);
+  async function deleteUserComment() {
+    const postId = searchParams.get('comment');
+
+    await deleteDoc(doc(db, "userPosts", postId, 'comments', commentId));
+  }
+
   return (
     <div className="w-full">
       <div className="flex items-center justify-between">
@@ -22,7 +31,10 @@ export default function MainPageCommentItem({ username, content, createdAt, comm
           <span className="ml-2 text-gray-500 text-xs leading-3">{relativeTime}</span>
         </h1>
 
-        <button type="button" className='text-sm text-red-600 hover:text-red-500'>remove</button>
+        <button
+          onClick={deleteUserComment}
+          type="button"
+          className='text-sm text-red-600 hover:text-red-500'>remove</button>
       </div>
 
       <p ref={paragraphRef} className={isTruncatedStyle}>
