6267dc0bfe5ef039004a4f72089163de9a3afd59
Jules <julesmoreno66@gmail.com>
2025-02-11 13:17:27 +0800

    refactor: update button margin

--- src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
+++ src/features/MainPageFeatures/components/buttons/MainPageCreatePostBtn.jsx
@@ -11,7 +11,7 @@ export default function MainPageCreatePostBtn() {
     <div className="ml-auto">
       <button
         type="button"
-        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:text-gray-600 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-2"
+        className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:text-gray-600 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 active:opacity-70 font-bold rounded-lg text-sm px-2.5 py-0.5 text-center me-1"
         onClick={displayPostModal}
       >
         Post
