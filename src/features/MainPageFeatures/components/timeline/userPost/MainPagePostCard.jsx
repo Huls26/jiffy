7e27f8f6e374b3c3c58f0b98f4492bfd89d67207
Jules <julesmoreno66@gmail.com>
2025-03-03 12:10:31 +0800

    refactor: import Btn section component

--- src/features/MainPageFeatures/components/timeline/userPost/MainPagePostCard.jsx
+++ src/features/MainPageFeatures/components/timeline/userPost/MainPagePostCard.jsx
@@ -1,12 +1,8 @@
-
-import usePostInteraction from '../../../hooks/usePostInteraction';
-import handleLikeButton from "../../../utils/timeline/handleLikeButton";
-import MainPagePostCardBtn from './MainPagePostCardBtn';
 import MainPageUserPostCard from './MainPageUserPostCard';
 import "../style/MainPagePostCard.css";
+import MainPagePostCardBtnSection from './MainPagePostCardBtnSection';
 
 import PropTypes from "prop-types";
-import { useSearchParams } from "react-router-dom";
 
 /**
  * MainPagePostCard component renders a user's post on the main page timeline.
@@ -21,24 +17,6 @@ import { useSearchParams } from "react-router-dom";
  * @returns {JSX.Element} The rendered component.
  */
 export default function MainPagePostCard({ userPost }) {
-  const {
-    buttonState,
-    dispatch,
-    currentUserId,
-  } = usePostInteraction(userPost);
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const handleCommentButton = () => {
-    if (!searchParams.get('comment')) {
-      setSearchParams({ comment: userPost.postId });
-    } if (searchParams.get('comment') !== userPost.postId) {
-      setSearchParams({ comment: userPost.postId });
-    }
-    else {
-      setSearchParams({});
-    }
-  }
-
   return (
     <section
       className='space-y-2 bg-slate-950 text-start min-w-[270px] max-w-xl sm:rounded-lg border-4 border-gray-950 cursor-pointer'
@@ -48,25 +26,7 @@ export default function MainPagePostCard({ userPost }) {
 
       {/* button section */}
       {/* Container for the buttons */}
-      <div className='p-2 text-sm sm:text-base flex justify-between select-none'>
-
-        {/* Like button */}
-        <MainPagePostCardBtn
-          isActive={buttonState.likeButton}
-          onClick={() => handleLikeButton(dispatch, userPost, buttonState, currentUserId)}
-          ariaLabel="Like post"
-          likesCount={buttonState.likesCount}
-          textContent={"Like"}
-        />
-
-        {/* Comment button */}
-        <MainPagePostCardBtn
-          isActive={searchParams.get('comment') === userPost.postId}
-          onClick={handleCommentButton}
-          ariaLabel="Toggle comment section for post"
-          textContent={"Comment"}
-        />
-      </div>
+      <MainPagePostCardBtnSection userPost={userPost} />
     </section>);
 }
 
