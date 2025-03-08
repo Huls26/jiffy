7215a27bc0be738a310c83937c4fca44e773699e
Jules <julesmoreno66@gmail.com>
2025-03-08 12:35:52 +0800

    refactor: added user profile

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,28 @@
-import useAuthListener from "@/hooks/useAuthListener";
 import usePageTitle from "@/hooks/usePageTitle";
+import UserProfile from "@/components/UserProfile";
+import { GlobalContext } from "@/contexts/GlobalContextProvider";
+
+import { useContext } from "react";
 
 export default function ProfilePage() {
-  const { username } = useAuthListener();
-  usePageTitle(`${username} 👀`);
+  const [globalState] = useContext(GlobalContext);
+  usePageTitle(`${globalState.username} 👀`);
 
+  console.log(globalState)
   return (
-    <div className="text-center text-3xl text-gray-50">
-      ProfilePage
-    </div>);
+    <main className="px-5 py-3 text-gray-50">
+      <header className="flex items-center space-x-4 ">
+        <UserProfile
+          photoURL={globalState.photoURL}
+          addedClassName={"w-15 h-15"}
+        />
+        <div>
+          <h1 className="text-xl text-sky-400 font-semibold">{globalState.username}</h1>
+          <p className="text-base text-gray-300 leading-3">{globalState.email}</p>
+        </div>
+      </header>
+      <section>
+        user post
+      </section>
+    </main>);
 }
