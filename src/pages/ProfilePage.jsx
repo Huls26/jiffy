95a469c2d7b7d586df52ed228200ed984d4807dd
Jules <julesmoreno66@gmail.com>
2025-03-13 11:26:02 +0800

    refactor: added update profile button

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,7 +2,8 @@ import usePageTitle from "@/hooks/usePageTitle";
 import UserProfile from "@/components/UserProfile";
 import { GlobalContext } from "@/contexts/GlobalContextProvider";
 import { db } from "@/lib/fb";
-import MainPagePostsFeed from "../features/MainPageFeatures/components/timeline/userPost/MainPagePostsFeed";
+import MainPagePostsFeed from "@/features/MainPageFeatures/components/timeline/userPost/MainPagePostsFeed";
+import MainPageSidebar from "@/features/MainPageFeatures/sections/MainPageSidebar";
 
 import { useContext } from "react";
 import { collection, query, where, onSnapshot, doc, getDoc } from "firebase/firestore";
@@ -37,8 +38,8 @@ export default function ProfilePage() {
   }, [globalState.userId]);
 
   return (
-    <main className="px-5 py-3 text-gray-50">
-      <header className="mb-5 flex items-center space-x-3">
+    <main className="px-5 py-3 text-gray-50 max-w-6xl m-auto">
+      <header className="mb-5 flex items-center space-x-3 cursor-pointer">
         <UserProfile
           photoURL={globalState?.photoURL}
           addedClassName={"w-15 h-15"}
@@ -48,9 +49,23 @@ export default function ProfilePage() {
           <p className="text-sm text-gray-400 leading-3">{globalState.email}</p>
           <p className="font-semibold text-gray-300">{userData?.followersCount} Followers</p>
         </div>
+
+        <button
+          type="button"
+          className="ml-auto font-semibold text-sky-400 text-lg hover:text-sky-600">
+          Update Profile
+        </button>
       </header>
-      <section className="pt-5 border-t-2 border-gray-400">
-        <MainPagePostsFeed userPosts={userPosts} />
+
+      <section className="
+        pt-5 border-t-2 border-gray-400 text-center
+        md:px-2 flex flex-col-reverse md:flex-row
+      ">
+        <div className="flex-1 mt-5">
+          <MainPagePostsFeed userPosts={userPosts} />
+        </div>
+
+        <MainPageSidebar />
       </section>
     </main>);
 }
