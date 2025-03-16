6c66ac46dcc5e1b062fa9732fd90b42db61e9d41
Jules <julesmoreno66@gmail.com>
2025-03-16 11:00:30 +0800

    refactor: import modal input field

--- src/features/ProfilePageFeatures/components/ProfilePageUpdateProfileModal.jsx
+++ src/features/ProfilePageFeatures/components/ProfilePageUpdateProfileModal.jsx
@@ -1,9 +1,12 @@
+import ProfilePageModalInputField from "./ProfilePageModalInputField";
+
 import { createPortal } from "react-dom";
 import { useReducer } from "react";
 
 const initialState = {
   profilePic: null,
   username: "",
+  fullName: "",
   email: "",
   password: ""
 };
@@ -14,6 +17,8 @@ const reducer = (state, action) => {
       return { ...state, profilePic: action.payload };
     case "SET_USERNAME":
       return { ...state, username: action.payload };
+    case "SET_FULL_NAME":
+      return { ...state, fullName: action.payload };
     case "SET_EMAIL":
       return { ...state, email: action.payload };
     case "SET_PASSWORD":
@@ -37,26 +42,36 @@ export default function Modal({ isOpen, onClose }) {
       <h1 className="mb-5 text-lg font-bold">Update Profile</h1>
 
       <div className="flex flex-col gap-4">
-        <input
-          className="px-3 py-1 bg-gray-800 text-gray-200 rounded"
-          placeholder="Username"
-          type="text"
+        <ProfilePageModalInputField
+          placeholder={"Username"}
+          type={"text"}
           value={state.username}
           onChange={(e) => dispatch({ type: "SET_USERNAME", payload: e.target.value })} />
-        <input
-          placeholder="Email"
-          type="email"
+        <ProfilePageModalInputField
+          placeholder={"Full Name"}
+          type={"text"}
+          value={state.fullName}
+          onChange={(e) => dispatch({ type: "SET_FULL_NAME", payload: e.target.value })} />
+        <ProfilePageModalInputField
+          placeholder={"Email"}
+          type={"email"}
           value={state.email}
           onChange={(e) => dispatch({ type: "SET_EMAIL", payload: e.target.value })} />
-        <input
-          placeholder="New Password"
-          type="password"
+        <ProfilePageModalInputField
+          placeholder={"New Password"}
+          type={"password"}
           value={state.password}
           onChange={(e) => dispatch({ type: "SET_PASSWORD", payload: e.target.value })} />
-        <input className="text-blue-300" type="file" onChange={handleFileChange} />
+        <div className="flex flex-col">
+          <label htmlFor="modalProfilePic" className="text-blue-100">Profile Picture:</label>
+          <input className="text-blue-300" name="modalProfilePic" id="modalProfilePic" type="file" onChange={handleFileChange} />
+        </div>
       </div>
       <button type='button' onClick={onClose} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">
         Cancel
+      </button>
+      <button type='submit' onClick={onClose} className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+        Update
       </button>
     </div>,
     document.getElementById("root") // Append modal outside the normal React tree
