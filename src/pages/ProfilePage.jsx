d5d042ac445efc1bbf0c1d3d31a9e83f93fda62b
Jules <julesmoreno66@gmail.com>
2025-04-19 16:01:01 +0800

    refactor: update users fetch post useEffect

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,22 +5,41 @@ import MainPagePostsFeed from "@/features/MainPageFeatures/components/timeline/u
 import MainPageSidebar from "@/features/MainPageFeatures/sections/MainPageSidebar";
 import ProfilePageHeader from "@/features/ProfilePageFeatures/components/ProfilePageHeader";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProfilePage() {
   const [globalState] = useContext(GlobalContext);
   usePageTitle(`${globalState.username} 👀`);
   const [userPosts, setUserPosts] = useState(null);
+  const { username } = useParams();
 
+  // Fetch user posts from Firestore
   useEffect(() => {
-    // Get all posts for the user from Firestore
-    const citiesRef = collection(db, "userPosts");
-    const q = query(citiesRef,
-      where("userId", "==", globalState.userId),
-      orderBy("dateCreated", "desc")
-    );
+    let q = null;
+
+    // check if the user is viewing their own profile or someone else's
+    // if the user is viewing their own profile, use globalState.userId
+
+    if (username === globalState.username) {
+      const citiesRef = collection(db, "userPosts");
+      q = query(
+        citiesRef,
+        where("userId", "==", globalState.userId),
+        orderBy("dateCreated", "desc")
+      );
+    } else if (username) {
+      const citiesRef = collection(db, "userPosts");
+      q = query(
+        citiesRef,
+        where("userId", "==", username),
+        orderBy("dateCreated", "desc")
+      );
+    }
+
+    if (!q) return; // ⚠️ Prevent running onSnapshot with a null query
+
     const unsubscribePosts = onSnapshot(q, (querySnapshot) => {
       const posts = querySnapshot.docs.map((doc) => doc.data());
       setUserPosts(posts);
@@ -29,7 +48,8 @@ export default function ProfilePage() {
     return () => {
       unsubscribePosts();
     };
-  }, [globalState.userId]);
+  }, [globalState.userId, globalState.username, username]);
+
 
   return (
     <main className="px-5 py-3 text-gray-50 max-w-6xl m-auto">
